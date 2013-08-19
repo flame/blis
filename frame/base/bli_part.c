flame/blis

commit 46d3d09d49aded1d9f1b468c83fce75e07d631dc

Consolidated lower/upper her[2]k blocked variants.
Details:
- Consolidated lower and upper blocked variants for herk and her2k, and
  renamed the resulting variants, according to the same changes recently
  made to trmm and trsm.
- Implemented support for four new subpartitions types:
    BLIS_SUBPART1T
    BLIS_SUBPART1B
    BLIS_SUBPART1L
    BLIS_SUBPART1R
  which correspond to "merged" partitions that include the middle "1"
  partition as well as either the neighboring "0" or "2" partition. This is
  used to clean up code in herk/her2k var2 that attempts to partition away
  the strictly zero region above or below the diagonal of a matrix operand
  that is being marched through diagonally.
- Added safeguards to herk macro-kernels that skip any leading or trailing
  zero region in the panel of C that is passed in. This is now needed given
  that herk/her2k var1 no longer partitions off this zero region before
  calling the macro-kernel (via bli_her[2]k_int()).
- Updated comments and other whitespace changes to trmm/trsm macro-kernels.

frame/base/bli_part.c
@@ -99,6 +99,15 @@ void bli_acquire_mpart_t2b( subpart_t  requested_part,
 		m_part   = i;
 		n_part   = n;
 	}
+	if      ( requested_part == BLIS_SUBPART1T )
+	{
+		// A1T (offm,offn) unchanged.
+		// A1T is (i+b) x n.
+		offm_inc = 0;
+		offn_inc = 0;
+		m_part   = i + b;
+		n_part   = n;
+	}
 	else if ( requested_part == BLIS_SUBPART1 )
 	{
 		// A1 (offm,offn) += (i,0).
@@ -106,6 +115,15 @@ void bli_acquire_mpart_t2b( subpart_t  requested_part,
 		offm_inc = i;
 		offn_inc = 0;
 		m_part   = b;
+		n_part   = n;
+	}
+	else if ( requested_part == BLIS_SUBPART1B )
+	{
+		// A1B (offm,offn) += (i,0).
+		// A1B is (m-i) x n.
+		offm_inc = i;
+		offn_inc = 0;
+		m_part   = m - i;
 		n_part   = n;
 	}
 	else // if ( requested_part == BLIS_SUBPART2 )
@@ -269,6 +287,15 @@ void bli_acquire_mpart_l2r( subpart_t  requested_part,
 		m_part   = m;
 		n_part   = j;
 	}
+	if      ( requested_part == BLIS_SUBPART1L )
+	{
+		// A1L (offm,offn) unchanged.
+		// A1L is m x (j+b).
+		offm_inc = 0;
+		offn_inc = 0;
+		m_part   = m;
+		n_part   = j + b;
+	}
 	else if ( requested_part == BLIS_SUBPART1 )
 	{
 		// A1 (offm,offn) += (0,j).
@@ -277,6 +304,15 @@ void bli_acquire_mpart_l2r( subpart_t  requested_part,
 		offn_inc = j;
 		m_part   = m;
 		n_part   = b;
+	}
+	else if ( requested_part == BLIS_SUBPART1R )
+	{
+		// A1R (offm,offn) += (0,j).
+		// A1R is m x (n-j).
+		offm_inc = 0;
+		offn_inc = j;
+		m_part   = m;
+		n_part   = n - j;
 	}
 	else // if ( requested_part == BLIS_SUBPART2 )
 	{
@@ -451,15 +487,6 @@ void bli_acquire_mpart_tl2br( subpart_t  requested_part,
 		m_part   = b;
 		n_part   = ij;
 	}
-	else if ( requested_part == BLIS_SUBPART10B )
-	{
-		// A10B (offm,offn) += (ij,0).
-		// A10B is b x (ij + b).
-		offm_inc = ij;
-		offn_inc = 0;
-		m_part   = b;
-		n_part   = ij + b;
-	}
 	else if ( requested_part == BLIS_SUBPART20 )
 	{
 		// A20 (offm,offn) += (ij+b,0).
@@ -517,15 +544,6 @@ void bli_acquire_mpart_tl2br( subpart_t  requested_part,
 		offn_inc = ij + b;
 		m_part   = b;
 		n_part   = n - ij - b;
-	}
-	else if ( requested_part == BLIS_SUBPART12B )
-	{
-		// A12B (offm,offn) += (ij,ij).
-		// A12B is b x (n-ij).
-		offm_inc = ij;
-		offn_inc = ij;
-		m_part   = b;
-		n_part   = n - ij;
 	}
 	else // if ( requested_part == BLIS_SUBPART22 )
 	{

frame/3/her2k/bli_her2k.h
@@ -37,16 +37,11 @@
 #include "bli_her2k_int.h"
 #include "bli_her2k_target.h"
 
-#include "bli_her2k_l_blk_var1.h"
-#include "bli_her2k_u_blk_var1.h"
+#include "bli_her2k_blk_var1f.h"
 
-#include "bli_her2k_l_blk_var2.h"
-#include "bli_her2k_u_blk_var2.h"
+#include "bli_her2k_blk_var2f.h"
 
-#include "bli_her2k_blk_var3.h"
-
-#include "bli_her2k_l_blk_var4.h"
-#include "bli_her2k_u_blk_var4.h"
+#include "bli_her2k_blk_var3f.h"
 
 #include "bli_her2k_l_ker_var2.h"
 #include "bli_her2k_u_ker_var2.h"

frame/3/her2k/bli_her2k_blk_var1f.c
@@ -34,27 +34,32 @@
 
 #include "blis.h"
 
-void bli_herk_u_blk_var1( obj_t*  alpha,
-                          obj_t*  a,
-                          obj_t*  ah,
-                          obj_t*  beta,
-                          obj_t*  c,
-                          herk_t* cntl )
+void bli_her2k_blk_var1f( obj_t*   alpha,
+                          obj_t*   a,
+                          obj_t*   bh,
+                          obj_t*   alpha_conj,
+                          obj_t*   b,
+                          obj_t*   ah,
+                          obj_t*   beta,
+                          obj_t*   c,
+                          her2k_t* cntl )
 {
 	obj_t a1, a1_pack;
-	obj_t ah_pack, ahR_pack;
-	obj_t c1;
-	obj_t c1R, c1R_pack;
+	obj_t bh_pack;
+	obj_t b1, b1_pack;
+	obj_t ah_pack;
+	obj_t c1, c1_pack;
 
 	dim_t i;
 	dim_t b_alg;
 	dim_t m_trans;
-	dim_t offR, nR;
 
 	// Initialize all pack objects that are passed into packm_init().
 	bli_obj_init_pack( &a1_pack );
+	bli_obj_init_pack( &bh_pack );
+	bli_obj_init_pack( &b1_pack );
 	bli_obj_init_pack( &ah_pack );
-	bli_obj_init_pack( &c1R_pack );
+	bli_obj_init_pack( &c1_pack );
 
 	// Query dimension in partitioning direction.
 	m_trans = bli_obj_length_after_trans( *c );
@@ -64,22 +69,23 @@ void bli_herk_u_blk_var1( obj_t*  alpha,
 	               c,
 	               cntl_sub_scalm( cntl ) );
 
-	// Initialize object for packing A'.
-	bli_packm_init( ah, &ah_pack,
+	//
+	// Perform first rank-k update: C = C + alpha * A * B'.
+	//
+
+	// Initialize object for packing B'.
+	bli_packm_init( bh, &bh_pack,
 	                cntl_sub_packm_b( cntl ) );
 
-	// Pack A' and scale by alpha (if instructed).
+	// Pack B' and scale by alpha (if instructed).
 	bli_packm_int( alpha,
-	               ah, &ah_pack,
+	               bh, &bh_pack,
 	               cntl_sub_packm_b( cntl ) );
 
 	// Partition along the m dimension.
 	for ( i = 0; i < m_trans; i += b_alg )
 	{
 		// Determine the current algorithmic blocksize.
-		// NOTE: Use of a (for execution datatype) is intentional!
-		// This causes the right blocksize to be used if c and a are
-		// complex and b is real.
 		b_alg = bli_determine_blocksize_f( i, m_trans, a,
 		                                   cntl_blocksize( cntl ) );
 
@@ -89,20 +95,10 @@ void bli_herk_u_blk_var1( obj_t*  alpha,
 		bli_acquire_mpart_t2b( BLIS_SUBPART1,
 		                       i, b_alg, c, &c1 );
 
-		// Partition off the stored region of C1 and the corresponding region
-		// of Ah_pack. We compute the width of the subpartition taking the
-		// location of the diagonal into account.
-		offR = bli_max( 0, bli_obj_diag_offset_after_trans( c1 ) );
-		nR   = bli_obj_width_after_trans( c1 ) - offR;
-		bli_acquire_mpart_l2r( BLIS_SUBPART1,
-		                       offR, nR, &c1, &c1R );
-		bli_acquire_mpart_l2r( BLIS_SUBPART1,
-		                       offR, nR, &ah_pack, &ahR_pack );
-
 		// Initialize objects for packing A1 and C1.
 		bli_packm_init( &a1, &a1_pack,
 		                cntl_sub_packm_a( cntl ) );
-		bli_packm_init( &c1R, &c1R_pack,
+		bli_packm_init( &c1, &c1_pack,
 		                cntl_sub_packm_c( cntl ) );
 
 		// Pack A1 and scale by alpha (if instructed).
@@ -112,26 +108,86 @@ void bli_herk_u_blk_var1( obj_t*  alpha,
 
 		// Pack C1 and scale by beta (if instructed).
 		bli_packm_int( beta,
-		               &c1R, &c1R_pack,
+		               &c1, &c1_pack,
 		               cntl_sub_packm_c( cntl ) );
 
 		// Perform herk subproblem.
 		bli_herk_int( alpha,
 		              &a1_pack,
-		              &ahR_pack,
+		              &bh_pack,
 		              beta,
-		              &c1R_pack,
+		              &c1_pack,
 		              cntl_sub_herk( cntl ) );
 
 		// Unpack C1 (if C1 was packed).
-		bli_unpackm_int( &c1R_pack, &c1R,
+		bli_unpackm_int( &c1_pack, &c1,
 		                 cntl_sub_unpackm_c( cntl ) );
 	}
 
 	// If any packing buffers were acquired within packm, release them back
 	// to the memory manager.
 	bli_obj_release_pack( &a1_pack );
+	bli_obj_release_pack( &bh_pack );
+
+	//
+	// Perform second rank-k update: C = C + conj(alpha) * B * A'.
+	//
+
+	// Initialize object for packing A'.
+	bli_packm_init( ah, &ah_pack,
+	                cntl_sub_packm_b( cntl ) );
+
+	// Pack A' and scale by alpha_conj (if instructed).
+	bli_packm_int( alpha_conj,
+	               ah, &ah_pack,
+	               cntl_sub_packm_b( cntl ) );
+
+	// Partition along the m dimension.
+	for ( i = 0; i < m_trans; i += b_alg )
+	{
+		// Determine the current algorithmic blocksize.
+		b_alg = bli_determine_blocksize_f( i, m_trans, b,
+		                                   cntl_blocksize( cntl ) );
+
+		// Acquire partitions for B1 and C1.
+		bli_acquire_mpart_t2b( BLIS_SUBPART1,
+		                       i, b_alg, b, &b1 );
+		bli_acquire_mpart_t2b( BLIS_SUBPART1,
+		                       i, b_alg, c, &c1 );
+
+		// Initialize objects for packing B1 and C1.
+		bli_packm_init( &b1, &b1_pack,
+		                cntl_sub_packm_a( cntl ) );
+		bli_packm_init( &c1, &c1_pack,
+		                cntl_sub_packm_c( cntl ) );
+
+		// Pack B1 and scale by alpha_conj (if instructed).
+		bli_packm_int( alpha_conj,
+		               &b1, &b1_pack,
+		               cntl_sub_packm_a( cntl ) );
+
+		// Pack C1 and scale by beta (if instructed).
+		bli_packm_int( beta,
+		               &c1, &c1_pack,
+		               cntl_sub_packm_c( cntl ) );
+
+		// Perform herk subproblem.
+		bli_herk_int( alpha_conj,
+		              &b1_pack,
+		              &ah_pack,
+		              &BLIS_ONE,
+		              &c1_pack,
+		              cntl_sub_herk( cntl ) );
+
+		// Unpack C1 (if C1 was packed).
+		bli_unpackm_int( &c1_pack, &c1,
+		                 cntl_sub_unpackm_c( cntl ) );
+	}
+
+	// If any packing buffers were acquired within packm, release them back
+	// to the memory manager.
+	bli_obj_release_pack( &b1_pack );
 	bli_obj_release_pack( &ah_pack );
-	bli_obj_release_pack( &c1R_pack );
+	bli_obj_release_pack( &c1_pack );
 }
 

frame/3/her2k/bli_her2k_blk_var1f.h
@@ -32,10 +32,13 @@
 
 */
 
-void bli_herk_l_blk_var2( obj_t*  alpha,
-                          obj_t*  a,
-                          obj_t*  ah,
-                          obj_t*  beta,
-                          obj_t*  c,
-                          herk_t* cntl );
+void bli_her2k_blk_var1f( obj_t*   alpha,
+                          obj_t*   a,
+                          obj_t*   bh,
+                          obj_t*   alpha_conj,
+                          obj_t*   b,
+                          obj_t*   ah,
+                          obj_t*   beta,
+                          obj_t*   c,
+                          her2k_t* cntl );
 

frame/3/her2k/bli_her2k_blk_var2f.c
@@ -34,129 +34,133 @@
 
 #include "blis.h"
 
-void bli_her2k_blk_var3( obj_t*   alpha,
-                         obj_t*   a,
-                         obj_t*   bh,
-                         obj_t*   alpha_conj,
-                         obj_t*   b,
-                         obj_t*   ah,
-                         obj_t*   beta,
-                         obj_t*   c,
-                         her2k_t* cntl )
+void bli_her2k_blk_var2f( obj_t*   alpha,
+                          obj_t*   a,
+                          obj_t*   bh,
+                          obj_t*   alpha_conj,
+                          obj_t*   b,
+                          obj_t*   ah,
+                          obj_t*   beta,
+                          obj_t*   c,
+                          her2k_t* cntl )
 {
-	obj_t  a1, a1_pack;
-	obj_t  bh1, bh1_pack;
-	obj_t  b1, b1_pack;
-	obj_t  ah1, ah1_pack;
-	obj_t  c_pack;
-	obj_t* beta_use;
+	obj_t a_pack, aS_pack;
+	obj_t bh1, bh1_pack;
+	obj_t b_pack, bS_pack;
+	obj_t ah1, ah1_pack;
+	obj_t c1;
+	obj_t c1S, c1S_pack;
 
-	dim_t  i;
-	dim_t  b_alg;
-	dim_t  k_trans;
+	dim_t i;
+	dim_t b_alg;
+	dim_t n_trans;
+	subpart_t stored_part;
 
 	// Initialize all pack objects that are passed into packm_init().
-	bli_obj_init_pack( &a1_pack );
+	bli_obj_init_pack( &a_pack );
 	bli_obj_init_pack( &bh1_pack );
-	bli_obj_init_pack( &b1_pack );
+	bli_obj_init_pack( &b_pack );
 	bli_obj_init_pack( &ah1_pack );
-	bli_obj_init_pack( &c_pack );
+	bli_obj_init_pack( &c1S_pack );
+
+	// The upper and lower variants are identical, except for which
+	// merged subpartition is acquired in the loop body.
+	if ( bli_obj_is_lower( *c ) ) stored_part = BLIS_SUBPART1B;
+	else                          stored_part = BLIS_SUBPART1T;
 
 	// Query dimension in partitioning direction.
-	k_trans = bli_obj_width_after_trans( *a );
+	n_trans = bli_obj_width_after_trans( *c );
 
 	// Scale C by beta (if instructed).
 	bli_scalm_int( beta,
 	               c,
 	               cntl_sub_scalm( cntl ) );
 
-	// Initialize object for packing C.
-	bli_packm_init( c, &c_pack,
-	                cntl_sub_packm_c( cntl ) );
+	// Initialize objects for packing A and B.
+	bli_packm_init( a, &a_pack,
+	                cntl_sub_packm_a( cntl ) );
+	bli_packm_init( b, &b_pack,
+	                cntl_sub_packm_a( cntl ) );
 
-	// Pack C and scale by beta (if instructed).
-	bli_packm_int( beta,
-	               c, &c_pack,
-	               cntl_sub_packm_c( cntl ) );
+	// Pack A and scale by alpha (if instructed).
+	bli_packm_int( alpha,
+	               a, &a_pack,
+	               cntl_sub_packm_a( cntl ) );
 
-	// Partition along the k dimension.
-	for ( i = 0; i < k_trans; i += b_alg )
+	// Pack B and scale by alpha_conj (if instructed).
+	bli_packm_int( alpha_conj,
+	               b, &b_pack,
+	               cntl_sub_packm_a( cntl ) );
+
+	// Partition along the n dimension.
+	for ( i = 0; i < n_trans; i += b_alg )
 	{
 		// Determine the current algorithmic blocksize.
-		b_alg = bli_determine_blocksize_f( i, k_trans, bh,
+		b_alg = bli_determine_blocksize_f( i, n_trans, bh,
 		                                   cntl_blocksize( cntl ) );
 
-		// Acquire partitions for A1 and B1'.
+		// Acquire partitions for B1', A1', and C1.
 		bli_acquire_mpart_l2r( BLIS_SUBPART1,
-		                       i, b_alg, a, &a1 );
-		bli_acquire_mpart_t2b( BLIS_SUBPART1,
 		                       i, b_alg, bh, &bh1 );
+		bli_acquire_mpart_l2r( BLIS_SUBPART1,
+		                       i, b_alg, ah, &ah1 );
+		bli_acquire_mpart_l2r( BLIS_SUBPART1,
+		                       i, b_alg, c, &c1 );
 
-		// Initialize objects for packing A1 and B1'.
-		bli_packm_init( &a1, &a1_pack,
-		                cntl_sub_packm_a( cntl ) );
+		// Partition off the stored region of C1 and the corresponding regions
+		// of A_pack and B_pack.
+		bli_acquire_mpart_t2b( stored_part,
+		                       i, b_alg, &c1, &c1S );
+		bli_acquire_mpart_t2b( stored_part,
+		                       i, b_alg, &a_pack, &aS_pack );
+		bli_acquire_mpart_t2b( stored_part,
+		                       i, b_alg, &b_pack, &bS_pack );
+
+		// Initialize objects for packing B1', A1', and C1.
 		bli_packm_init( &bh1, &bh1_pack,
 		                cntl_sub_packm_b( cntl ) );
-
-		// Pack A1 and scale by alpha (if instructed).
-		bli_packm_int( alpha,
-		               &a1, &a1_pack,
-		               cntl_sub_packm_a( cntl ) );
+		bli_packm_init( &ah1, &ah1_pack,
+		                cntl_sub_packm_b( cntl ) );
+		bli_packm_init( &c1S, &c1S_pack,
+		                cntl_sub_packm_c( cntl ) );
 
 		// Pack B1' and scale by alpha (if instructed).
 		bli_packm_int( alpha,
 		               &bh1, &bh1_pack,
 		               cntl_sub_packm_b( cntl ) );
 
-		// Acquire partitions for B1 and A1'.
-		bli_acquire_mpart_l2r( BLIS_SUBPART1,
-		                       i, b_alg, b, &b1 );
-		bli_acquire_mpart_t2b( BLIS_SUBPART1,
-		                       i, b_alg, ah, &ah1 );
-
-		// Initialize objects for packing B1 and A1'.
-		bli_packm_init( &b1, &b1_pack,
-		                cntl_sub_packm_a( cntl ) );
-		bli_packm_init( &ah1, &ah1_pack,
-		                cntl_sub_packm_b( cntl ) );
-
-		// Pack B1 and scale by alpha_conj (if instructed).
-		bli_packm_int( alpha_conj,
-		               &b1, &b1_pack,
-		               cntl_sub_packm_a( cntl ) );
-
 		// Pack A1' and scale by alpha_conj (if instructed).
 		bli_packm_int( alpha_conj,
 		               &ah1, &ah1_pack,
 		               cntl_sub_packm_b( cntl ) );
 
-		// Since this variant executes multiple rank-2k updates, we must use
-		// beta only for the first iteration and BLIS_ONE for all others.
-		if ( i == 0 ) beta_use = beta;
-		else          beta_use = &BLIS_ONE;
+		// Pack C1 and scale by beta (if instructed).
+		bli_packm_int( beta,
+		               &c1S, &c1S_pack,
+		               cntl_sub_packm_c( cntl ) );
 
-		// Perform herk subproblem.
+		// Perform her2k subproblem.
 		bli_her2k_int( alpha,
-		               &a1_pack,
+		               &aS_pack,
 		               &bh1_pack,
 		               alpha_conj,
-		               &b1_pack,
+		               &bS_pack,
 		               &ah1_pack,
-		               beta_use,
-		               &c_pack,
+		               beta,
+		               &c1S_pack,
 		               cntl_sub_her2k( cntl ) );
-	}
 
-	// Unpack C (if C was packed).
-	bli_unpackm_int( &c_pack, c,
-	                 cntl_sub_unpackm_c( cntl ) );
+		// Unpack C1 (if C1 was packed).
+		bli_unpackm_int( &c1S_pack, &c1S,
+		                 cntl_sub_unpackm_c( cntl ) );
+	}
 
 	// If any packing buffers were acquired within packm, release them back
 	// to the memory manager.
-	bli_obj_release_pack( &a1_pack );
+	bli_obj_release_pack( &a_pack );
 	bli_obj_release_pack( &bh1_pack );
-	bli_obj_release_pack( &b1_pack );
+	bli_obj_release_pack( &b_pack );
 	bli_obj_release_pack( &ah1_pack );
-	bli_obj_release_pack( &c_pack );
+	bli_obj_release_pack( &c1S_pack );
 }
 

commit dedda523dc5dc779ecc34e6a03dc74cb8eb220de

Fixed bug in bli_acquire_mpart_t2b(), _l2r().
Details:
- Fixed a bug in bli_acquire_mpart_t2b() and bli_acquire_mpart_l2r()
  that cause incorrect partitioning when SUBPART0 was requested. This
  bug was introduced in 46d3d09d49ad. Thanks to Bryan for isolating
  this bug.
- Removed dupl kernels from kernels/x86_64/3 directory.
- Uncommented beta == 0 optimizaition code in
  kernels/x86_64/3/bli_gemm_opt_d4x4.c.

frame/base/bli_part.c
@@ -99,7 +99,7 @@ void bli_acquire_mpart_t2b( subpart_t  requested_part,
 		m_part   = i;
 		n_part   = n;
 	}
-	if      ( requested_part == BLIS_SUBPART1T )
+	else if ( requested_part == BLIS_SUBPART1T )
 	{
 		// A1T (offm,offn) unchanged.
 		// A1T is (i+b) x n.
@@ -287,7 +287,7 @@ void bli_acquire_mpart_l2r( subpart_t  requested_part,
 		m_part   = m;
 		n_part   = j;
 	}
-	if      ( requested_part == BLIS_SUBPART1L )
+	else if ( requested_part == BLIS_SUBPART1L )
 	{
 		// A1L (offm,offn) unchanged.
 		// A1L is m x (j+b).

kernels/x86_64/3/bli_dupl_opt_var1.c
@@ -1,186 +1,0 @@
-/*
-
-   BLIS    
-   An object-based framework for developing high-performance BLAS-like
-   libraries.
-
-   Copyright (C) 2013, The University of Texas
-
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions are
-   met:
-    - Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    - Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    - Neither the name of The University of Texas nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-
-   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-   HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-   THEORY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-*/
-
-#include "blis.h"
-
-void bli_sdupl_opt_var1(
-                         dim_t     n_elem,
-                         float*    b,
-                         float*    bd
-                       )
-{
-	bli_check_error_code( BLIS_NOT_YET_IMPLEMENTED );
-}
-
-void bli_ddupl_opt_var1(
-                         dim_t     n_elem,
-                         double*   b,
-                         double*   bd
-                       )
-{
-	dim_t n_iter = n_elem / 16;
-	dim_t n_left = n_elem % 16;
-
-	__asm__ volatile
-	(
-		"                                  \n\t"
-		"movq      %2, %%rax               \n\t" // load address of b.
-		"movq      %3, %%rbx               \n\t" // load address of bd.
-		"                                  \n\t"
-		"addq     $8 * 16, %%rax           \n\t" // increment pointers to allow byte
-		"addq     $8 * 16, %%rbx           \n\t" // offsets in the unrolled iterations.
-		"                                  \n\t"
-		"movq      %0, %%rsi               \n\t" // i = n_iter;
-		"testq  %%rsi, %%rsi               \n\t" // check n_iter via logical AND.
-		"je     .CONSIDERNLEFT             \n\t" // if i == 0, jump to code that
-		"                                  \n\t" // contains the n_left loop.
-		"                                  \n\t"
-		"                                  \n\t"
-		".LOOPNITER:                       \n\t" // MAIN LOOP
-		"                                  \n\t"
-		"movapd  -8 * 16(%%rax),  %%xmm1   \n\t"
-		"movapd  -7 * 16(%%rax),  %%xmm3   \n\t"
-		"movapd  -6 * 16(%%rax),  %%xmm5   \n\t"
-		"movapd  -5 * 16(%%rax),  %%xmm7   \n\t"
-		"                                  \n\t"
-		"movapd  -4 * 16(%%rax),  %%xmm9   \n\t"
-		"movapd  -3 * 16(%%rax), %%xmm11   \n\t"
-		"movapd  -2 * 16(%%rax), %%xmm13   \n\t"
-		"movapd  -1 * 16(%%rax), %%xmm15   \n\t"
-		"                                  \n\t"
-		"                                  \n\t"
-		"movddup    %%xmm1,  %%xmm0        \n\t"
-		"unpckhpd   %%xmm1,  %%xmm1        \n\t"
-		"movddup    %%xmm3,  %%xmm2        \n\t"
-		"unpckhpd   %%xmm3,  %%xmm3        \n\t"
-		"movddup    %%xmm5,  %%xmm4        \n\t"
-		"unpckhpd   %%xmm5,  %%xmm5        \n\t"
-		"movddup    %%xmm7,  %%xmm6        \n\t"
-		"unpckhpd   %%xmm7,  %%xmm7        \n\t"
-		"                                  \n\t"
-		"movddup    %%xmm9,  %%xmm8        \n\t"
-		"unpckhpd   %%xmm9,  %%xmm9        \n\t"
-		"movddup   %%xmm11, %%xmm10        \n\t"
-		"unpckhpd  %%xmm11, %%xmm11        \n\t"
-		"movddup   %%xmm13, %%xmm12        \n\t"
-		"unpckhpd  %%xmm13, %%xmm13        \n\t"
-		"movddup   %%xmm15, %%xmm14        \n\t"
-		"unpckhpd  %%xmm15, %%xmm15        \n\t"
-		"                                  \n\t"
-		"                                  \n\t"
-		"movapd   %%xmm0, -8 * 16(%%rbx)   \n\t"
-		"movapd   %%xmm1, -7 * 16(%%rbx)   \n\t"
-		"movapd   %%xmm2, -6 * 16(%%rbx)   \n\t"
-		"movapd   %%xmm3, -5 * 16(%%rbx)   \n\t"
-		"movapd   %%xmm4, -4 * 16(%%rbx)   \n\t"
-		"movapd   %%xmm5, -3 * 16(%%rbx)   \n\t"
-		"movapd   %%xmm6, -2 * 16(%%rbx)   \n\t"
-		"movapd   %%xmm7, -1 * 16(%%rbx)   \n\t"
-		"                                  \n\t"
-		"movapd   %%xmm8,  0 * 16(%%rbx)   \n\t"
-		"movapd   %%xmm9,  1 * 16(%%rbx)   \n\t"
-		"movapd  %%xmm10,  2 * 16(%%rbx)   \n\t"
-		"movapd  %%xmm11,  3 * 16(%%rbx)   \n\t"
-		"movapd  %%xmm12,  4 * 16(%%rbx)   \n\t"
-		"movapd  %%xmm13,  5 * 16(%%rbx)   \n\t"
-		"movapd  %%xmm14,  6 * 16(%%rbx)   \n\t"
-		"movapd  %%xmm15,  7 * 16(%%rbx)   \n\t"
-		"                                  \n\t"
-		"addq     $8 * 16, %%rax           \n\t" // b += 16;
-		"addq    $16 * 16, %%rbx           \n\t" // bd += 16*2;
-		"                                  \n\t"
-		"decq   %%rsi                      \n\t" // i -= 1;
-		"jne    .LOOPNITER                 \n\t" // iterate again if i != 0.
-		"                                  \n\t"
-		"                                  \n\t"
-		"                                  \n\t"
-		".CONSIDERNLEFT:                   \n\t"
-		"                                  \n\t"
-		"movq      %1, %%rsi               \n\t" // i = n_left;
-		"testq  %%rsi, %%rsi               \n\t" // check n_left via logical AND.
-		"je     .DONE                      \n\t" // if i == 0, we're done; jump to end.
-		"                                  \n\t" // else, we prepare to enter n_left loop.
-		"                                  \n\t"
-		"                                  \n\t"
-		".LOOPNLEFT:                       \n\t" // EDGE LOOP
-		"                                  \n\t"
-		"movddup  -8 * 16(%%rax), %%xmm0   \n\t"
-		"addq      $8, %%rax               \n\t" // b += 1;
-		"                                  \n\t"
-		"movapd   %%xmm0, -8 * 16(%%rbx)   \n\t"
-		"addq     $16, %%rbx               \n\t" // bd += 2;
-		"                                  \n\t"
-		"decq   %%rsi                      \n\t" // i -= 1;
-		"jne    .LOOPNLEFT                 \n\t" // iterate again if i != 0.
-		"                                  \n\t"
-		"                                  \n\t"
-		"                                  \n\t"
-		".DONE:                            \n\t"
-		"                                  \n\t"
-
-		: // output operands (none)
-		: // input operands
-		  "r" (n_iter),
-		  "r" (n_left),
-		  "m" (b),
-		  "m" (bd)
-		: // register clobber list
-		  "rax", "rbx", "rsi",
-		  "xmm0", "xmm1", "xmm2", "xmm3",
-		  "xmm4", "xmm5", "xmm6", "xmm7",
-		  "xmm8", "xmm9", "xmm10", "xmm11",
-		  "xmm12", "xmm13", "xmm14", "xmm15",
-		  "memory"
-	);
-	
-}
-
-void bli_cdupl_opt_var1(
-                         dim_t     k,
-                         scomplex* b,
-                         scomplex* bd
-                       )
-{
-	bli_check_error_code( BLIS_NOT_YET_IMPLEMENTED );
-}
-
-void bli_zdupl_opt_var1(
-                         dim_t     k,
-                         dcomplex* b,
-                         dcomplex* bd
-                       )
-{
-	bli_check_error_code( BLIS_NOT_YET_IMPLEMENTED );
-}
-

kernels/x86_64/3/bli_dupl_opt_var1.h
@@ -1,46 +1,0 @@
-/*
-
-   BLIS    
-   An object-based framework for developing high-performance BLAS-like
-   libraries.
-
-   Copyright (C) 2013, The University of Texas
-
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions are
-   met:
-    - Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    - Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    - Neither the name of The University of Texas nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-
-   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-   HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-   THEORY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-*/
-
-
-#undef  GENTPROT
-#define GENTPROT( ctype, ch, varname ) \
-\
-void PASTEMAC(ch,varname)( \
-                           dim_t   n_elem, \
-                           ctype*  b, \
-                           ctype*  bd \
-                         );
-
-INSERT_GENTPROT_BASIC( dupl_opt_var1 )
-

kernels/x86_64/3/bli_gemm_opt_d4x4.c
@@ -371,9 +371,9 @@ void bli_dgemm_opt_d4x4(
 		"                                \n\t"
 		"                                \n\t" // now avoid loading C if beta == 0
 		"                                \n\t"
-		//"xorpd     %%xmm0,  %%xmm0       \n\t" // set xmm0 to zero.
-		//"ucomisd   %%xmm0,  %%xmm7       \n\t" // check if beta == 0.
-		//"je      .BETAZERO               \n\t" // if ZF = 1, jump to beta == 0 case
+		"xorpd     %%xmm0,  %%xmm0       \n\t" // set xmm0 to zero.
+		"ucomisd   %%xmm0,  %%xmm7       \n\t" // check if beta == 0.
+		"je      .BETAZERO               \n\t" // if ZF = 1, jump to beta == 0 case
 		"                                \n\t"
 		"                                \n\t"
 		"                                \n\t" // check if aligned/column-stored