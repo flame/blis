flame/blis

commit 25b258d61f9c8cee64e922f4131784b6edb196dd

Fixed a non-fatal problem with bugfix in a68b316c.
Details:
- The bugfix in a68b316c was inadvertantly checkin alignment of the
  leading dimension itself, rather than the byte size of the leading
  dimension. Now, we simply check alignment of a+lda.

kernels/x86_64/core2-sse3/1f/bli_axpyf_opt_var1.c
@@ -92,7 +92,7 @@ void bli_daxpyf_opt_var1(
 		use_ref = TRUE;
 	}
 	else if ( inca != 1 || incx != 1 || incy != 1 ||
-	          bli_is_unaligned_to( lda, 16 ) )
+	          bli_is_unaligned_to( a+lda, 16 ) )
 	{
 		use_ref = TRUE;
 	}

kernels/x86_64/core2-sse3/1f/bli_dotxaxpyf_opt_var1.c
@@ -116,7 +116,7 @@ void bli_ddotxaxpyf_opt_var1( conj_t             conjat,
 		use_ref = TRUE;
 	}
     else if ( inca != 1 || incw != 1 || incx != 1 || incy != 1 || incz != 1 ||
-	          bli_is_unaligned_to( lda, 16 ) )
+	          bli_is_unaligned_to( a+lda, 16 ) )
     {
         use_ref = TRUE;
     }

kernels/x86_64/core2-sse3/1f/bli_dotxf_opt_var1.c
@@ -104,7 +104,7 @@ void bli_ddotxf_opt_var1(
 		use_ref = TRUE;
 	}
     else if ( inca != 1 || incx != 1 || incy != 1 ||
-	          bli_is_unaligned_to( lda, 16 ) )
+	          bli_is_unaligned_to( a+lda, 16 ) )
     {
         use_ref = TRUE;
     }

commit e80a4537846416719c067ae08a53aeda978c572d

Fixed bug introduced by bugfix in 25b258d.
Details:
- We actually need to check alignment of lda*sizeof(double) and NOT
  a+lda because in the latter case, alignment could cancel out and
  still allow the optimized code to run when it shouldn't. Thanks
  to Devin for pointing this out.

kernels/x86_64/core2-sse3/1f/bli_axpyf_opt_var1.c
@@ -92,7 +92,7 @@ void bli_daxpyf_opt_var1(
 		use_ref = TRUE;
 	}
 	else if ( inca != 1 || incx != 1 || incy != 1 ||
-	          bli_is_unaligned_to( a+lda, 16 ) )
+	          bli_is_unaligned_to( lda*sizeof(double), 16 ) )
 	{
 		use_ref = TRUE;
 	}

kernels/x86_64/core2-sse3/1f/bli_dotxaxpyf_opt_var1.c
@@ -116,7 +116,7 @@ void bli_ddotxaxpyf_opt_var1( conj_t             conjat,
 		use_ref = TRUE;
 	}
     else if ( inca != 1 || incw != 1 || incx != 1 || incy != 1 || incz != 1 ||
-	          bli_is_unaligned_to( a+lda, 16 ) )
+	          bli_is_unaligned_to( lda*sizeof(double), 16 ) )
     {
         use_ref = TRUE;
     }

kernels/x86_64/core2-sse3/1f/bli_dotxf_opt_var1.c
@@ -104,7 +104,7 @@ void bli_ddotxf_opt_var1(
 		use_ref = TRUE;
 	}
     else if ( inca != 1 || incx != 1 || incy != 1 ||
-	          bli_is_unaligned_to( a+lda, 16 ) )
+	          bli_is_unaligned_to( lda*sizeof(double), 16 ) )
     {
         use_ref = TRUE;
     }