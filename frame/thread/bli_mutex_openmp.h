flame/blis

commit a017062fdf763037da9d971a028bb07d47aa1c8a

Integrated "memory broker" (membrk_t) abstraction.
Details:
- Integrated a patch originally authored and submitted by Ricardo Magana
  of HP Enterprise. The changeset inserts use of a new object type, membrk_t,
  (memory broker) that allows multiple sets of memory pools on, for example,
  separate NUMA nodes, each of which has a separate memory space.
- Added membrk field to cntx_t and defined corresponding accessor macros.
- Added membrk field to mem_t object and defined corresponding accessor macros.
- Created new bli_membrk.c file, which contains the new memory broker API,
  including:
    bli_membrk_init(), bli_membrk_finalize()
    bli_membrk_acquire_[mv](), bli_membrk_release(),
    bli_membrk_init_pools(), bli_membrk_reinit_pools(),
    bli_membrk_finalize_pools(),
    bli_membrk_pool_size()
- In bli_mem.c, changed function calls to
    bli_mem_init_pools()     -> bli_membrk_init()
    bli_mem_reinit_pools()   -> bli_membrk_reinit()
    bli_mem_finalize_pools() -> bli_membrk_finalize()
- In bli_packv_init.c, bli_packm_init.c, changed function calls to:
    bli_mem_acquire_[mv]() -> bli_membrk_acquire_[mv]()
    bli_mem_release()      -> bli_membrk_release()
- Added bli_mutex.c and related files to frame/thread. These files define
  abstract mutexes (locks) and corresponding APIs for pthreads, openmp, or
  single-threaded execution. This new API is employed within functions
  such as bli_membrk_acquire_[mv]() and bli_membrk_release().

frame/thread/bli_mutex_openmp.h
@@ -33,21 +33,40 @@
 
 */
 
-#ifndef BLIS_MEM_H
-#define BLIS_MEM_H
+#ifndef BLIS_MUTEX_OPENMP_H
+#define BLIS_MUTEX_OPENMP_H
 
-// -----------------------------------------------------------------------------
+// Define mutex_t for situations when OpenMP multithreading is enabled.
+#ifdef BLIS_ENABLE_OPENMP
 
-membrk_t* bli_mem_global_membrk( void );
-siz_t     bli_mem_pool_size( packbuf_t buf_type );
+#include <omp.h>
 
-// -----------------------------------------------------------------------------
+// Define mtx_t.
+typedef struct mtx_s
+{
+	omp_lock_t mutex;
+} mtx_t;
 
-void   bli_mem_init( void );
-void   bli_mem_reinit( cntx_t* cntx );
-void   bli_mem_finalize( void );
-bool_t bli_mem_is_initialized( void );
+// Define macros to operate on OpenMP-based mtx_t.
+#define bli_mutex_init( mtx_p ) \
+{ \
+	omp_init_lock( mtx_p ); \
+}
+#define bli_mutex_finalize( mtx_p ) \
+{ \
+	omp_destroy_lock( mtx_p ); \
+}
 
+#define bli_mutex_lock( mtx_p ) \
+{ \
+	omp_set_lock( mtx_p ); \
+}
+#define bli_mutex_unlock( mtx_p ) \
+{ \
+	omp_unset_lock( mtx_p ); \
+}
+
+#endif
 
 #endif
 

frame/thread/bli_mutex_pthreads.h
@@ -33,21 +33,40 @@
 
 */
 
-#ifndef BLIS_MEM_H
-#define BLIS_MEM_H
+#ifndef BLIS_MUTEX_PTHREADS_H
+#define BLIS_MUTEX_PTHREADS_H
 
-// -----------------------------------------------------------------------------
+// Define mutex_t for situations when POSIX multithreading is enabled.
+#ifdef BLIS_ENABLE_PTHREADS
 
-membrk_t* bli_mem_global_membrk( void );
-siz_t     bli_mem_pool_size( packbuf_t buf_type );
+#include <pthread.h>
 
-// -----------------------------------------------------------------------------
+// Define mtx_t.
+typedef struct mtx_s
+{
+	pthread_mutex_t mutex;
+} mtx_t;
 
-void   bli_mem_init( void );
-void   bli_mem_reinit( cntx_t* cntx );
-void   bli_mem_finalize( void );
-bool_t bli_mem_is_initialized( void );
+// Define macros to operate on pthread-based mtx_t.
+#define bli_mutex_init( mtx_p ) \
+{ \
+	pthread_mutex_init( mtx_p ); \
+}
+#define bli_mutex_finalize( mtx_p ) \
+{ \
+	pthread_mutex_destroy( mtx_p ); \
+}
 
+#define bli_mutex_lock( mtx_p ) \
+{ \
+	pthread_mutex_lock( mtx_p ); \
+}
+#define bli_mutex_unlock( mtx_p ) \
+{ \
+	pthread_mutex_unlock( mtx_p ); \
+}
+
+#endif
 
 #endif
 

frame/1/packv/bli_packv_init.c
@@ -5,6 +5,7 @@
    libraries.
 
    Copyright (C) 2014, The University of Texas at Austin
+   Copyright (C) 2016 Hewlett Packard Enterprise Development LP
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -43,6 +44,7 @@ void bli_packv_init
      )
 {
 	// The purpose of packm_init() is to initialize an object P so that
+
 	// a source object A can be packed into P via one of the packv
 	// implementations. This initialization includes acquiring a suitable
 	// block of memory from the memory allocator, if such a block of memory
@@ -132,15 +134,17 @@ void bli_packv_init_pack
        cntx_t* cntx
      )
 {
-	num_t  dt    = bli_obj_datatype( *c );
-	dim_t  dim_c = bli_obj_vector_dim( *c );
-	dim_t  bmult = bli_cntx_get_blksz_def_dt( dt, bmult_id, cntx );
+	num_t     dt     = bli_obj_datatype( *c );
+	dim_t     dim_c  = bli_obj_vector_dim( *c );
+	dim_t     bmult  = bli_cntx_get_blksz_def_dt( dt, bmult_id, cntx );
 
-	mem_t* mem_p;
-	dim_t  m_p_pad;
-	siz_t  size_p;
-	inc_t  rs_p, cs_p;
-	void*  buf;
+	membrk_t* membrk = bli_cntx_membrk( cntx );
+
+	mem_t*    mem_p;
+	dim_t     m_p_pad;
+	siz_t     size_p;
+	inc_t     rs_p, cs_p;
+	void*     buf;
 
 
 	// We begin by copying the basic fields of c.
@@ -170,8 +174,9 @@ void bli_packv_init_pack
 	{
 		// If the mem_t object of p has not yet been allocated, then acquire
 		// a memory block suitable for a vector.
-		bli_mem_acquire_v( size_p,
-		                   mem_p );
+		bli_membrk_acquire_v( membrk,
+		                      size_p,
+		                      mem_p );
 	}
 	else
 	{
@@ -179,10 +184,11 @@ void bli_packv_init_pack
 		// re-acquire the memory so there is sufficient space.
 		if ( bli_mem_size( mem_p ) < size_p )
 		{
-			bli_mem_release( mem_p ); 
+			bli_membrk_release( mem_p );
 
-			bli_mem_acquire_v( size_p,
-			                   mem_p );
+			bli_membrk_acquire_v( membrk,
+			                      size_p,
+			                      mem_p );
 		}
 	}
 
@@ -218,7 +224,7 @@ void bli_packv_release
      )
 {
 	if ( !bli_cntl_is_noop( cntl ) )
-	    bli_obj_release_pack( p );
+		bli_obj_release_pack( p );
 }
 
 

frame/1m/packm/bli_packm_cntx.c
@@ -5,6 +5,7 @@
    libraries.
 
    Copyright (C) 2014, The University of Texas at Austin
+   Copyright (C) 2016 Hewlett Packard Enterprise Development LP
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -49,6 +50,9 @@ void bli_packm_cntx_init( cntx_t* cntx )
 	bli_gks_cntx_set_l1v_ker( BLIS_SCALV_KER, cntx );
 	bli_gks_cntx_set_l1v_ker( BLIS_SCAL2V_KER, cntx );
 	bli_gks_cntx_set_l1v_ker( BLIS_SETV_KER, cntx );
+
+	// Initialize the context with the global membrk object.
+	bli_cntx_set_membrk( bli_mem_global_membrk(), cntx );
 }
 
 void bli_packm_cntx_finalize( cntx_t* cntx )

frame/1m/packm/bli_packm_init.c
@@ -5,6 +5,7 @@
    libraries.
 
    Copyright (C) 2014, The University of Texas at Austin
+   Copyright (C) 2016 Hewlett Packard Enterprise Development LP
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -202,23 +203,25 @@ void bli_packm_init_pack( invdiag_t invert_diag,
                           obj_t*    p,
                           cntx_t*   cntx )
 {
-	num_t   dt           = bli_obj_datatype( *c );
-	trans_t transc       = bli_obj_onlytrans_status( *c );
-	dim_t   m_c          = bli_obj_length( *c );
-	dim_t   n_c          = bli_obj_width( *c );
-	dim_t   bmult_m_def  = bli_cntx_get_blksz_def_dt( dt, bmult_id_m, cntx );
-	dim_t   bmult_m_pack = bli_cntx_get_blksz_max_dt( dt, bmult_id_m, cntx );
-	dim_t   bmult_n_def  = bli_cntx_get_blksz_def_dt( dt, bmult_id_n, cntx );
-	dim_t   bmult_n_pack = bli_cntx_get_blksz_max_dt( dt, bmult_id_n, cntx );
+	num_t     dt           = bli_obj_datatype( *c );
+	trans_t   transc       = bli_obj_onlytrans_status( *c );
+	dim_t     m_c          = bli_obj_length( *c );
+	dim_t     n_c          = bli_obj_width( *c );
+	dim_t     bmult_m_def  = bli_cntx_get_blksz_def_dt( dt, bmult_id_m, cntx );
+	dim_t     bmult_m_pack = bli_cntx_get_blksz_max_dt( dt, bmult_id_m, cntx );
+	dim_t     bmult_n_def  = bli_cntx_get_blksz_def_dt( dt, bmult_id_n, cntx );
+	dim_t     bmult_n_pack = bli_cntx_get_blksz_max_dt( dt, bmult_id_n, cntx );
 
-	mem_t*  mem_p;
-	dim_t   m_p, n_p;
-	dim_t   m_p_pad, n_p_pad;
-	siz_t   size_p;
-	siz_t   elem_size_p;
-	inc_t   rs_p, cs_p;
-	inc_t   is_p;
-	void*   buf;
+	membrk_t* membrk       = bli_cntx_get_membrk( cntx );
+
+	mem_t*    mem_p;
+	dim_t     m_p, n_p;
+	dim_t     m_p_pad, n_p_pad;
+	siz_t     size_p;
+	siz_t     elem_size_p;
+	inc_t     rs_p, cs_p;
+	inc_t     is_p;
+	void*     buf;
 
 
 	// We begin by copying the basic fields of c. We do NOT copy the
@@ -549,9 +552,10 @@ void bli_packm_init_pack( invdiag_t invert_diag,
 	{
 		// If the mem_t object of p has not yet been allocated, then acquire
 		// a memory block of type pack_buf_type.
-		bli_mem_acquire_m( size_p,
-		                   pack_buf_type,
-		                   mem_p );
+		bli_membrk_acquire_m( membrk,
+		                      size_p,
+		                      pack_buf_type,
+		                      mem_p );
 	}
 	else
 	{
@@ -562,10 +566,11 @@ void bli_packm_init_pack( invdiag_t invert_diag,
 		// pack_buf_type value.
 		if ( bli_mem_size( mem_p ) < size_p )
 		{
-			bli_mem_release( mem_p );
-			bli_mem_acquire_m( size_p,
-			                   pack_buf_type,
-			                   mem_p );
+			bli_membrk_release( mem_p );
+			bli_membrk_acquire_m( membrk,
+			                      size_p,
+			                      pack_buf_type,
+			                      mem_p );
 		}
 	}
 
@@ -582,7 +587,7 @@ void bli_packm_release( obj_t*   p,
                         packm_t* cntl )
 {
 	if ( !bli_cntl_is_noop( cntl ) )
-	    bli_obj_release_pack( p );
+		bli_obj_release_pack( p );
 }
 
 

frame/base/bli_cntx.h
@@ -5,6 +5,7 @@
    libraries.
 
    Copyright (C) 2014, The University of Texas at Austin
+   Copyright (C) 2016 Hewlett Packard Enterprise Development LP
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -57,6 +58,7 @@ typedef struct cntx_s
 	pack_t    schema_b;
 	pack_t    schema_c;
 
+	membrk_t* membrk;
 } cntx_t;
 */
 
@@ -116,66 +118,75 @@ typedef struct cntx_s
 \
 	( (cntx)->schema_c )
 
+#define bli_cntx_membrk( cntx ) \
+\
+	( (cntx)->membrk )
+
 // cntx_t modification (fields only)
 
 #define bli_cntx_set_blkszs_buf( _blkszs, cntx_p ) \
 { \
-    (cntx_p)->blkszs = _blkszs; \
+	(cntx_p)->blkszs = _blkszs; \
 }
 
 #define bli_cntx_set_bmults_buf( _bmults, cntx_p ) \
 { \
-    (cntx_p)->bmults = _bmults; \
+	(cntx_p)->bmults = _bmults; \
 }
 
 #define bli_cntx_set_l3_vir_ukrs_buf( _l3_vir_ukrs, cntx_p ) \
 { \
-    (cntx_p)->l3_vir_ukrs = _l3_vir_ukrs; \
+	(cntx_p)->l3_vir_ukrs = _l3_vir_ukrs; \
 }
 
 #define bli_cntx_set_l3_nat_ukrs_buf( _l3_nat_ukrs, cntx_p ) \
 { \
-    (cntx_p)->l3_nat_ukrs = _l3_nat_ukrs; \
+	(cntx_p)->l3_nat_ukrs = _l3_nat_ukrs; \
 }
 
 #define bli_cntx_set_l3_nat_ukrs_prefs_buf( _l3_nat_ukrs_prefs, cntx_p ) \
 { \
-    (cntx_p)->l3_nat_ukrs_prefs = _l3_nat_ukrs_prefs; \
+	(cntx_p)->l3_nat_ukrs_prefs = _l3_nat_ukrs_prefs; \
 }
 
 #define bli_cntx_set_l1f_kers_buf( _l1f_kers, cntx_p ) \
 { \
-    (cntx_p)->l1f_kers = _l1f_kers; \
+	(cntx_p)->l1f_kers = _l1f_kers; \
 }
 
 #define bli_cntx_set_l1v_kers_buf( _l1v_kers, cntx_p ) \
 { \
-    (cntx_p)->l1v_kers = _l1v_kers; \
+	(cntx_p)->l1v_kers = _l1v_kers; \
 }
 
 #define bli_cntx_set_packm_ukrs( _packm_ukrs, cntx_p ) \
 { \
-    (cntx_p)->packm_ukrs = _packm_ukrs; \
+	(cntx_p)->packm_ukrs = _packm_ukrs; \
 }
 
 #define bli_cntx_set_method( _method, cntx_p ) \
 { \
-    (cntx_p)->method = _method; \
+	(cntx_p)->method = _method; \
 }
 
 #define bli_cntx_set_schema_a( _schema_a, cntx_p ) \
 { \
-    (cntx_p)->schema_a = _schema_a; \
+	(cntx_p)->schema_a = _schema_a; \
 }
 
 #define bli_cntx_set_schema_b( _schema_b, cntx_p ) \
 { \
-    (cntx_p)->schema_b = _schema_b; \
+	(cntx_p)->schema_b = _schema_b; \
 }
 
 #define bli_cntx_set_schema_c( _schema_c, cntx_p ) \
 { \
-    (cntx_p)->schema_c = _schema_c; \
+	(cntx_p)->schema_c = _schema_c; \
+}
+
+#define bli_cntx_set_membrk( _membrk, cntx_p ) \
+{ \
+	(cntx_p)->membrk = _membrk; \
 }
 
 // cntx_t query (complex)
@@ -263,6 +274,11 @@ typedef struct cntx_s
 #define bli_cntx_get_pack_schema_b( cntx ) \
 \
 	bli_cntx_schema_b( cntx )
+
+#define bli_cntx_get_membrk( cntx ) \
+\
+	bli_cntx_membrk( cntx )
+
 
 
 

commit 16a4c7a823d60707ed9272f5d36e5c5d54c0ba4b

Fixed bugs in bli_mutex_init() and friends.
Details:
- Fixed a couple of bugs that affected OpenMP and POSIX threads
  configurations that resulted in compiler errors and warnings due
  to type mismatch, and in the case of pthreads, a missing function
  argument. The bugs are fairly recent, introduced in a017062.

frame/thread/bli_mutex_openmp.h
@@ -50,20 +50,20 @@ typedef struct mtx_s
 // Define macros to operate on OpenMP-based mtx_t.
 #define bli_mutex_init( mtx_p ) \
 { \
-	omp_init_lock( mtx_p ); \
+	omp_init_lock( &((mtx_p)->mutex) ); \
 }
 #define bli_mutex_finalize( mtx_p ) \
 { \
-	omp_destroy_lock( mtx_p ); \
+	omp_destroy_lock( &((mtx_p)->mutex) ); \
 }
 
 #define bli_mutex_lock( mtx_p ) \
 { \
-	omp_set_lock( mtx_p ); \
+	omp_set_lock( &((mtx_p)->mutex) ); \
 }
 #define bli_mutex_unlock( mtx_p ) \
 { \
-	omp_unset_lock( mtx_p ); \
+	omp_unset_lock( &((mtx_p)->mutex) ); \
 }
 
 #endif

frame/thread/bli_mutex_pthreads.h
@@ -50,20 +50,20 @@ typedef struct mtx_s
 // Define macros to operate on pthread-based mtx_t.
 #define bli_mutex_init( mtx_p ) \
 { \
-	pthread_mutex_init( mtx_p ); \
+	pthread_mutex_init( &((mtx_p)->mutex), NULL ); \
 }
 #define bli_mutex_finalize( mtx_p ) \
 { \
-	pthread_mutex_destroy( mtx_p ); \
+	pthread_mutex_destroy( &((mtx_p)->mutex) ); \
 }
 
 #define bli_mutex_lock( mtx_p ) \
 { \
-	pthread_mutex_lock( mtx_p ); \
+	pthread_mutex_lock( &((mtx_p)->mutex) ); \
 }
 #define bli_mutex_unlock( mtx_p ) \
 { \
-	pthread_mutex_unlock( mtx_p ); \
+	pthread_mutex_unlock( &((mtx_p)->mutex) ); \
 }
 
 #endif