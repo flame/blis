flame/blis

commit 1c732d3ddc4ac0861d3b0e0dd15eb7e071615502

Added 1m-specific APIs for bp, pb gemm algorithms.
Details:
- Defined bli_gemmbp_cntl_create(), bli_gemmpb_cntl_create(), with the
  body of bli_gemm_cntl_create() replaced with a call to the former.
- Defined bli_cntl_free_w_thrinfo(), bli_cntl_free_wo_thrinfo(). Now,
  bli_cntl_free() can check if the thread parameter is NULL, and if so,
  call the latter, and otherwise call the former.
- Defined bli_gemm1mbp_cntx_init(), bli_gemm1mpb_cntx_init(), both in
  terms of bli_gemm1mxx_cntx_init(), which behaves the same as
  bli_gemm1m_cntx_init() did before, except that an extra bool parameter
  (is_pb) is used to support both bp and pb algorithms (including to
  support the anti-preference field described below).
- Added support for "anti-preference" in context. The anti_pref field,
  when true, will toggle the boolean return value of routines such as
  bli_cntx_l3_ukr_eff_prefers_storage_of(), which has the net effect of
  causing BLIS to transpose the operation to achieve disagreement (rather
  than agreement) between the storage of C and the micro-kernel output
  preference. This disagreement is needed for panel-block implementations,
  since they induce a transposition of the suboperation immediately before
  the macro-kernel is called, which changes the apparent storage of C. For
  now, anti-preference is used only with the pb algorithm for 1m (and not
  with any other non-1m implementation).
- Defined new functions,
    bli_cntx_l3_ukr_eff_prefers_storage_of()
    bli_cntx_l3_ukr_eff_dislikes_storage_of()
    bli_cntx_l3_nat_ukr_eff_prefers_storage_of()
    bli_cntx_l3_nat_ukr_eff_dislikes_storage_of()
  which are identical to their non-"eff" (effectively) counterparts except
  that they take the anti-preference field of the context into account.
- Explicitly initialize the anti-pref field to FALSE in
  bli_gks_cntx_set_l3_nat_ukr_prefs().
- Added bli_gemm_ker_var1.c, which implements a panel-block macro-kernel

frame/ind/cntx/bli_trsmind_cntx.c
@@ -74,9 +74,9 @@ void bli_trsm3m1_cntx_init( num_t dt, cntx_t* cntx )
 	);
 
 	// Set the pack_t schemas for native execution.
-	bli_cntx_set_pack_schema_ab( BLIS_PACKED_ROW_PANELS_3MI,
-	                             BLIS_PACKED_COL_PANELS_3MI,
-	                             cntx );
+	bli_cntx_set_pack_schema_ab_blockpanel( BLIS_PACKED_ROW_PANELS_3MI,
+	                                        BLIS_PACKED_COL_PANELS_3MI,
+	                                        cntx );
 }
 
 void bli_trsm3m1_cntx_finalize( cntx_t* cntx )
@@ -123,9 +123,9 @@ void bli_trsm4m1_cntx_init( num_t dt, cntx_t* cntx )
 	);
 
 	// Set the pack_t schemas for native execution.
-	bli_cntx_set_pack_schema_ab( BLIS_PACKED_ROW_PANELS_4MI,
-	                             BLIS_PACKED_COL_PANELS_4MI,
-	                             cntx );
+	bli_cntx_set_pack_schema_ab_blockpanel( BLIS_PACKED_ROW_PANELS_4MI,
+	                                        BLIS_PACKED_COL_PANELS_4MI,
+	                                        cntx );
 }
 
 void bli_trsm4m1_cntx_finalize( cntx_t* cntx )
@@ -174,9 +174,9 @@ void bli_trsm1m_cntx_init( num_t dt, cntx_t* cntx )
 		);
 
 		// Set the pack_t schemas for the current induced method.
-		bli_cntx_set_pack_schema_ab( BLIS_PACKED_ROW_PANELS_1E,
-		                             BLIS_PACKED_COL_PANELS_1R,
-		                             cntx );
+		bli_cntx_set_pack_schema_ab_blockpanel( BLIS_PACKED_ROW_PANELS_1E,
+		                                        BLIS_PACKED_COL_PANELS_1R,
+		                                        cntx );
 	}
 	else // if ( bli_cntx_l3_ukr_prefers_rows_dt( dt, BLIS_GEMM_UKR, cntx ) )
 	{
@@ -195,9 +195,9 @@ void bli_trsm1m_cntx_init( num_t dt, cntx_t* cntx )
 		);
 
 		// Set the pack_t schemas for the current induced method.
-		bli_cntx_set_pack_schema_ab( BLIS_PACKED_ROW_PANELS_1R,
-		                             BLIS_PACKED_COL_PANELS_1E,
-		                             cntx );
+		bli_cntx_set_pack_schema_ab_blockpanel( BLIS_PACKED_ROW_PANELS_1R,
+		                                        BLIS_PACKED_COL_PANELS_1E,
+		                                        cntx );
 	}
 }
 

frame/1m/packm/bli_packm_init.c
@@ -121,11 +121,11 @@ siz_t bli_packm_init
 
 	if ( pack_buf_type == BLIS_BUFFER_FOR_A_BLOCK )
 	{
-		schema = bli_cntx_get_pack_schema_a( cntx );
+		schema = bli_cntx_get_pack_schema_a_block( cntx );
 	}
 	else if ( pack_buf_type == BLIS_BUFFER_FOR_B_PANEL )
 	{
-		schema = bli_cntx_get_pack_schema_b( cntx );
+		schema = bli_cntx_get_pack_schema_b_panel( cntx );
 	}
 	else // if ( pack_buf_type == BLIS_BUFFER_FOR_C_PANEL )
 	{

frame/3/bli_l3_cntx.c
@@ -63,9 +63,8 @@ void bli_gemm_cntx_init( num_t dt, cntx_t* cntx )
 	                         cntx );
 
 	// Set the pack_t schemas for native execution.
-	bli_cntx_set_pack_schema_ab( BLIS_PACKED_ROW_PANELS,
-	                             BLIS_PACKED_COL_PANELS,
-	                             cntx );
+	bli_cntx_set_pack_schema_a_block( BLIS_PACKED_ROW_PANELS, cntx );
+	bli_cntx_set_pack_schema_b_panel( BLIS_PACKED_COL_PANELS, cntx );
 }
 
 void bli_gemm_cntx_finalize( cntx_t* cntx )
@@ -106,9 +105,8 @@ void bli_trsm_cntx_init( num_t dt, cntx_t* cntx )
 	                         cntx );
 
 	// Set the pack_t schemas for native execution.
-	bli_cntx_set_pack_schema_ab( BLIS_PACKED_ROW_PANELS,
-	                             BLIS_PACKED_COL_PANELS,
-	                             cntx );
+	bli_cntx_set_pack_schema_a_block( BLIS_PACKED_ROW_PANELS, cntx );
+	bli_cntx_set_pack_schema_b_panel( BLIS_PACKED_COL_PANELS, cntx );
 }
 
 void bli_trsm_cntx_finalize( cntx_t* cntx )

frame/3/gemm/bli_gemm_cntl.c
@@ -39,8 +39,17 @@ cntl_t* bli_gemm_cntl_create
        opid_t family
      )
 {
-	void* macro_kernel_p = bli_gemm_ker_var2;
+	return bli_gemmbp_cntl_create( family );
+}
 
+// -----------------------------------------------------------------------------
+
+cntl_t* bli_gemmbp_cntl_create
+     (
+       opid_t family
+     )
+{
+	void* macro_kernel_p = bli_gemm_ker_var2;
 
 	// Change the macro-kernel if the operation family is herk or trmm.
 	if      ( family == BLIS_HERK ) macro_kernel_p = bli_herk_x_ker_var2;
@@ -64,7 +73,7 @@ cntl_t* bli_gemm_cntl_create
 	// Create a node for packing matrix A.
 	cntl_t* gemm_cntl_packa = bli_packm_cntl_obj_create
 	(
-	  bli_gemm_packa,
+	  bli_gemm_packa,  // pack the left-hand operand
 	  bli_packm_blk_var1,
 	  BLIS_MR,
 	  BLIS_KR,
@@ -87,7 +96,7 @@ cntl_t* bli_gemm_cntl_create
 	// Create a node for packing matrix B.
 	cntl_t* gemm_cntl_packb = bli_packm_cntl_obj_create
 	(
-	  bli_gemm_packb,
+	  bli_gemm_packb,  // pack the right-hand operand
 	  bli_packm_blk_var1,
 	  BLIS_KR,
 	  BLIS_NR,
@@ -117,6 +126,95 @@ cntl_t* bli_gemm_cntl_create
 
 	return gemm_cntl_vl_mm;
 }
+
+// -----------------------------------------------------------------------------
+
+cntl_t* bli_gemmpb_cntl_create
+     (
+       opid_t family
+     )
+{
+	void* macro_kernel_p = bli_gemm_ker_var1;
+
+	// Change the macro-kernel if the operation family is herk or trmm.
+	//if      ( family == BLIS_HERK ) macro_kernel_p = bli_herk_x_ker_var2;
+	//else if ( family == BLIS_TRMM ) macro_kernel_p = bli_trmm_xx_ker_var2;
+
+	// Create two nodes for the macro-kernel.
+	cntl_t* gemm_cntl_ub_ke = bli_gemm_cntl_obj_create
+	(
+	  BLIS_MR, // needed for bli_thrinfo_rgrow()
+	  NULL,    // variant function pointer not used
+	  NULL     // no sub-node; this is the leaf of the tree.
+	);
+
+	cntl_t* gemm_cntl_pb_ub = bli_gemm_cntl_obj_create
+	(
+	  BLIS_NR, // not used by macro-kernel, but needed for bli_thrinfo_rgrow()
+	  macro_kernel_p,
+	  gemm_cntl_ub_ke
+	);
+
+	// Create a node for packing matrix A (which is really the right-hand
+	// operand "B").
+	cntl_t* gemm_cntl_packb = bli_packm_cntl_obj_create
+	(
+	  bli_gemm_packb,  // pack the right-hand operand
+	  bli_packm_blk_var1,
+	  BLIS_KR,
+	  BLIS_MR,
+	  FALSE,   // do NOT invert diagonal
+	  FALSE,   // reverse iteration if upper?
+	  FALSE,   // reverse iteration if lower?
+	  BLIS_PACKED_COL_PANELS,
+	  BLIS_BUFFER_FOR_A_BLOCK,
+	  gemm_cntl_pb_ub
+	);
+
+	// Create a node for partitioning the n dimension by MC.
+	cntl_t* gemm_cntl_op_pb = bli_gemm_cntl_obj_create
+	(
+	  BLIS_MC,
+	  bli_gemm_blk_var2,
+	  gemm_cntl_packb
+	);
+
+	// Create a node for packing matrix B (which is really the left-hand
+	// operand "A").
+	cntl_t* gemm_cntl_packa = bli_packm_cntl_obj_create
+	(
+	  bli_gemm_packa,  // pack the left-hand operand
+	  bli_packm_blk_var1,
+	  BLIS_NR,
+	  BLIS_KR,
+	  FALSE,   // do NOT invert diagonal
+	  FALSE,   // reverse iteration if upper?
+	  FALSE,   // reverse iteration if lower?
+	  BLIS_PACKED_ROW_PANELS,
+	  BLIS_BUFFER_FOR_B_PANEL,
+	  gemm_cntl_op_pb
+	);
+
+	// Create a node for partitioning the k dimension by KC.
+	cntl_t* gemm_cntl_mm_op = bli_gemm_cntl_obj_create
+	(
+	  BLIS_KC,
+	  bli_gemm_blk_var3,
+	  gemm_cntl_packa
+	);
+
+	// Create a node for partitioning the m dimension by NC.
+	cntl_t* gemm_cntl_vl_mm = bli_gemm_cntl_obj_create
+	(
+	  BLIS_NC,
+	  bli_gemm_blk_var1,
+	  gemm_cntl_mm_op
+	);
+
+	return gemm_cntl_vl_mm;
+}
+
+// -----------------------------------------------------------------------------
 
 void bli_gemm_cntl_free
      (

frame/3/gemm/bli_gemm_cntl.h
@@ -37,6 +37,20 @@ cntl_t* bli_gemm_cntl_create
        opid_t family
      );
 
+// -----------------------------------------------------------------------------
+
+cntl_t* bli_gemmbp_cntl_create
+     (
+       opid_t family
+     );
+
+cntl_t* bli_gemmpb_cntl_create
+     (
+       opid_t family
+     );
+
+// -----------------------------------------------------------------------------
+
 void bli_gemm_cntl_free
      (
        cntl_t* cntl,

frame/3/gemm/bli_gemm_front.c
@@ -73,7 +73,7 @@ void bli_gemm_front
 	// contiguous columns, or if C is stored by columns and the micro-kernel
 	// prefers contiguous rows, transpose the entire operation to allow the
 	// micro-kernel to access elements of C in its preferred manner.
-	if ( bli_cntx_l3_ukr_dislikes_storage_of( &c_local, BLIS_GEMM_UKR, cntx ) )
+	if ( bli_cntx_l3_ukr_eff_dislikes_storage_of( &c_local, BLIS_GEMM_UKR, cntx ) )
 	{
 		bli_obj_swap( a_local, b_local );
 

frame/3/gemm/bli_gemm_ker_var1.c
@@ -34,81 +34,23 @@
 
 #include "blis.h"
 
-
-void bli_l3_cntl_create_if
-     (
-       obj_t*   a,
-       obj_t*   b,
-       obj_t*   c,
-       cntx_t*  cntx,
-       cntl_t*  cntl_orig,
-       cntl_t** cntl_use
-     )
-{
-	// If the control tree pointer is NULL, we construct a default
-	// tree as a function of the operation family.
-	if ( cntl_orig == NULL )
-	{
-		opid_t family = bli_cntx_get_family( cntx );
-
-		if ( family == BLIS_GEMM ||
-		     family == BLIS_HERK ||
-		     family == BLIS_TRMM )
-		{
-			*cntl_use = bli_gemm_cntl_create( family );
-		}
-		else // if ( family == BLIS_TRSM )
-		{
-			side_t side;
-
-			if ( bli_obj_is_triangular( *a ) ) side = BLIS_LEFT;
-			else                               side = BLIS_RIGHT;
-
-			*cntl_use = bli_trsm_cntl_create( side );
-		}
-	}
-	else
-	{
-		// If the user provided a control tree, create a copy and use it
-		// instead (so that threads can use its local tree as a place to
-		// cache things like pack mem_t entries).
-		*cntl_use = bli_cntl_copy( cntl_orig );
-	}
-}
-
-void bli_l3_cntl_free_if
+void bli_gemm_ker_var1
      (
        obj_t*  a,
        obj_t*  b,
        obj_t*  c,
        cntx_t* cntx,
-       cntl_t* cntl_orig,
-       cntl_t* cntl_use,
+       cntl_t* cntl,
        thrinfo_t* thread
      )
 {
-	// If the control tree pointer is NULL, a default tree would have
-	// been created, so we now must free it.
-	if ( cntl_orig == NULL )
-	{
-		opid_t family = bli_cntx_get_family( cntx );
+	// Implement _ker_var1() in terms of _ker_var2() by transposing the
+	// entire suboperation (which also requires swapping A and B).
 
-		if ( family == BLIS_GEMM ||
-		     family == BLIS_HERK ||
-		     family == BLIS_TRMM )
-		{
-			bli_gemm_cntl_free( cntl_use, thread );
-		}
-		else // if ( family == BLIS_TRSM )
-		{
-			bli_trsm_cntl_free( cntl_use, thread );
-		}
-	}
-	else
-	{
-		// If the user provided a control tree, free the copy of it that
-		// was created.
-		bli_cntl_free( cntl_use, thread );
-	}
+	bli_obj_induce_trans( *a );
+	bli_obj_induce_trans( *b );
+	bli_obj_induce_trans( *c );
+
+	bli_gemm_ker_var2( b, a, c, cntx, cntl, thread );
 }
 

frame/3/gemm/bli_gemm_var.h
@@ -56,6 +56,7 @@ GENPROT( gemm_blk_var3 )
 GENPROT( gemm_packa )
 GENPROT( gemm_packb )
 
+GENPROT( gemm_ker_var1 )
 GENPROT( gemm_ker_var2 )
 
 // Headers for induced algorithms:

commit d5a5e003ea9b24bb6abf12e88862e8eb61ffb03d

Fixed a trsm1m bug that affected right-side cases.
Details:
- Fixed a bug introduced in 1c732d3 that affected trsm1m_r. The result
  was nondeterministic behavior (usually segmentation faults) for certain
  problem sizes beyond the 1m instance of kc (e.g. 128 on haswell). The
  cause of the bug was my commenting out lines in bli_gemm1m_ukr_ref.c
  which explicitly directed the virtual gemm micro-kernel to use temporary
  space if the storage preference of the [real domain] gemm ukernel did
  not match the storage of the output matrix C. In the context of gemm,
  this handling is not needed because agreement between the storage pref
  and the matrix is guaranteed by a high-level optimization in BLIS.
  However, this optimization is not applied to trsm because the storage
  of C is not necessarily the same as the storage of the micro-panels of
  B--both of which are updated by the micro-kernel during a trsm
  operation. Thus, the guarantee of storage/preference agreement is not
  in place for trsm, which means we must handle that case within the
  virtual gemm micro-kernel.
- Comment updates and a minor macro change to bli_trsm*_cntx_init() for
  3m1, 4m1a, and 1m.

frame/ind/cntx/bli_trsmind_cntx.c
@@ -73,10 +73,9 @@ void bli_trsm3m1_cntx_init( num_t dt, cntx_t* cntx )
 	  cntx
 	);
 
-	// Set the pack_t schemas for native execution.
-	bli_cntx_set_pack_schema_ab_blockpanel( BLIS_PACKED_ROW_PANELS_3MI,
-	                                        BLIS_PACKED_COL_PANELS_3MI,
-	                                        cntx );
+	// Set the pack_t schemas for the current induced method.
+	bli_cntx_set_pack_schema_a_block( BLIS_PACKED_ROW_PANELS_3MI, cntx );
+	bli_cntx_set_pack_schema_b_panel( BLIS_PACKED_COL_PANELS_3MI, cntx );
 }
 
 void bli_trsm3m1_cntx_finalize( cntx_t* cntx )
@@ -122,10 +121,9 @@ void bli_trsm4m1_cntx_init( num_t dt, cntx_t* cntx )
 	  cntx
 	);
 
-	// Set the pack_t schemas for native execution.
-	bli_cntx_set_pack_schema_ab_blockpanel( BLIS_PACKED_ROW_PANELS_4MI,
-	                                        BLIS_PACKED_COL_PANELS_4MI,
-	                                        cntx );
+	// Set the pack_t schemas for the current induced method.
+	bli_cntx_set_pack_schema_a_block( BLIS_PACKED_ROW_PANELS_4MI, cntx );
+	bli_cntx_set_pack_schema_b_panel( BLIS_PACKED_COL_PANELS_4MI, cntx );
 }
 
 void bli_trsm4m1_cntx_finalize( cntx_t* cntx )
@@ -174,9 +172,11 @@ void bli_trsm1m_cntx_init( num_t dt, cntx_t* cntx )
 		);
 
 		// Set the pack_t schemas for the current induced method.
-		bli_cntx_set_pack_schema_ab_blockpanel( BLIS_PACKED_ROW_PANELS_1E,
-		                                        BLIS_PACKED_COL_PANELS_1R,
-		                                        cntx );
+		//bli_cntx_set_pack_schema_ab_blockpanel( BLIS_PACKED_ROW_PANELS_1E,
+		//                                        BLIS_PACKED_COL_PANELS_1R,
+		//                                        cntx );
+		bli_cntx_set_pack_schema_a_block( BLIS_PACKED_ROW_PANELS_1E, cntx );
+		bli_cntx_set_pack_schema_b_panel( BLIS_PACKED_COL_PANELS_1R, cntx );
 	}
 	else // if ( bli_cntx_l3_ukr_prefers_rows_dt( dt, BLIS_GEMM_UKR, cntx ) )
 	{
@@ -195,9 +195,11 @@ void bli_trsm1m_cntx_init( num_t dt, cntx_t* cntx )
 		);
 
 		// Set the pack_t schemas for the current induced method.
-		bli_cntx_set_pack_schema_ab_blockpanel( BLIS_PACKED_ROW_PANELS_1R,
-		                                        BLIS_PACKED_COL_PANELS_1E,
-		                                        cntx );
+		//bli_cntx_set_pack_schema_ab_blockpanel( BLIS_PACKED_ROW_PANELS_1R,
+		//                                        BLIS_PACKED_COL_PANELS_1E,
+		//                                        cntx );
+		bli_cntx_set_pack_schema_a_block( BLIS_PACKED_ROW_PANELS_1R, cntx );
+		bli_cntx_set_pack_schema_b_panel( BLIS_PACKED_COL_PANELS_1E, cntx );
 	}
 }
 

frame/ind/ukernels/gemm/bli_gemm1m_ukr_ref.c
@@ -55,7 +55,7 @@ void PASTEMAC(ch,varname) \
 	PASTECH(chr,gemm_ukr_ft) \
 	                  rgemm_ukr = bli_cntx_get_l3_nat_ukr_dt( dt_r, gemmkerid, cntx ); \
 	const bool_t      col_pref  = bli_cntx_l3_ukr_prefers_cols_dt( dt, BLIS_GEMM_UKR, cntx ); \
-	/*const bool_t      row_pref  = !col_pref;*/ \
+	const bool_t      row_pref  = !col_pref; \
 \
 	const dim_t       mr        = bli_cntx_get_blksz_def_dt( dt, BLIS_MR, cntx ); \
 	const dim_t       nr        = bli_cntx_get_blksz_def_dt( dt, BLIS_NR, cntx ); \
@@ -95,23 +95,18 @@ void PASTEMAC(ch,varname) \
 		bli_check_error_code( BLIS_NOT_YET_IMPLEMENTED ); \
 \
 \
-	/* Sanity check: These should never occur because storage/preference
-	   agreement is handled at a higher level. */ \
-	/*
-	if      ( bli_is_col_stored( rs_c, cs_c ) && row_pref ) bli_abort(); \
-	else if ( bli_is_row_stored( rs_c, cs_c ) && col_pref ) bli_abort(); \
-	*/ \
-\
-\
 	/* If beta has a non-zero imaginary component OR if c is stored with
 	   general stride, then we compute the alpha*a*b product into temporary
 	   storage and then accumulate that result into c afterwards. Note that
 	   the other two cases concerning disagreement between the storage of C
-	   and the output preference of the micro-kernel, should never occur
-	   (though we could handle them if they did occur). */ \
+	   and the output preference of the micro-kernel, should ONLY occur in
+	   the context of trsm, whereby this virtual micro-kernel is called
+	   directly from the trsm macro-kernel to update the micro-tile b11
+	   that exists within the packed row-panel of B. Indeed that is the
+	   reason those cases MUST be explicitly handled. */ \
 	if      ( !PASTEMAC(chr,eq0)( *beta_i ) )               using_ct = TRUE; \
-	/*else if ( bli_is_col_stored( rs_c, cs_c ) && row_pref ) using_ct = TRUE; \
-	else if ( bli_is_row_stored( rs_c, cs_c ) && col_pref ) using_ct = TRUE;*/ \
+	else if ( bli_is_col_stored( rs_c, cs_c ) && row_pref ) using_ct = TRUE; \
+	else if ( bli_is_row_stored( rs_c, cs_c ) && col_pref ) using_ct = TRUE; \
 	else if ( bli_is_gen_stored( rs_c, cs_c ) )             using_ct = TRUE; \
 	else                                                    using_ct = FALSE; \
 \