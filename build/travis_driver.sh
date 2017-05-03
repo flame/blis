38e101ce91872300a4d8c52b9deef10a7b37039f
Jeff Hammond <jeff.science@gmail.com>
2017-05-02 21:00:52 -0700

    Travis support for SDE
    - it is hard to do all the branching required for this in the .travis.yml
      so we drop into travis_driver.sh and handle everything.
    - detect hardware and run tests natively when possible
    - only use -d sde for KNL (requires merge w/ not-real-knl branch)
    - add SDE download+install
    - compile tests outside of SDE
    - support 32-bit Travis (unlikely?)

---
+++ build/travis_driver.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+CC="$1"
+THREADING="$2"
+TARGET="$3"
+RUN_TEST="$4"
+
+export BLIS_IC_NT=2
+export BLIS_JC_NT=1
+export BLIS_IR_NT=1
+export BLIS_JR_NT=1
+
+HARDWARE=0
+SDE_OPTIONS="false"
+case "$TARGET" in
+    knl)
+        if $(grep avx512f -q /proc/cpuinfo) && $(grep avx512pf -q /proc/cpuinfo) ; then HARDWARE=1 ; fi
+        SDE_OPTIONS="-knl"
+        ;;
+    skx)
+        if $(grep avx512f -q /proc/cpuinfo) && $(grep avx512vl -q /proc/cpuinfo) && $(grep avx512dq -q /proc/cpuinfo) ; then HARDWARE=1 ; fi
+        SDE_OPTIONS="-skx"
+        ;;
+    haswell)
+        if $(grep avx2 -q /proc/cpuinfo) ; then HARDWARE=1 ; fi
+        SDE_OPTIONS="-hsw"
+        ;;
+    sandybridge)
+        if $(grep avx -q /proc/cpuinfo) ; then HARDWARE=1 ; fi
+        SDE_OPTIONS="-snb"
+        ;;
+    dunnington)
+        if $(grep ssse3 -q /proc/cpuinfo) && $(grep sse4_1 -q /proc/cpuinfo) ; then HARDWARE=1 ; fi
+        SDE_OPTIONS="-pnr"
+        ;;
+    auto)
+        HARDWARE=1
+        ;;
+    reference)
+        HARDWARE=1
+        ;;
+    *)
+        ;;
+esac
+
+if [ "x$TARGET" == "xknl" ] ; then
+    ./configure -d sde -t $THREADING CC=$CC $TARGET
+else
+    ./configure        -t $THREADING CC=$CC $TARGET
+fi
+
+make
+
+if [ "x${RUN_TEST}" == "x1" ] ; then
+    make testsuite-bin
+    # We make no attempt to run SDE_OPTIONS on Mac.  It is supported but requires elevated permissions.
+    if [ "x${HARDWARE}" != "x1" ] && [ "${TRAVIS_OS_NAME}" == "linux" ] ; then
+        wget -q ${SDE_LOCATION}
+        tar -xaf sde-external-7.58.0-2017-01-23-lin.tar.bz2
+        export PATH=${PWD}/sde-external-7.58.0-2017-01-23-lin:${PATH}
+        if [ `uname -m` = x86_64 ] ; then SDE_BIN=sde64 ; else SDE_BIN=sde ; fi
+        ${PWD}/sde-external-7.58.0-2017-01-23-lin/${SDE_BIN} ${SDE_OPTIONS} -- make BLIS_ENABLE_TEST_OUTPUT=yes testsuite-run
+    else
+        make BLIS_ENABLE_TEST_OUTPUT=yes testsuite-run
+    fi
+fi
