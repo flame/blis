66cc4a4da43ce118c014c3dd29878a5a1f0d3a10
Devin Matthews <dmatthews@utexas.edu>
2018-05-23 15:49:38 -0500

    Update do_testsuite.sh
    [ci skip]

--- travis/do_testsuite.sh
+++ travis/do_testsuite.sh
@@ -8,7 +8,7 @@ export BLIS_JC_NT=1
 export BLIS_IR_NT=1
 export BLIS_JR_NT=1
 
-travis_wait 30 make BLIS_ENABLE_TEST_OUTPUT=yes testblis
+make BLIS_ENABLE_TEST_OUTPUT=yes testblis
 $DIST_PATH/build/check-blistest.sh ./output.testsuite
 make BLIS_ENABLE_TEST_OUTPUT=yes testblas
 $DIST_PATH/build/check-blastest.sh
