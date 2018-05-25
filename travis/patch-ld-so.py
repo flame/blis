85aed5166e0844defe49337f35a5dca984f6e3b8
Devin Matthews <dmatthews@utexas.edu>
2018-05-24 19:31:22 -0500

    Allow unpatched ld.so if we don't know how to patch it.
    I *think* this only happens for older glibc without the multi-arch stuff (e.g. Ubuntu 14.04 on Travis), but who knows?

--- travis/patch-ld-so.py
+++ travis/patch-ld-so.py
@@ -12,6 +12,6 @@ infile, outfile = sys.argv[1:]
 d = open(infile, 'rb').read()
 # Match CPUID(eax=0), "xor eax,eax" followed closely by "cpuid"
 o = re.sub(b'(\x31\xc0.{0,32})\x0f\xa2', b'\\1\x66\x90', d)
-assert d != o
+#assert d != o
 open(outfile, 'wb').write(o)
 
