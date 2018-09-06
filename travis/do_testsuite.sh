97965b09059a610db06fb7a22bdfa79c0d37d673
Mathieu Poumeyrol <kali@users.noreply.github.com>
2018-09-06 21:10:29 +0200

    cortexa9 and cortexa53 travis build + qemu test (#245)

--- travis/do_testsuite.sh
+++ travis/do_testsuite.sh
@@ -8,7 +8,13 @@ export BLIS_JC_NT=1
 export BLIS_IR_NT=1
 export BLIS_JR_NT=1
 
-make testblis
+if [ "$TEST" = "FAST" ]
+then
+    make testblis-fast
+else
+    make testblis
+fi
+
 $DIST_PATH/testsuite/check-blistest.sh ./output.testsuite
 make testblas
 $DIST_PATH/blastest/check-blastest.sh
