a65a39043528d194f974b47fa15b97d550068a4c
Isuru Fernando <isuruf@gmail.com>
2018-09-07 13:01:55 -0500

    Add declspec for dll

--- frame/include/bli_extern_defs.h
+++ frame/include/bli_extern_defs.h
@@ -35,16 +35,26 @@
 #ifndef BLIS_EXTERN_DEFS_H
 #define BLIS_EXTERN_DEFS_H
 
-extern obj_t BLIS_TWO;
-extern obj_t BLIS_ONE;
-//extern obj_t BLIS_ONE_HALF;
-extern obj_t BLIS_ZERO;
-//extern obj_t BLIS_MINUS_ONE_HALF;
-extern obj_t BLIS_MINUS_ONE;
-extern obj_t BLIS_MINUS_TWO;
+#if !defined(BLIS_ENABLE_SHARED) || !defined(_MSC_VER)
+#define BLIS_EXPORT
+#else
+#ifdef BLIS_BUILD_DLL
+#define BLIS_EXPORT __declspec(dllexport)
+#else
+#define BLIS_EXPORT __declspec(dllimport)
+#endif
+#endif
 
-extern thrcomm_t BLIS_SINGLE_COMM;
-extern thrinfo_t BLIS_PACKM_SINGLE_THREADED;
-extern thrinfo_t BLIS_GEMM_SINGLE_THREADED;
+BLIS_EXPORT extern obj_t BLIS_TWO;
+BLIS_EXPORT extern obj_t BLIS_ONE;
+//BLIS_EXPORT extern obj_t BLIS_ONE_HALF;
+BLIS_EXPORT extern obj_t BLIS_ZERO;
+//BLIS_EXPORT extern obj_t BLIS_MINUS_ONE_HALF;
+BLIS_EXPORT extern obj_t BLIS_MINUS_ONE;
+BLIS_EXPORT extern obj_t BLIS_MINUS_TWO;
+
+BLIS_EXPORT extern thrcomm_t BLIS_SINGLE_COMM;
+BLIS_EXPORT extern thrinfo_t BLIS_PACKM_SINGLE_THREADED;
+BLIS_EXPORT extern thrinfo_t BLIS_GEMM_SINGLE_THREADED;
 
 #endif
