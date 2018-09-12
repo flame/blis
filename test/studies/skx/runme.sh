e669057fe35f2037d8111af687d84a0ecf6d7a2a
Devangi N. Parikh <dnp@cs.utexas.edu>
2018-09-11 22:29:42 -0500

    Fixed bug in runme script in test/studies
    Details:
    - Fixed bug in runme script for skx studies that set the number of
      threads incorrectly

--- test/studies/skx/runme.sh
+++ test/studies/skx/runme.sh
@@ -257,7 +257,7 @@ for th in ${threads}; do
                                                 export GOMP_CPU_AFFINITY="0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39"
 
                                         fi
-                                        if [ ${nc} -eq 28 ]; then
+                                        if [ ${nc} -eq 20 ]; then
 
                                                 export BLIS_JC_NT=${jc_1_nt}
                                                 export BLIS_IC_NT=${ic_1_nt}
@@ -265,7 +265,7 @@ for th in ${threads}; do
                                                 export BLIS_IR_NT=${ir_1_nt}
                                                 export OMP_NUM_THREADS=${nt_1}
                                                 out_dir="${out_rootdir}/1socket"
-                                        elif [ ${nc} -eq 56 ]; then
+                                        elif [ ${nc} -eq 40 ]; then
                                                 export BLIS_JC_NT=${jc_2_nt}
                                                 export BLIS_IC_NT=${ic_2_nt}
                                                 export BLIS_JR_NT=${jr_2_nt}
