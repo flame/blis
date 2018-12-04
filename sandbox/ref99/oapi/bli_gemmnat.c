0645f239fbdf37ee9d2096ee3bb0e76b3302cfff
Field G. Van Zee <field@cs.utexas.edu>
2018-12-04 14:31:06 -0600

    Remove UT-Austin from copyright headers' clause 3.
    Details:
    - Removed explicit reference to The University of Texas at Austin in the
      third clause of the license comment blocks of all relevant files and
      replaced it with a more all-encompassing "copyright holder(s)".
    - Removed duplicate words ("derived") from a few kernels' license
      comment blocks.
    - Homogenized license comment block in kernels/zen/3/bli_gemm_small.c
      with format of all other comment blocks.

--- sandbox/ref99/oapi/bli_gemmnat.c
+++ sandbox/ref99/oapi/bli_gemmnat.c
@@ -16,8 +16,8 @@
       notice, this list of conditions and the following disclaimer in the
       documentation and/or other materials provided with the distribution.
     - Neither the name of copyright holder(s) nor the names
-      of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
 
    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
