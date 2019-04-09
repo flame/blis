2330becabecaa0d9948851376f7e60abf49a7a9b
Nicholai <nicholai@utexas.edu>
2019-04-09 16:07:55 -0500

    Chnage MC to a multiple of 12

--- config/power9/bli_cntx_init_power9.c
+++ config/power9/bli_cntx_init_power9.c
@@ -71,7 +71,7 @@ void bli_cntx_init_power9( cntx_t* cntx )
 	bli_blksz_init_easy( &blkszs[ BLIS_MR ],     0,     4,     0,     0 );
 	bli_blksz_init_easy( &blkszs[ BLIS_NR ],     0,     6,     0,     0 );
 #endif
-	bli_blksz_init_easy( &blkszs[ BLIS_MC ],     0,    64,     0,     0 );
+	bli_blksz_init_easy( &blkszs[ BLIS_MC ],     0,    60,     0,     0 );
 	bli_blksz_init_easy( &blkszs[ BLIS_KC ],     0,   256,     0,     0 );
 	bli_blksz_init_easy( &blkszs[ BLIS_NC ],     0,  2562,     0,     0 );
 
