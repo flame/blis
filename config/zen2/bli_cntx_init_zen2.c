3f867c96caea3bbbbeeff1995d90f6cf8c9895fb
kdevraje <Kiran.Devrajegowda@amd.com>
2019-05-31 12:22:44 +0530

    When running HPL with pure MPI without DGEMM Threading (Single Threaded BLIS ), making this macro 1 gives best performance.wq
    Change-Id: I24fd0bf99216f315e49f1c74c44c3feaffd7078d

--- config/zen2/bli_cntx_init_zen2.c
+++ config/zen2/bli_cntx_init_zen2.c
@@ -106,12 +106,15 @@ void bli_cntx_init_zen2( cntx_t* cntx )
 	//                                           s      d      c      z
 	bli_blksz_init_easy( &blkszs[ BLIS_MR ],     6,     6,     3,     3 );
 	bli_blksz_init_easy( &blkszs[ BLIS_NR ],    16,     8,     8,     4 );
-
-
+#if AOCL_BLIS_MULTIINSTANCE
+	bli_blksz_init_easy( &blkszs[ BLIS_MC ],   144,   240,   144,    72 );
+        bli_blksz_init_easy( &blkszs[ BLIS_KC ],   256,   512,   256,   256 );
+        bli_blksz_init_easy( &blkszs[ BLIS_NC ],  4080,   2040,  4080,  4080 );
+#else
         bli_blksz_init_easy( &blkszs[ BLIS_MC ],   144,    72,   144,    72 );
         bli_blksz_init_easy( &blkszs[ BLIS_KC ],   256,   256,   256,   256 );
         bli_blksz_init_easy( &blkszs[ BLIS_NC ],  4080,   4080,  4080,  4080 );
-
+#endif
 
 	bli_blksz_init_easy( &blkszs[ BLIS_AF ],     8,     8,    -1,    -1 );
 	bli_blksz_init_easy( &blkszs[ BLIS_DF ],     8,     8,    -1,    -1 );
--- config/zen2/bli_family_zen2.h
+++ config/zen2/bli_family_zen2.h
@@ -66,5 +66,8 @@
 
 #define D_BLIS_SMALL_MATRIX_THRES_TRSM_DIM_RATIO 22
 
+//When running HPL with pure MPI without DGEMM Threading (Single Threaded BLIS ), making this macro 1 gives best performance.
+#define AOCL_BLIS_MULTIINSTANCE   0
+
 #endif
 
