d0da3bbdc85aa6c488bad38adab305d74270c33e
Nicholai <nicholai@utexas.edu>
2019-06-12 11:40:56 -0500

    debug

--- kernels/power9/3/bli_gemm_power9_asm_d12x6.c
+++ kernels/power9/3/bli_gemm_power9_asm_d12x6.c
@@ -268,12 +268,12 @@
   "stxv              %%vs27, 48(%%r20)    \n\t" \
   "stxv              %%vs28, 64(%%r20)    \n\t" \
   "stxv              %%vs29, 80(%%r20)    \n\t" \
-  "stxv              %%vs30, 0(%%r21)    \n\t" \
-  "stxv              %%vs31, 16(%%r21)    \n\t" \
-  "stxv              %%vs32, 32(%%r21)    \n\t" \
-  "stxv              %%vs33, 48(%%r21)    \n\t" \
-  "stxv              %%vs34, 64(%%r21)    \n\t" \
-  "stxv              %%vs35, 80(%%r21)    \n\t"
+  "stxv              %%vs30, 0(%%r28)    \n\t" \
+  "stxv              %%vs31, 16(%%r28)    \n\t" \
+  "stxv              %%vs32, 32(%%r28)    \n\t" \
+  "stxv              %%vs33, 48(%%r28)    \n\t" \
+  "stxv              %%vs34, 64(%%r28)    \n\t" \
+  "stxv              %%vs35, 80(%%r28)    \n\t"
 
 
 
@@ -322,7 +322,7 @@ void bli_dgemm_power9_asm_12x6
   "add              %%r18, %%r17, %%r6             \n\t" // c + cs_c * 2
   "add              %%r19, %%r18, %%r6             \n\t" // c + cs_c * 3
   "add              %%r20, %%r19, %%r6             \n\t" // c + cs_c * 4
-  "add              %%r21, %%r20, %%r6             \n\t" // c + cs_c * 5
+  "add              %%r28, %%r20, %%r6             \n\t" // c + cs_c * 5
   "                                               \n\t"
   "                                               \n\t" // Offsets for B
   "li               %%r22,0                       \n\t" // 0
