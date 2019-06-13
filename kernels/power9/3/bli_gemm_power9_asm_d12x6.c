48e3752bb0adae98785924e33881592d4f2c34e9
Nicholai <nicholai@utexas.edu>
2019-06-13 13:36:20 -0500

    Debugging

--- kernels/power9/3/bli_gemm_power9_asm_d12x6.c
+++ kernels/power9/3/bli_gemm_power9_asm_d12x6.c
@@ -574,6 +574,7 @@ void bli_dgemm_power9_asm_12x6
   "                                               \n\t"
   "DGENSTORED:                                    \n\t"
   "                                               \n\t"
+  #if 0
   "ld              %%r22, %6                      \n\t"
   "add             %%r23, %%r22, %%r9             \n\t" // c + rs_c
   "add             %%r23, %%r23, %%r9             \n\t" // c + rs_c * 2
@@ -710,6 +711,7 @@ void bli_dgemm_power9_asm_12x6
   "xxswapd         %%vs35, %%vs35                 \n\t" 
   "stxsdx          %%vs35, %%r9, %%r27            \n\t"
   "                                               \n\t"
+  #endif
   "b               DDONE                          \n\t"
   "                                               \n\t"
   "DCOLSTORED:                                    \n\t"
