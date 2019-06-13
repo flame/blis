58942c2349bcd6257b795aab22b36a4702ff0bfe
Nicholai <nicholai@utexas.edu>
2019-06-13 13:56:15 -0500

    Add more printouts

--- kernels/power9/3/bli_gemm_power9_asm_d12x6.c
+++ kernels/power9/3/bli_gemm_power9_asm_d12x6.c
@@ -352,7 +352,8 @@ void bli_dgemm_power9_asm_12x6
 	uint64_t rs_c   = rs_c0;
 	uint64_t cs_c   = cs_c0;
 
-  printf("rs_c0 = %ld | cs_c0 = %ld\n", rs_c0, cs_c0);
+  printf("rs_c0 = %ld | cs_c0 = %ld\n | alpha = %lf | beta = %lf",
+     rs_c0, cs_c0, alpha, beta);
 
   // if(rs_c0 != 1)
   // {
