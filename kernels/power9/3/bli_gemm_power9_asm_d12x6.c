38bd48e5576867441a5bb023efd16fc83cc8031f
Nicholai <nicholai@utexas.edu>
2019-06-14 10:36:26 -0500

    test xxswapd

--- kernels/power9/3/bli_gemm_power9_asm_d12x6.c
+++ kernels/power9/3/bli_gemm_power9_asm_d12x6.c
@@ -593,7 +593,6 @@ void bli_dgemm_power9_asm_12x6
   "add             %%r27, %%r27, %%r9             \n\t" // c + rs_c * 10
   "                                               \n\t"
   "                                               \n\t"
-  "lxv            %%vs0, 0(%%r22)                 \n\t"
   "xxswapd        %%vs0, %%vs0       \n\t"
   "stxsdx          %%vs0, 0, %%r22         \n\t" 
   "xxswapd        %%vs0, %%vs0       \n\t"
