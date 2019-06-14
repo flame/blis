90d6e92a0ca6fe93a867dffd1ea09668667d051d
Nicholai <nicholai@utexas.edu>
2019-06-14 13:24:29 -0500

    Set unroll factor = 4

--- kernels/power9/3/bli_gemm_power9_asm_d12x6.c
+++ kernels/power9/3/bli_gemm_power9_asm_d12x6.c
@@ -421,7 +421,7 @@ void bli_dgemm_power9_asm_12x6
 
 	// Typecast local copies of integers in case dim_t and inc_t are a
 	// different size than is expected by load instructions.
-  #if 0
+  #if 1
 	uint64_t k_iter = k0 / 4;
 	uint64_t k_left = k0 % 4;
   #else
