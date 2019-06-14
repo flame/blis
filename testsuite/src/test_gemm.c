a9242bb283001f2c06dd37e250e6ae8e4710fc3f
Nicholai <nicholai@utexas.edu>
2019-06-14 11:19:18 -0500

    set alpha to 0, beta != 0

--- testsuite/src/test_gemm.c
+++ testsuite/src/test_gemm.c
@@ -245,8 +245,8 @@ void libblis_test_gemm_experiment
 	// Set alpha and beta.
 	if ( bli_obj_is_real( &c ) )
 	{
-		bli_setsc(  1.0,  0.0, &alpha );
-		bli_setsc(  1.0,  0.0, &beta );
+		bli_setsc(  0.0,  0.0, &alpha );
+		bli_setsc(  0.9,  0.0, &beta );
 	}
 	else
 	{
