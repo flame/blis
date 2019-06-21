ed02da2c33b3bb4ac7f5a4e1f6ddfec3db97e503
Nicholai <nicholai@utexas.edu>
2019-06-21 12:56:59 -0500

    Change cache block sizes

--- config/power9/bli_cntx_init_power9.c
+++ config/power9/bli_cntx_init_power9.c
@@ -56,9 +56,9 @@ void bli_cntx_init_power9( cntx_t* cntx )
 	//                                           s      d      c      z
 	bli_blksz_init_easy( &blkszs[ BLIS_MR ],     0,     12,     0,     0 );
 	bli_blksz_init_easy( &blkszs[ BLIS_NR ],     0,     6,     0,     0 );
-	bli_blksz_init_easy( &blkszs[ BLIS_MC ],     0,    96,     0,     0 );
-	bli_blksz_init_easy( &blkszs[ BLIS_KC ],     0,   678,     0,     0 );
-	bli_blksz_init_easy( &blkszs[ BLIS_NC ],     0,  1902,     0,     0 );
+	bli_blksz_init_easy( &blkszs[ BLIS_MC ],     0,    72,     0,     0 );
+	bli_blksz_init_easy( &blkszs[ BLIS_KC ],     0,   256,     0,     0 );
+	bli_blksz_init_easy( &blkszs[ BLIS_NC ],     0,  4080,     0,     0 );
 
 	// Update the context with the current architecture's register and cache
 	// blocksizes (and multiples) for native execution.
