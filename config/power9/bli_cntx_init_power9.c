fc688b69a296e980645196c6a1167284ebcdbf83
Nicholai <nicholai@utexas.edu>
2019-06-26 15:39:53 -0500

    kc = 2730

--- config/power9/bli_cntx_init_power9.c
+++ config/power9/bli_cntx_init_power9.c
@@ -59,7 +59,7 @@ void bli_cntx_init_power9( cntx_t* cntx )
 	bli_blksz_init_easy( &blkszs[ BLIS_MR ],     0,     12,     0,     0 );
 	bli_blksz_init_easy( &blkszs[ BLIS_NR ],     0,     6,     0,     0 );
 	bli_blksz_init_easy( &blkszs[ BLIS_MC ],     0,   4032,     0,     0 );
-	bli_blksz_init_easy( &blkszs[ BLIS_KC ],     0,   648,     0,     0 );
+	bli_blksz_init_easy( &blkszs[ BLIS_KC ],     0,   2730,     0,     0 );
 	bli_blksz_init_easy( &blkszs[ BLIS_NC ],     0,  8400,     0,     0 );
 	
 	// Update the context with the current architecture's register and cache
