1bed4cfae9848c9c3177baabe9c00ccc8ea24e97
Nicholai <nicholai@utexas.edu>
2019-07-01 16:28:24 -0500

    examine beta case

--- kernels/power9/3/bli_gemm_power9_asm_d16x4.c
+++ kernels/power9/3/bli_gemm_power9_asm_d16x4.c
@@ -348,8 +348,8 @@ void bli_dgemm_power9_asm_16x4
   uint64_t rs_c   = rs_c0;
 	uint64_t cs_c   = cs_c0;
 
-  printf("Entering ukernel | k = %ld | alpha = %lf | beta = %lf | beta* = %x | rs_c = %ld | cs_c = %ld \n",
-                                     k_left, *alpha, *beta, beta, rs_c, cs_c);
+  printf("Entering ukernel | k = %ld | alpha = %lf | beta = %lf | rs_c = %ld | cs_c = %ld \n",
+                                     k_left, *alpha, *beta, rs_c, cs_c);
 
 	__asm__ volatile
 	(
@@ -444,7 +444,7 @@ void bli_dgemm_power9_asm_16x4
   	"                                               \n\t"
   	"ld               %%r0, %4                      \n\t" // load ptr for alpha
   	"ld               %%r28, %5                     \n\t" // load ptr for beta
-    "ld               %%r29, 0(%%r28)               \n\t"
+    "ld               %%r30, 0(%%r28)               \n\t" // load val of beta
   	"                                               \n\t"
   	"lxvdsx           %%vs60, 0, %%r0               \n\t" // splat alpha
   	"lxvdsx           %%vs59, 0, %%r28              \n\t" // splat beta
@@ -456,7 +456,7 @@ void bli_dgemm_power9_asm_16x4
   	"                                               \n\t"
   	"                                               \n\t"
   	"                                               \n\t"
-  	"cmpdi            %%r0, %%r29, 0                \n\t"
+  	"cmpdi            %%r0, %%r30, 0                \n\t"
   	"beq              %%r0, DBETAZERO               \n\t" // jump to BZ case if beta = 0
   	"                                               \n\t"
   	"ld               %%r22, %6                     \n\t" // load ptr for C (used as offset)
