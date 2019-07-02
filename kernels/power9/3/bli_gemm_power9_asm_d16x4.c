4156afbaaddd60b04ed35638fdd197028ae33e5d
Nicholai <nicholai@utexas.edu>
2019-07-02 13:30:28 -0500

    change format of printout

--- kernels/power9/3/bli_gemm_power9_asm_d16x4.c
+++ kernels/power9/3/bli_gemm_power9_asm_d16x4.c
@@ -395,10 +395,10 @@ void bli_dgemm_power9_asm_16x4
 	uint64_t cs_c   = cs_c0;
 
   #if 1
-  printf("Entering ukernel | k_iter = %ld | k_left = %ld | alpha = %lf | beta = %lf | rs_c = %ld | cs_c = %ld \n",
+  printf("[Entering ukernel] k_iter = %ld | k_left = %ld | alpha = %lf | beta = %lf | rs_c = %ld | cs_c = %ld\n",
                                       k_iter, k_left, *alpha, *beta, rs_c, cs_c);
   #elif 0
-  printf("Entering kernel\n");
+  printf("[Entering kernel]\n");
   #endif
 
 	__asm__ volatile
