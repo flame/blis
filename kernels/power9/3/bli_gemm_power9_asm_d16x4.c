fecd80e8ab4cae782c49e012802a4d2e86b4f076
Nicholai <nicholai@utexas.edu>
2019-07-02 13:24:21 -0500

    printout with info

--- kernels/power9/3/bli_gemm_power9_asm_d16x4.c
+++ kernels/power9/3/bli_gemm_power9_asm_d16x4.c
@@ -394,10 +394,10 @@ void bli_dgemm_power9_asm_16x4
   uint64_t rs_c   = rs_c0;
 	uint64_t cs_c   = cs_c0;
 
-  #if 0
+  #if 1
   printf("Entering ukernel | k = %ld | alpha = %lf | beta = %lf | rs_c = %ld | cs_c = %ld \n",
                                       k_left, *alpha, *beta, rs_c, cs_c);
-  #elif 1
+  #elif 0
   printf("Entering kernel\n");
   #endif
 
