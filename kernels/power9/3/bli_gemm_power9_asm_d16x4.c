f1437bb4cf3220c0c687cec8e96fb03ee65cc1b1
Nicholai <nicholai@utexas.edu>
2019-07-02 14:16:06 -0500

    make some changes

--- kernels/power9/3/bli_gemm_power9_asm_d16x4.c
+++ kernels/power9/3/bli_gemm_power9_asm_d16x4.c
@@ -408,9 +408,6 @@ void bli_dgemm_power9_asm_16x4
 	(
 	  "                                               \n\t"
   	"                                               \n\t"
-  	// "ld               %%r10, %9                     \n\t"
-    // "li               %%r9, 1                       \n\t"
-    // "stw              %%r9, 0(%9)                   \n\t"
     "                                               \n\t"
     "                                               \n\t"
     "ld               %%r10, %8                     \n\t" // load cs_c
@@ -541,6 +538,21 @@ void bli_dgemm_power9_asm_16x4
   	"                                               \n\t"
     GENLOAD_SCALE_UPDATE                                  // (1) load, scale, increment offsets
   	"                                              	\n\t"
+    "xxpermdi     %%vs40, %%vs0, %%vs8, 1           \n\t" // permute
+    "xxpermdi     %%vs41, %%vs1, %%vs9, 1           \n\t"
+    "xxpermdi     %%vs42, %%vs2, %%vs10, 1          \n\t"
+    "xxpermdi     %%vs43, %%vs3, %%vs11, 1          \n\t"
+    "xxpermdi     %%vs44, %%vs4, %%vs12, 1          \n\t"
+    "xxpermdi     %%vs45, %%vs5, %%vs13, 1          \n\t"
+    "xxpermdi     %%vs46, %%vs6, %%vs14, 1          \n\t"
+    "xxpermdi     %%vs47, %%vs7, %%vs15, 1          \n\t"
+    "                                              	\n\t" 
+	  GENADD_STORE                                          // add and store
+  	"                                               \n\t"
+  	"                                               \n\t"
+    GENLOAD_SCALE_UPDATE                                  // (2) load, scale, increment offsets
+  	"                                               \n\t"
+    "                                               \n\t"
     "xxpermdi     %%vs40, %%vs8, %%vs0, 1           \n\t" // permute
     "xxpermdi     %%vs41, %%vs9, %%vs1, 1           \n\t"
     "xxpermdi     %%vs42, %%vs10, %%vs2, 1          \n\t"
@@ -549,21 +561,6 @@ void bli_dgemm_power9_asm_16x4
     "xxpermdi     %%vs45, %%vs13, %%vs5, 1          \n\t"
     "xxpermdi     %%vs46, %%vs14, %%vs6, 1          \n\t"
     "xxpermdi     %%vs47, %%vs15, %%vs7, 1          \n\t"
-    "                                              	\n\t" 
-	  GENADD_STORE                                          // add and store
-  	"                                               \n\t"
-  	"                                               \n\t"
-    GENLOAD_SCALE_UPDATE                                  // (2) load, scale, increment offsets
-  	"                                               \n\t"
-    "                                               \n\t"
-  	"xxpermdi     %%vs40, %%vs0, %%vs8, 1           \n\t" // permute
-    "xxpermdi     %%vs41, %%vs1, %%vs9, 1           \n\t"
-    "xxpermdi     %%vs42, %%vs2, %%vs10, 1          \n\t"
-    "xxpermdi     %%vs43, %%vs3, %%vs11, 1          \n\t"
-    "xxpermdi     %%vs44, %%vs4, %%vs12, 1          \n\t"
-    "xxpermdi     %%vs45, %%vs5, %%vs13, 1          \n\t"
-    "xxpermdi     %%vs46, %%vs6, %%vs14, 1          \n\t"
-    "xxpermdi     %%vs47, %%vs7, %%vs15, 1          \n\t"
     "                                              	\n\t" 
 	  GENADD_STORE                                          // add and store
   	"                                               \n\t"
