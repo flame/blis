79c97164657bf27a35354410ae18f37f93b8190a
Nicholai <nicholai@utexas.edu>
2019-07-03 13:19:36 -0500

    start wroking on rank2 update

--- kernels/power9/3/bli_gemm_power9_asm_d16x4.c
+++ kernels/power9/3/bli_gemm_power9_asm_d16x4.c
@@ -136,24 +136,152 @@
 "xvmuldp         %%vs30, %%vs30, %%vs60         \n\t" \
 "xvmuldp         %%vs31, %%vs31, %%vs60         \n\t"
  
-#define LOADANDUPDATE \
+#define LOADANDUPDATE2 \
+"                                               \n\t" \
+"                                               \n\t" \
+"xvmaddadp        %%vs0, %%vs32, %%vs48         \n\t" \
+"xvmaddadp        %%vs1, %%vs33, %%vs48         \n\t" \
+"xvmaddadp        %%vs2, %%vs34, %%vs48         \n\t" \
+"xvmaddadp        %%vs3, %%vs35, %%vs48         \n\t" \
+"                                               \n\t" \
+"lxv              %%vs40, 0(%%r7)               \n\t" \
+"lxv              %%vs41, 16(%%r7)              \n\t" \
+"                                               \n\t" \
+"xvmaddadp        %%vs4, %%vs36, %%vs48         \n\t" \
+"xvmaddadp        %%vs5, %%vs37, %%vs48         \n\t" \
+"xvmaddadp        %%vs6, %%vs38, %%vs48         \n\t" \
+"xvmaddadp        %%vs7, %%vs39, %%vs48         \n\t" \
+"                                               \n\t" \
+"lxv              %%vs42, 32(%%r7)              \n\t" \
+"lxv              %%vs43, 48(%%r7)              \n\t" \
+"                                               \n\t" \
+"xvmaddadp        %%vs8, %%vs32, %%vs49         \n\t" \
+"xvmaddadp        %%vs9, %%vs33, %%vs49         \n\t" \
+"xvmaddadp        %%vs10, %%vs34, %%vs49        \n\t" \
+"xvmaddadp        %%vs11, %%vs35, %%vs49        \n\t" \
+"                                               \n\t" \
+"lxv              %%vs52, 0(%%r8)               \n\t" \
+"lxv              %%vs54, 16(%%r8)              \n\t" \
+"xxpermdi         %%vs53, %%vs52, %%vs52, 2     \n\t" \
+"xxpermdi         %%vs55, %%vs54, %%vs54, 2     \n\t" \
+"                                               \n\t" \
+"xvmaddadp        %%vs12, %%vs36, %%vs49        \n\t" \
+"xvmaddadp        %%vs13, %%vs37, %%vs49        \n\t" \
+"xvmaddadp        %%vs14, %%vs38, %%vs49        \n\t" \
+"xvmaddadp        %%vs15, %%vs39, %%vs49        \n\t" \
+"                                               \n\t" \
+"lxv              %%vs44, 64(%%r7)              \n\t" \
+"lxv              %%vs45, 80(%%r7)              \n\t" \
+"                                               \n\t" \
+"xvmaddadp        %%vs16, %%vs32, %%vs50        \n\t" \
+"xvmaddadp        %%vs17, %%vs33, %%vs50        \n\t" \
+"xvmaddadp        %%vs18, %%vs34, %%vs50        \n\t" \
+"xvmaddadp        %%vs19, %%vs35, %%vs50        \n\t" \
+"                                               \n\t" \
+"lxv              %%vs46, 96(%%r7)              \n\t" \
+"lxv              %%vs47, 112(%%r7)             \n\t" \
+"                                               \n\t" \
+"xvmaddadp        %%vs20, %%vs36, %%vs50        \n\t" \
+"xvmaddadp        %%vs21, %%vs37, %%vs50        \n\t" \
+"xvmaddadp        %%vs22, %%vs38, %%vs50        \n\t" \
+"xvmaddadp        %%vs23, %%vs39, %%vs50        \n\t" \
+"                                               \n\t" \
+"lxv              %%vs48, 32(%%r8)              \n\t" \
+"xxpermdi         %%vs49, %%vs48, %%vs48, 2     \n\t" \
+"                                               \n\t" \
+"xvmaddadp        %%vs24, %%vs32, %%vs51        \n\t" \
+"xvmaddadp        %%vs25, %%vs33, %%vs51        \n\t" \
+"xvmaddadp        %%vs26, %%vs34, %%vs51        \n\t" \
+"xvmaddadp        %%vs27, %%vs35, %%vs51        \n\t" \
+"                                               \n\t" \
+"xvmaddadp        %%vs28, %%vs36, %%vs51        \n\t" \
+"xvmaddadp        %%vs29, %%vs37, %%vs51        \n\t" \
+"xvmaddadp        %%vs30, %%vs38, %%vs51        \n\t" \
+"xvmaddadp        %%vs31, %%vs39, %%vs51        \n\t" \
+"                                               \n\t" \
+"                                               \n\t" \
+"                                               \n\t" \
+"                                               \n\t" \
+"                                               \n\t" \
+"                                               \n\t" \
+"                                               \n\t" \
+"                                               \n\t" \
+"xvmaddadp        %%vs0, %%vs40, %%vs52         \n\t" \
+"xvmaddadp        %%vs1, %%vs41, %%vs52         \n\t" \
+"xvmaddadp        %%vs2, %%vs42, %%vs52         \n\t" \
+"xvmaddadp        %%vs3, %%vs43, %%vs52         \n\t" \
+"                                               \n\t" \
+"lxv              %%vs50, 48(%%r8)              \n\t" \
+"xxpermdi         %%vs51, %%vs51, %%vs51, 2     \n\t" \
+"                                               \n\t" \
+"xvmaddadp        %%vs4, %%vs44, %%vs52         \n\t" \
+"xvmaddadp        %%vs5, %%vs45, %%vs52         \n\t" \
+"xvmaddadp        %%vs6, %%vs46, %%vs52         \n\t" \
+"xvmaddadp        %%vs7, %%vs47, %%vs52         \n\t" \
+"                                               \n\t" \
+"lxv              %%vs32, 128(%%r7)             \n\t" \
+"lxv              %%vs33, 144(%%r7)             \n\t" \
+"                                               \n\t" \
+"xvmaddadp        %%vs8, %%vs40, %%vs53         \n\t" \
+"xvmaddadp        %%vs9, %%vs41, %%vs53         \n\t" \
+"xvmaddadp        %%vs10, %%vs42, %%vs53        \n\t" \
+"xvmaddadp        %%vs11, %%vs43, %%vs53        \n\t" \
+"                                               \n\t" \
+"lxv              %%vs34, 160(%%r7)             \n\t" \
+"lxv              %%vs35, 176(%%r7)             \n\t" \
+"                                               \n\t" \
+"xvmaddadp        %%vs12, %%vs44, %%vs53        \n\t" \
+"xvmaddadp        %%vs13, %%vs45, %%vs53        \n\t" \
+"xvmaddadp        %%vs14, %%vs46, %%vs53        \n\t" \
+"xvmaddadp        %%vs15, %%vs47, %%vs53        \n\t" \
+"                                               \n\t" \
+"lxv              %%vs36, 192(%%r7)             \n\t" \
+"lxv              %%vs37, 208(%%r7)             \n\t" \
+"                                               \n\t" \
+"xvmaddadp        %%vs16, %%vs40, %%vs54        \n\t" \
+"xvmaddadp        %%vs17, %%vs41, %%vs54        \n\t" \
+"xvmaddadp        %%vs18, %%vs42, %%vs54        \n\t" \
+"xvmaddadp        %%vs19, %%vs43, %%vs54        \n\t" \
+"                                               \n\t" \
+"lxv              %%vs38, 224(%%r7)             \n\t" \
+"lxv              %%vs39, 240(%%r7)             \n\t" \
+"                                               \n\t" \
+"xvmaddadp        %%vs20, %%vs44, %%vs54        \n\t" \
+"xvmaddadp        %%vs21, %%vs45, %%vs54        \n\t" \
+"xvmaddadp        %%vs22, %%vs46, %%vs54        \n\t" \
+"xvmaddadp        %%vs23, %%vs47, %%vs54        \n\t" \
+"                                               \n\t" \
+"                                               \n\t" \
+"xvmaddadp        %%vs24, %%vs40, %%vs55        \n\t" \
+"xvmaddadp        %%vs25, %%vs41, %%vs55        \n\t" \
+"xvmaddadp        %%vs26, %%vs42, %%vs55        \n\t" \
+"xvmaddadp        %%vs27, %%vs43, %%vs55        \n\t" \
+"                                               \n\t" \
+"xvmaddadp        %%vs28, %%vs44, %%vs55        \n\t" \
+"xvmaddadp        %%vs29, %%vs45, %%vs55        \n\t" \
+"xvmaddadp        %%vs30, %%vs46, %%vs55        \n\t" \
+"xvmaddadp        %%vs31, %%vs47, %%vs55        \n\t" \
+"                                               \n\t" \
+"                                               \n\t" \
+"                                               \n\t" \
+"                                               \n\t"
+
+#define LOADANDUPDATE1 \
 "lxv              %%vs32, 0(%%r7)               \n\t" \
 "lxv              %%vs33, 16(%%r7)              \n\t" \
 "lxv              %%vs34, 32(%%r7)              \n\t" \
 "lxv              %%vs35, 48(%%r7)              \n\t" \
-"lxv              %%vs36, 64(%%r7)              \n\t" \
-"lxv              %%vs37, 80(%%r7)              \n\t" \
-"lxv              %%vs38, 96(%%r7)              \n\t" \
-"lxv              %%vs39, 112(%%r7)             \n\t" \
 "                                               \n\t" \
 "lxv              %%vs48, 0(%%r8)               \n\t" \
 "lxv              %%vs50, 16(%%r8)              \n\t" \
 "xxpermdi         %%vs49, %%vs48, %%vs48, 2     \n\t" \
 "xxpermdi         %%vs51, %%vs50, %%vs50, 2     \n\t" \
 "                                               \n\t" \
+"lxv              %%vs36, 64(%%r7)              \n\t" \
+"lxv              %%vs37, 80(%%r7)              \n\t" \
+"lxv              %%vs38, 96(%%r7)              \n\t" \
+"lxv              %%vs39, 112(%%r7)             \n\t" \
 "                                               \n\t" \
-"addi             %%r8, %%r8, 32                \n\t" \
-"addi             %%r7, %%r7, 128               \n\t" \
 "                                               \n\t" \
 "xvmaddadp        %%vs0, %%vs32, %%vs48         \n\t" \
 "xvmaddadp        %%vs1, %%vs33, %%vs48         \n\t" \
@@ -174,6 +302,11 @@
 "xvmaddadp        %%vs14, %%vs38, %%vs49        \n\t" \
 "xvmaddadp        %%vs15, %%vs39, %%vs49        \n\t" \
 "                                               \n\t" \
+"                                               \n\t" \
+"addi             %%r8, %%r8, 32                \n\t" \
+"addi             %%r7, %%r7, 128               \n\t" \
+"                                               \n\t" \
+"                                               \n\t" \
 "xvmaddadp        %%vs16, %%vs32, %%vs50        \n\t" \
 "xvmaddadp        %%vs17, %%vs33, %%vs50        \n\t" \
 "xvmaddadp        %%vs18, %%vs34, %%vs50        \n\t" \
@@ -190,7 +323,19 @@
 "xvmaddadp        %%vs28, %%vs36, %%vs51        \n\t" \
 "xvmaddadp        %%vs29, %%vs37, %%vs51        \n\t" \
 "xvmaddadp        %%vs30, %%vs38, %%vs51        \n\t" \
-"xvmaddadp        %%vs31, %%vs39, %%vs51        \n\t" 
+"xvmaddadp        %%vs31, %%vs39, %%vs51        \n\t" \
+"                                               \n\t" \
+"                                               \n\t" \
+"                                               \n\t" \
+"                                               \n\t" \
+"                                               \n\t" 
+
+
+
+
+
+
+
 
 #define SCALECOL_CMATRIX \
 "xvmuldp         %%vs32, %%vs32, %%vs59         \n\t" \
@@ -452,10 +597,10 @@ void bli_dgemm_power9_asm_16x4
     "                                               \n\t"
     "                                               \n\t"
     "                                               \n\t"
-    LOADANDUPDATE
-    LOADANDUPDATE
-    LOADANDUPDATE
-    LOADANDUPDATE
+    LOADANDUPDATE1
+    LOADANDUPDATE1
+    LOADANDUPDATE1
+    LOADANDUPDATE1
   	"                                               \n\t"
     "                                               \n\t"
     "                                               \n\t"
@@ -472,7 +617,7 @@ void bli_dgemm_power9_asm_16x4
   	"                                               \n\t"
   	"DLOOPKLEFT:                                    \n\t" // EDGE LOOP
     "                                               \n\t"
-    LOADANDUPDATE
+    LOADANDUPDATE1
     "                                               \n\t"
   	"bdnz             DLOOPKLEFT                    \n\t"
   	"                                               \n\t"
