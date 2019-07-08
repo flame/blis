db0d22a9c3e8a9990ea7ee11c1354457617a9dcf
Nicholai <nicholai@utexas.edu>
2019-07-08 12:24:40 -0500

    add debug variable back

--- kernels/power9/3/bli_gemm_power9_asm_d16x4.c
+++ kernels/power9/3/bli_gemm_power9_asm_d16x4.c
@@ -54,6 +54,7 @@ void bli_dgemm_power9_asm_16x4
 	//void*   b_next = bli_auxinfo_next_b( data );
 
   int *debug = malloc(sizeof(int));
+  *debug = 0;
 
 	// Typecast local copies of integers in case dim_t and inc_t are a
 	// different size than is expected by load instructions.
