020e0a1cfdc2b89f0c19469c1310fd50aa5e6bad
Nicholai <nicholai@utexas.edu>
2019-07-22 17:20:08 -0500

    Unroll by 16 in 16x4

--- kernels/power9/3/bli_gemm_power9_asm_d16x4.c
+++ kernels/power9/3/bli_gemm_power9_asm_d16x4.c
@@ -54,8 +54,8 @@ void bli_dgemm_power9_asm_16x4
 	// Typecast local copies of integers in case dim_t and inc_t are a
 	// different size than is expected by load instructions.
 
-	uint64_t k_iter = k0 / 32;
-	uint64_t k_left = k0 % 32;
+	uint64_t k_iter = k0 / 16;
+	uint64_t k_left = k0 % 16;
   uint64_t rs_c   = rs_c0;
 	uint64_t cs_c   = cs_c0;
 
@@ -108,7 +108,7 @@ void bli_dgemm_power9_asm_16x4
     "                                               \n\t"
     "                                               \n\t"
     "                                               \n\t"
-    DLOAD_UPDATE_32
+    DLOAD_UPDATE_16
   	"                                               \n\t"
     "                                               \n\t"
     "                                               \n\t"
