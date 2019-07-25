7e61f194b4da8a701b3cdcb28f3e9dd8944630b3
Nicholai <nicholai@utexas.edu>
2019-07-25 12:35:39 -0500

    Remove print out

--- kernels/power9/3/bli_gemm_power9_asm_d16x4.c
+++ kernels/power9/3/bli_gemm_power9_asm_d16x4.c
@@ -54,8 +54,6 @@ void bli_dgemm_power9_asm_16x4
 	// Typecast local copies of integers in case dim_t and inc_t are a
 	// different size than is expected by load instructions.
 
-  printf("[%s] A: %p | B: %p \n", __func__, a, b);
-
   #if 1
 	uint64_t k_iter = k0 / 16;
 	uint64_t k_left = k0 % 16;
