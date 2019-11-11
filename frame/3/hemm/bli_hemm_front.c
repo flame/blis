bdc7ee3394500d8e5b626af6ff37c048398bb27e
Field G. Van Zee <field@cs.utexas.edu>
2019-11-11 15:47:17 -0600

    Various fixes to support packing duplication in B.
    Details:
    - Added cpp macros to trmm and trmm3 front-ends to optionally force
      those operations to be cast so the structured matrix is on the left.
      symm and hemm already had such macros, but these too were renamed so
      that the macros were individual to the operation. We now have four
      such macros:
        #define BLIS_DISABLE_HEMM_RIGHT
        #define BLIS_DISABLE_SYMM_RIGHT
        #define BLIS_DISABLE_TRMM_RIGHT
        #define BLIS_DISABLE_TRMM3_RIGHT
      Also, updated the comments in the symm and hemm front-ends related to
      the first two macro guards, and added corresponding comments to the
      trmm and trmm3 front-ends for the latter two guards. (They all
      functionally do the same thing, just for their specific operations.)
      Thanks to Jeff Hammond for reporting the bugs that led me to this
      change (via #359).
    - Updated config/old/haswellbb subconfiguration (used to debug issues
      related to duplicating B during packing) to register: a packing
      kernel for single-precision real; gemmbb ukernels for s, c, and z;
      trsmbb ukernels for s, c, and z; gemmtrsmbb virtual ukrnels for s, c
      and z; and to use non-default cache and register blocksizes for s, c,
      and z datatypes. Also declared prototypes for all of the gemmbb,
      trsmbb, and gemmtrsmbb ukernel functions within the
      bli_cntx_init_haswellbb() function. This should, once applied to the
      power9 configuration, fix the remaining issues in #359.
    - Defined bli_spackm_6xk_bb4_ref(), which packs single reals with a
      duplication factor of 4. This function is defined in the same file as
      bli_dpackm_6xk_bb2_ref() (bli_packm_cxk_bb_ref.c).

--- frame/3/hemm/bli_hemm_front.c
+++ frame/3/hemm/bli_hemm_front.c
@@ -70,16 +70,21 @@ void bli_hemm_front
 	bli_obj_alias_to( c, &c_local );
 
 #ifdef BLIS_DISABLE_HEMM_RIGHT
-	// NOTE: This case casts right-side hemm/symm in terms of left side. This
-	// is necessary when the current subconfiguration uses a gemm microkernel
+	// NOTE: This case casts right-side hemm in terms of left side. This is
+	// necessary when the current subconfiguration uses a gemm microkernel
 	// that assumes that the packing kernel will have already duplicated
 	// (broadcast) element of B in the packed copy of B. Supporting
 	// duplication within the logic that packs micropanels from Hermitian/
-	// symmetric matrices would be ugly, and so we simply don't support it.
-	// As a consequence, those subconfigurations need a way to force the
-	// Hermitian/symmetric matrix to be on the left (and thus the general
-	// matrix to the on the right). So our solution is that in those cases,
-	// the subconfigurations simply #define BLIS_DISABLE_HEMM_RIGHT.
+	// matrices would be ugly, and so we simply don't support it. As a
+	// consequence, those subconfigurations need a way to force the Hermitian
+	// matrix to be on the left (and thus the general matrix to the on the
+	// right). So our solution is that in those cases, the subconfigurations
+	// simply #define BLIS_DISABLE_HEMM_RIGHT.
+
+	// NOTE: This case casts right-side hemm in terms of left side. This can
+	// lead to the microkernel being executed on an output matrix with the
+	// microkernel's general stride IO case (unless the microkernel supports
+	// both both row and column IO cases as well).
 
 	// If A is being multiplied from the right, transpose all operands
 	// so that we can perform the computation as if A were being multiplied
