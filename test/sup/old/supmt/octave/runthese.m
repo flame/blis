967ddf284718ba751e203d5812f9eaa6a9c71418
Field G. Van Zee <field@cs.utexas.edu>
2020-03-05 17:03:21 -0600

    Updated sup performance graphs; added mt results.
    Details:
    - Reran all existing single-threaded performance experiments comparing
      BLIS sup to other implementations (including the conventional code
      path within BLIS), using the latest versions (where appropriate).
    - Added multithreaded results for the three existing hardware types
      showcased in docs/PerformanceSmall.md: Kaby Lake, Haswell, and Epyc
      (Zen1).
    - Various minor updates to the text in docs/PerformanceSmall.md.
    - Updates to the octave scripts in test/sup/octave, test/supmt/octave.

--- test/sup/old/supmt/octave/runthese.m
+++ test/sup/old/supmt/octave/runthese.m
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 
 
 
@@ -24,16 +23,3 @@ plot_panel_trxsh(3.1,16,12,'mt','d','rrr',[ 6 8 10 ],'../../sup/results/haswell/
 
 % epyc
 plot_panel_trxsh(2.55,8,32,'mt','d','rrr',[ 6 8 10 ],'../../sup/results/epyc/20200302/mnkt100000_mt32','epyc','MKL','octave'); close; clear all;
-=======
-% haswell
-plot_panel_trxsh(3.25,16,1,'mt','d','ccc',[ 6 8 10 ],'../results/haswell/20190823/4_800_4_mt201','has','MKL','matlab'); close; clear all;
-plot_panel_trxsh(3.25,16,1,'mt','d','rrr',[ 6 8 10 ],'../results/haswell/20190823/4_800_4_mt201','has','MKL','matlab'); close; clear all;
-
-% kabylake
-plot_panel_trxsh(3.80,16,1,'mt','d','rrr',[ 6 8 10 ],'..','kbl','MKL','matlab'); close; clear all;
-plot_panel_trxsh(3.80,16,1,'mt','d','ccc',[ 6 8 10 ],'..','kbl','MKL','matlab'); close; clear all;
-
-% epyc
-plot_panel_trxsh(3.00, 8,1,'mt','d','rrr',[ 6 8 10 ],'../results/epyc/20190826/4_800_4_mt256','epyc','MKL','matlab'); close; clear all;
-plot_panel_trxsh(3.00, 8,1,'mt','d','ccc',[ 6 8 10 ],'../results/epyc/20190826/4_800_4_mt256','epyc','MKL','matlab'); close; clear all;
->>>>>>> Merged test/sup, test/supmt into test/sup.
