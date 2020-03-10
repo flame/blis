eb716edfe85d9a73c9fc013817a71dcbcd8274b9
Field G. Van Zee <field@cs.utexas.edu>
2020-03-10 13:55:29 -0500

    Merged test/sup, test/supmt into test/sup.
    Details:
    - Updated the Makefile, test_gemm.c, and runme.sh in test/sup to be able
      to compile and run both single-threaded and multithreaded experiments.
      This should help with maintenance going forward.
    - Created a test/sup/octave_st directory of scripts (based on the
      previous test/sup/octave scripts) as well as a test/sup/octave_mt
      directory (based on the previous test/supmt/octave scripts). The
      octave scripts are slightly different and not easily mergeable, and
      thus for now I'll maintain them separately.
    - Preserved the previous test/sup directory as test/sup/old/supst and
      the previous test/supmt directory as test/sup/old/supmt.
    
    Change-Id: Ia230fc65185fd9a34eec714721004aa9e0bd40ed

--- test/sup/octave_mt/plot_l3sup_perf.m
+++ test/sup/octave_mt/plot_l3sup_perf.m
@@ -9,6 +9,7 @@ function r_val = plot_l3sup_perf( opname, ...
                                   cfreq, ...
                                   dfps, ...
                                   theid, impl )
+
 %if ... %mod(theid-1,cols) == 2 || ...
 %   ... %mod(theid-1,cols) == 3 || ...
 %   ... %mod(theid-1,cols) == 4 || ...
@@ -19,11 +20,11 @@ function r_val = plot_l3sup_perf( opname, ...
 %end
 
 %legend_plot_id = 11;
-legend_plot_id = 1*cols + 1*5;
+legend_plot_id = 0*cols + 1*6;
 
 if 1
-ax1 = subplot( rows, cols, theid );
-hold( ax1, 'on' );
+	ax1 = subplot( rows, cols, theid );
+	hold( ax1, 'on' );
 end
 
 % Set line properties.
@@ -77,9 +78,9 @@ end
 flopscol = size( data_blissup, 2 );
 msize = 5;
 if 1
-fontsize = 11;
+	fontsize = 12;
 else
-fontsize = 16;
+	fontsize = 16;
 end
 linesize = 0.5;
 legend_loc = 'southeast';
@@ -95,13 +96,14 @@ for psize_col = 1:3
 end
 x_axis( :, 1 ) = data_blissup( :, psize_col );
 
-% Compute the number of data points we have in the x-axis. Note that
-% we only use quarter the data points for the m = n = k column of graphs.
-if mod(theid-1,cols) == 6
-	np = size( data_blissup, 1 ) / 4;
-else
-	np = size( data_blissup, 1 );
-end
+% Compute the number of data points we have in the x-axis. Note that we
+% only use half the data points for the m = n = k column of graphs.
+%if mod(theid-1,cols) == 6
+%	np = size( data_blissup, 1 ) / 2;
+%else
+%	np = size( data_blissup, 1 );
+%end
+np = size( data_blissup, 1 );
 
 % Grab the last x-axis value.
 x_end = data_blissup( np, psize_col );
@@ -125,8 +127,7 @@ open_ln     = line( x_axis( 1:np, 1 ), data_open( 1:np, flopscol ) / nth, ...
 vend_ln     = line( x_axis( 1:np, 1 ), data_vend( 1:np, flopscol ) / nth, ...
                     'Color',color_vend, 'LineStyle',lines_vend, ...
                     'LineWidth',linesize );
-else
-if theid == legend_plot_id
+elseif theid == legend_plot_id
 blissup_ln  = line( nan, nan, ...
                     'Color',color_blissup, 'LineStyle',lines_blissup, ...
                     'LineWidth',linesize );
@@ -143,15 +144,28 @@ vend_ln     = line( nan, nan, ...
                     'Color',color_vend, 'LineStyle',lines_vend, ...
                     'LineWidth',linesize );
 end
-end
 
 
 xlim( ax1, [x_begin x_end] );
 ylim( ax1, [y_begin y_end] );
 
-if     6000 <= x_end && x_end < 10000
+if mod(theid-1,cols) == 3 || mod(theid-1,cols) == 4 || mod(theid-1,cols) == 5
+	if nth == 12
+		ylim( ax1, [y_begin y_end/2] );
+	elseif nth > 12
+		ylim( ax1, [y_begin y_end/6] );
+	end
+end
+
+if    10000 <= x_end && x_end < 15000
 	x_tick2 = x_end - 2000;
 	x_tick1 = x_tick2/2;
+	%xticks( ax1, [ x_tick1 x_tick2 ] );
+	xticks( ax1, [ 4000 8000 12000 ] );
+elseif 6000 <= x_end && x_end < 10000
+	x_tick2 = x_end - 2000;
+	x_tick1 = x_tick2/2;
+	%xticks( ax1, [ x_tick1 x_tick2 ] );
 	xticks( ax1, [ x_tick1 x_tick2 ] );
 elseif 4000 <= x_end && x_end < 6000
 	x_tick2 = x_end - 1000;
@@ -188,11 +202,12 @@ if show_plot == 1 || theid == legend_plot_id
 		set( leg,'Color','none' );
 		set( leg,'Units','inches' );
 		if impl == 'octave'
-		set( leg,'FontSize',fontsize );
-		set( leg,'Position',[12.50 10.35 1.5 0.9 ] ); % (1,4tl)
+			set( leg,'FontSize',fontsize );
+			%set( leg,'Position',[12.40 10.60 1.9 0.95 ] ); % (1,4tl)
+			set( leg,'Position',[18.80 10.60 1.9 0.95 ] ); % (1,4tl)
 		else
-		set( leg,'FontSize',fontsize-1 );
-		set( leg,'Position',[18.24 10.15 1.15 0.7 ] ); % (1,4tl)
+			set( leg,'FontSize',fontsize-1 );
+			set( leg,'Position',[18.24 10.15 1.15 0.7 ] ); % (1,4tl)
 		end
 		set( leg,'Box','off' );
 		set( leg,'Color','none' );
@@ -209,17 +224,31 @@ box( ax1, 'on' );
 titl = title( titlename );
 set( titl, 'FontWeight', 'normal' ); % default font style is now 'bold'.
 
+% The default is to align the plot title across whole figure, not the box.
+% This is a hack to nudge the title back to the center of the box.
 if impl == 'octave'
-tpos = get( titl, 'Position' ); % default is to align across whole figure, not box.
-tpos(1) = tpos(1) + -40;
-set( titl, 'Position', tpos ); % here we nudge it back to centered with box.
+	tpos = get( titl, 'Position' );
+	% For some reason, the titles in the graphs in the last column start
+	% off in a different relative position than the graphs in the other
+	% columns. Here, we manually account for that.
+	if mod(theid-1,cols) == 6
+		tpos(1) = tpos(1) + -10;
+	else
+		tpos(1) = tpos(1) + -40;
+	end
+	set( titl, 'Position', tpos );
+	set( titl, 'FontSize', fontsize );
+else % impl == 'matlab'
+	tpos = get( titl, 'Position' );
+	tpos(1) = tpos(1) + 90;
+	set( titl, 'Position', tpos );
 end
 
 if theid > (rows-1)*cols
-%xlab = xlabel( ax1,xaxisname );
-%tpos = get( xlab, 'Position' )
-%tpos(2) = tpos(2) + 10;
-%set( xlab, 'Position', tpos );
+	%xlab = xlabel( ax1,xaxisname );
+	%tpos = get( xlab, 'Position' )
+	%tpos(2) = tpos(2) + 10;
+	%set( xlab, 'Position', tpos );
 	if     theid == rows*cols - 6
 	xlab = xlabel( ax1, 'm = 6; n = k' );
 	elseif theid == rows*cols - 5
@@ -238,13 +267,8 @@ if theid > (rows-1)*cols
 end
 
 if mod(theid-1,cols) == 0
-ylab = ylabel( ax1,yaxisname );
+	ylab = ylabel( ax1,yaxisname );
 end
-
-%export_fig( filename, colorflag, '-pdf', '-m2', '-painters', '-transparent' );
-%saveas( fig, filename_png );
-
-%hold( ax1, 'off' );
 
 r_val = 0;
 
