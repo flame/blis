c20c96d9c0c1481f2ea01bce9301f29f1d39bf06
Meghana <meghana.vankadari@amd.com>
2020-03-18 16:07:47 +0530

    Made some critical changes to small_gemm kernels
    Details:
    - In case of GEMM, whenever beta is zero, we need to perform C = alpha
    *(A * B) instead of C = beta * C + alpha * (A * B)
     Added conditions to check the value of beta at different levels inside
     small_gemm kernels and decide whether to perform scaling C with beta or
     not.
    -Modified small_gemm kernels to use BLIS specific functions to retrieve
     different fields of objects.
    -Calling bli_gemm_check before entering bli_gemm_small to facilitate
     early return in case of invalid inputs.
    -For corner cases inside small_gemm kernels, a buffer called f_temp
     is used to load and store data to and from registers.
     populating the buffer with zeroes before use.
    -In bli_gemm_front, datatypes of status and return value from
     bli_gemm_small are not matching.
     Corrected the datatype of the variable 'status' inside bli_gemm_front
     to err_t.
    
    Change-Id: I8b52ad55008f028d6c8b7e0d20f746a869d9daea
    Signed-off-by: Meghana Vankadari <Meghana.Vankadari@amd.com>
    AMD-Internal: [CPUPL-689,SWLCSG-104]

--- frame/3/gemm/bli_gemm_front.c
+++ frame/3/gemm/bli_gemm_front.c
@@ -5,7 +5,7 @@
    libraries.
 
    Copyright (C) 2014, The University of Texas at Austin
-   Copyright (C) 2018 - 2019, Advanced Micro Devices, Inc.
+   Copyright (C) 2018 - 2020, Advanced Micro Devices, Inc.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -53,17 +53,6 @@ void bli_gemm_front
 	obj_t   b_local;
 	obj_t   c_local;
 
-#ifdef BLIS_ENABLE_SMALL_MATRIX
-	// Only handle small problems separately for homogeneous datatypes.
-	if ( bli_obj_dt( a ) == bli_obj_dt( b ) &&
-	     bli_obj_dt( a ) == bli_obj_dt( c ) &&
-	     bli_obj_comp_prec( c ) == bli_obj_prec( c ) )
-	{
-		gint_t status = bli_gemm_small( alpha, a, b, beta, c, cntx, cntl );
-		if ( status == BLIS_SUCCESS ) return;
-	}
-#endif
-
 	// Check parameters.
 	if ( bli_error_checking_is_enabled() )
 		bli_gemm_check( alpha, a, b, beta, c, cntx );
@@ -74,6 +63,18 @@ void bli_gemm_front
 		bli_scalm( beta, c );
 		return;
 	}
+
+
+#ifdef BLIS_ENABLE_SMALL_MATRIX
+	// Only handle small problems separately for homogeneous datatypes.
+	if ( bli_obj_dt( a ) == bli_obj_dt( b ) &&
+	     bli_obj_dt( a ) == bli_obj_dt( c ) &&
+	     bli_obj_comp_prec( c ) == bli_obj_prec( c ) )
+	{
+		err_t status = bli_gemm_small( alpha, a, b, beta, c, cntx, cntl );
+		if ( status == BLIS_SUCCESS ) return;
+	}
+#endif
 
 	// Alias A, B, and C in case we need to apply transformations.
 	bli_obj_alias_to( a, &a_local );
