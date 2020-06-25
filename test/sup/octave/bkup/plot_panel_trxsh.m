908887358eaa5e6854b3c7f5b9f99cf944a9fdaa
Field G. Van Zee <field@cs.utexas.edu>
2020-06-25 16:05:12 -0500

    Support ldims, packing in sup/test drivers.
    Details:
    - Updated the test/sup source file (test_gemm.c) and Makefile to support
      building matrices with small or large leading dimensions, and updated
      runme.sh to support executing both kinds of test drivers.
    - Updated runme.sh to allow for executing sup drivers with unpacked (the
      default) or packed matrices (via setting BLIS_PACK_A, BLIS_PACK_B
      environment variables), and for capturing output to files that encode
      both the leading dimension (small or large) and packing status into
      the filenames.
    - Consolidated octave scripts in test/sup/octave_st, test/sup/octave_mt
      into test/sup/octave and updated the octave code in that consolidated
      directory to read the new output filename format (encoding ldim and
      packing). Also added comments and streamlined code, particularly in
      plot_panel_trxsh.m. Tested the octave scripts with octave 5.2.0.
    - Moved old octave_st, octave_mt directories to test/sup/old.

--- test/sup/octave/bkup/plot_panel_trxsh.m
+++ test/sup/octave/bkup/plot_panel_trxsh.m
@@ -7,42 +7,13 @@ function r_val = plot_panel_trxsh ...
        dt_ch, ...
        stor_str, ...
        smalldims, ...
+       ldim_str, ...
+       pack_str, ...
        dirpath, ...
        arch_str, ...
        vend_str, ...
        impl ...
      )
-
-%cfreq = 1.8;
-%dflopspercycle = 32;
-
-% Create filename "templates" for the files that contain the performance
-% results.
-filetemp_blissup  = '%s/output_%s_%s_blissup.m';
-filetemp_blislpab = '%s/output_%s_%s_blislpab.m';
-filetemp_eigen    = '%s/output_%s_%s_eigen.m';
-filetemp_open     = '%s/output_%s_%s_openblas.m';
-filetemp_vend     = '%s/output_%s_%s_vendor.m';
-
-% Create a variable name "template" for the variables contained in the
-% files outlined above.
-vartemp = 'data_%s_%s_%s( :, : )';
-
-% Define the datatypes and operations we will be plotting.
-oproot      = sprintf( '%cgemm', dt_ch );
-ops( 1, : ) = sprintf( '%s_nn', oproot );
-ops( 2, : ) = sprintf( '%s_nt', oproot );
-ops( 3, : ) = sprintf( '%s_tn', oproot );
-ops( 4, : ) = sprintf( '%s_tt', oproot );
-
-% Generate datatype-specific operation names from the set of operations
-% and datatypes.
-[ opsupnames, opnames ] = gen_opsupnames( ops, stor_str, smalldims );
-n_opsupnames = size( opsupnames, 1 );
-
-%opsupnames
-%opnames
-%return
 
 if 1 == 1
 	%fig = figure('Position', [100, 100, 2400, 1500]);
@@ -60,6 +31,44 @@ if 1 == 1
 	set(gcf,'PaperOrientation','landscape');
 end
 
+%cfreq = 1.8;
+%dflopspercycle = 32;
+
+if nth == 1
+	is_st = 1;
+else
+	is_st = 0;
+end
+
+% Create filename "templates" for the files that contain the performance
+% results.
+filetemp_blissup  = '%s/output_%s_%s_blissup.m';
+filetemp_blisconv = '%s/output_%s_%s_blisconv.m';
+filetemp_eigen    = '%s/output_%s_%s_eigen.m';
+filetemp_open     = '%s/output_%s_%s_openblas.m';
+filetemp_vend     = '%s/output_%s_%s_vendor.m';
+filetemp_bfeo     = '%s/output_%s_%s_blasfeo.m';
+filetemp_xsmm     = '%s/output_%s_%s_libxsmm.m';
+
+% Create a variable name "template" for the variables contained in the
+% files outlined above.
+vartemp = 'data_%s_%s_%s( :, : )';
+
+% Define the datatypes and operations we will be plotting.
+oproot      = sprintf( '%cgemm', dt_ch );
+ops( 1, : ) = sprintf( '%s_nn', oproot );
+ops( 2, : ) = sprintf( '%s_nt', oproot );
+ops( 3, : ) = sprintf( '%s_tn', oproot );
+ops( 4, : ) = sprintf( '%s_tt', oproot );
+
+% Generate datatype-specific operation names from the set of operations
+% and datatypes.
+[ opsupnames, opnames ] = gen_opsupnames( ops, stor_str, smalldims, ldim_str, pack_str );
+n_opsupnames = size( opsupnames, 1 );
+
+%opsupnames
+%opnames
+%return
 
 % Iterate over the list of datatype-specific operation names.
 for opi = 1:n_opsupnames
@@ -69,6 +78,7 @@ for opi = 1:n_opsupnames
 	opsupname = opsupnames( opi, : );
 	opname    = opnames( opi, : );
 
+	% Remove leading and trailing whitespace.
 	opsupname = strtrim( opsupname );
 	opname    = strtrim( opname );
 
@@ -76,63 +86,88 @@ for opi = 1:n_opsupnames
 
 	% Construct filenames for the data files from templates.
 	file_blissup  = sprintf( filetemp_blissup,  dirpath, thr_str, opsupname );
-	file_blislpab = sprintf( filetemp_blislpab, dirpath, thr_str, opsupname );
+	file_blisconv = sprintf( filetemp_blisconv, dirpath, thr_str, opsupname );
 	file_eigen    = sprintf( filetemp_eigen,    dirpath, thr_str, opsupname );
 	file_open     = sprintf( filetemp_open,     dirpath, thr_str, opsupname );
 	file_vend     = sprintf( filetemp_vend,     dirpath, thr_str, opsupname );
+	file_bfeo     = sprintf( filetemp_bfeo,     dirpath, thr_str, opsupname );
 
 	% Load the data files.
 	%str = sprintf( '  Loading %s', file_blissup ); disp(str);
 	run( file_blissup )
-	run( file_blislpab )
+	run( file_blisconv )
 	run( file_eigen )
 	run( file_open )
 	run( file_vend )
+	if is_st
+		run( file_bfeo )
+	end
 
 	% Construct variable names for the variables in the data files.
 	var_blissup  = sprintf( vartemp, thr_str, opname, 'blissup' );
-	var_blislpab = sprintf( vartemp, thr_str, opname, 'blislpab' );
+	var_blisconv = sprintf( vartemp, thr_str, opname, 'blisconv' );
 	var_eigen    = sprintf( vartemp, thr_str, opname, 'eigen' );
 	var_open     = sprintf( vartemp, thr_str, opname, 'openblas' );
 	var_vend     = sprintf( vartemp, thr_str, opname, 'vendor' );
+	var_bfeo     = sprintf( vartemp, thr_str, opname, 'blasfeo' );
 
 	% Use eval() to instantiate the variable names constructed above,
 	% copying each to a simplified name.
 	data_blissup  = eval( var_blissup );  % e.g. data_st_dgemm_blissup( :, : );
-	data_blislpab = eval( var_blislpab ); % e.g. data_st_dgemm_blislpab( :, : );
+	data_blisconv = eval( var_blisconv ); % e.g. data_st_dgemm_blisconv( :, : );
 	data_eigen = eval( var_eigen );       % e.g. data_st_dgemm_eigen( :, : );
 	data_open = eval( var_open );         % e.g. data_st_dgemm_openblas( :, : );
 	data_vend = eval( var_vend );         % e.g. data_st_dgemm_vendor( :, : );
+	if is_st
+		data_bfeo = eval( var_bfeo );         % e.g. data_st_dgemm_blasfeo( :, : );
+	else
+		% Set the data variable to zeros using the same dimensions as the other
+		% variables.
+		data_bfeo = zeros( size( data_blissup, 1 ), ...
+						   size( data_blissup, 2 ) );
+	end
 
-	%str = sprintf( '  Reading %s', var_blissup ); disp(str);
-	%str = sprintf( '  Reading %s', var_blislpab ); disp(str);
-	%str = sprintf( '  Reading %s', var_eigen ); disp(str);
-	%str = sprintf( '  Reading %s', var_open ); disp(str);
-	%str = sprintf( '  Reading %s', var_bfeo ); disp(str);
-	%str = sprintf( '  Reading %s', var_xsmm ); disp(str);
-	%str = sprintf( '  Reading %s', var_vend ); disp(str);
+	if is_st && stor_str == 'ccc'
+		% Only read xsmm data for the column storage case, since that's the
+		% only format that libxsmm supports.
+		file_xsmm = sprintf( filetemp_xsmm,     dirpath, thr_str, opsupname );
+		run( file_xsmm )
+		var_xsmm  = sprintf( vartemp, thr_str, opname, 'libxsmm' );
+		data_xsmm = eval( var_xsmm );     % e.g. data_st_dgemm_libxsmm( :, : );
+	else
+		% Set the data variable to zeros using the same dimensions as the other
+		% variables.
+		data_xsmm = zeros( size( data_blissup, 1 ), ...
+						   size( data_blissup, 2 ) );
+	end
 
 	% Plot one result in an m x n grid of plots, via the subplot()
 	% function.
 	if 1 == 1
 	plot_l3sup_perf( opsupname, ...
+	                 smalldims, ...
 	                 data_blissup, ...
-	                 data_blislpab, ...
+	                 data_blisconv, ...
 	                 data_eigen, ...
 	                 data_open, ...
 	                 data_vend, vend_str, ...
+	                 data_bfeo, ...
+	                 data_xsmm, ...
 	                 nth, ...
 	                 4, 7, ...
 	                 cfreq, ...
 	                 dflopspercycle, ...
 	                 opi, impl );
 
+	clear data_st_*gemm_*;
 	clear data_mt_*gemm_*;
 	clear data_blissup;
-	clear data_blislpab;
+	clear data_blisconv;
 	clear data_eigen;
 	clear data_open;
 	clear data_vend;
+	clear data_bfeo;
+	clear data_xsmm;
 
 	end
 
