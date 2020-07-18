1cad80442c70cd27cad67bb801855f1b07e8b171
RuQing Xu <r-xu@g.ecc.u-tokyo.ac.jp>
2020-07-19 07:18:51 +0900

    Register Arm SVE-512 A64fx Kernel Set

--- frame/include/bli_arch_config.h
+++ frame/include/bli_arch_config.h
@@ -274,6 +274,9 @@ CNTX_INIT_PROTS( generic )
 #ifdef BLIS_KERNELS_ARMSVE
 #include "bli_kernels_armsve.h"
 #endif
+#ifdef BLIS_KERNELS_A64FX
+#include "bli_kernels_a64fx.h"
+#endif
 #ifdef BLIS_KERNELS_ARMV8A
 #include "bli_kernels_armv8a.h"
 #endif
