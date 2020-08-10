fdfab1c613a06e77ad40c6bd95a0685d91d9fbaa
Dipal M Zambare <DipalMadhukar.Zambare@amd.com>
2020-08-10 12:17:09 +0530

    Fixed build issue in cpp testsuite.
    This issue was caused by incorrect merging of cpp and testcpp files.
    
    Change-Id: Idc40fbdaa55b6052a6a061d2d3e5cfae76b99916
    AMD-Internal: [CPUPL-1067]

--- testcpp/test_gemm.cc
+++ testcpp/test_gemm.cc
@@ -33,14 +33,8 @@
 
 #include <complex>
 #include <iostream>
-<<<<<<< HEAD
 #include "blis.hh"
 #include "test.hh"
-=======
-#include <string.h>
-#include <unistd.h>
-#include "blis.hh"
->>>>>>> Code Cleanup done; Test code updated to add performance measurement
 
 using namespace blis;
 using namespace std;
@@ -160,88 +154,10 @@ void test_gemm(  )
 // -----------------------------------------------------------------------------
 int main( int argc, char** argv )
 {
-<<<<<<< HEAD
     test_gemm<double>( );
     test_gemm<float>( );
     test_gemm<complex<float>>( );
     test_gemm<complex<double>>( );
     return 0;
-=======
-	int M, N, K, lda, ldb, ldc;
-	double a_d[DIM * DIM] = { 1.111, 2.222, 3.333, 4.444 };
-	double b_d[DIM * DIM] = { 5.555, 6.666, 7.777, 8.888 };
-	double c_d[DIM * DIM];
-	double alpha_d, beta_d;
-	float a_f[DIM * DIM] = { 1.1, 2.2, 3.3, 4.4 };
-	float b_f[DIM * DIM] = { 5.5, 6.6, 7.7, 8.8 };
-	float c_f[DIM * DIM];
-	float alpha_f, beta_f;
-	std::complex<float> a_c[DIM * DIM]={{1, 2},{3, 4},{5,6},{7,8}};
-	std::complex<float> b_c[DIM * DIM]={{1, 2},{3, 4},{5,6},{7,8}};
-	std::complex<float> c_c[DIM * DIM];
-	std::complex<float> alpha_c, beta_c;
-	std::complex<double> a_z[DIM * DIM]={{1.1, 2.2},{3.3, 4.4},{5.5,6.6},{7.7,8.8}};
-	std::complex<double> b_z[DIM * DIM]={{1.1, 2.2},{3.3, 4.4},{5.5,6.6},{7.7,8.8}};
-	std::complex<double> c_z[DIM * DIM];
-	std::complex<double> alpha_z, beta_z;
-	M = DIM;
-	N = M;
-	K = M;
-	lda = M;
-	ldb = K;
-	ldc = M;
-	alpha_d = 1.0;
-	beta_d = 0.0;
-	alpha_f = 1.0;
-	beta_f = 0.0;
-	alpha_c = {1.0,1.0};
-	beta_c = {0.0,0.0};
-	alpha_z = {1.0,1.0};
-	beta_z = {0.0,0.0};
-
-	/*cblis_sgemm*/	
-	cout<<"a_f= \n";
-	print_matrix<float>(a_f , M , K);
-	cout<<"b_f= \n";
-	print_matrix<float>(b_f , K , N);
-	blis::gemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, M, N, K, alpha_f, a_f,
-			lda, b_f, ldb, beta_f, c_f, ldc);
-	cout<<"c_f= \n";
-	print_matrix<float>(c_f , M , N);
-
-
-	/*cblis_dgemm*/	
-	printf("a_d = \n");
-	print_matrix<double>(a_d , M , K);
-	printf("b_d = \n");
-	print_matrix<double>(b_d , K , N);
-	blis::gemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, M, N, K, alpha_d, a_d,
-			lda, b_d, ldb, beta_d, c_d, ldc);
-	printf("c_d = \n");
-	print_matrix<double>(c_d , M , N);
-
-
-	/*cblis_cgemm*/	
-	printf("a_c = \n");
-	print_matrix<std::complex<float>>(a_c , M , K);
-	printf("b_c = \n");
-	print_matrix<std::complex<float>>(b_c , K , N);
-	blis::gemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, M, N, K, alpha_c, a_c,
-			lda, b_c, ldb, beta_c, c_c, ldc);
-	printf("c_c = \n");
-	print_matrix<std::complex<float>>(c_c , M , N);
-
-
-	/*cblis_zgemm*/	
-	printf("a_z = \n");
-	print_matrix<std::complex<double>>(a_z , M , K);
-	printf("b_z = \n");
-	print_matrix<std::complex<double>>(b_z , K , N);
-	blis::gemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, M, N, K, alpha_z, a_z,
-			lda, b_z, ldb, beta_z, c_z, ldc);
-	printf("c_z = \n");
-	print_matrix<std::complex<double>>(c_z , M , N);
-	return 0;
->>>>>>> Code Cleanup done; Test code updated to add performance measurement
 
 }
