1303732e8362ae866dcab057925bc77eea7adb9e
Meghana Vankadari <Meghana.Vankadari@amd.com>
2021-04-29 12:32:34 +0530

    Added sup functionality for SYRK
    Details:
    - Added bli_syrksup function that internally uses gemmt implementation.
    - Modified OAPI of syrk to call SUP before proceeding to the
      conventional implementation.
    - Copied gemmsup threshold function for syrk temporarily. Thresholds are
      yet to be derived for syrk.
    
    Change-Id: I751c6bd62bc76a3e4717f77c5cb33f19b759151d

--- frame/3/bli_l3_oapi.c
+++ frame/3/bli_l3_oapi.c
@@ -5,7 +5,7 @@
    libraries.
 
    Copyright (C) 2014, The University of Texas at Austin
-   Copyright (C) 2020, Advanced Micro Devices, Inc. All rights reserved.
+   Copyright (C) 2020-21, Advanced Micro Devices, Inc. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -306,9 +306,47 @@ void PASTEMAC(opname,EX_SUF) \
        BLIS_OAPI_EX_PARAMS  \
      ) \
 { \
+    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_2) \
 	bli_init_once(); \
 \
 	BLIS_OAPI_EX_DECLS \
+\
+    /* If C has a zero dimension, return early. */ \
+    if ( bli_obj_has_zero_dim( c ) ) {\
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_2) \
+        return; \
+    } \
+\
+    /* If alpha or A or B has a zero dimension, \
+     * scale C by beta and return early. */ \
+\
+    if( bli_obj_equals( alpha, &BLIS_ZERO ) || \
+        bli_obj_has_zero_dim( a ) ) \
+    { \
+        bli_scalm( beta, c ); \
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_2) \
+        return; \
+    } \
+\
+    /* If the rntm is non-NULL, it may indicate that we should forgo SUP handling altogether. */ \
+    bool enable_sup = TRUE; \
+    if( rntm != NULL ) enable_sup = bli_rntm_l3_sup( rntm ); \
+\
+    if( enable_sup ) \
+    { \
+        /* Execute the small/unpacked oapi handler.
+         * If it finds that the problem does not fall within the
+         * thresholds that define "small", or for some other reason
+         * decides not to use the small/unpacked implementation,
+         * the function returns with BLIS_FAILURE, which causes excution
+         * to proceed forward towards conventional implementation, */ \
+\
+        err_t result = PASTEMAC(opname, sup) ( alpha, a, beta, c, cntx, rntm ); \
+        if( result == BLIS_SUCCESS ) { \
+            AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_2) \
+            return; \
+        } \
+    } \
 \
 	/* Only proceed with an induced method if all operands have the same
 	   (complex) datatype. If any datatypes differ, skip the induced method
@@ -330,9 +368,44 @@ void PASTEMAC(opname,EX_SUF) \
 	} \
 }
 
-GENFRONT( herk )
 GENFRONT( syrk )
 
+#undef  GENFRONT
+#define GENFRONT( opname ) \
+\
+void PASTEMAC(opname,EX_SUF) \
+     ( \
+       obj_t*  alpha, \
+       obj_t*  a, \
+       obj_t*  beta, \
+       obj_t*  c  \
+       BLIS_OAPI_EX_PARAMS  \
+     ) \
+{ \
+    bli_init_once(); \
+\
+    BLIS_OAPI_EX_DECLS \
+\
+    /* Only proceed with an induced method if all operands have the same
+       (complex) datatype. If any datatypes differ, skip the induced method
+       chooser function and proceed directly with native execution, which is
+       where mixed datatype support will be implemented (if at all). */ \
+    if ( bli_obj_dt( a ) == bli_obj_dt( c ) && \
+         bli_obj_is_complex( c ) ) \
+    { \
+        /* Invoke the operation's "ind" function--its induced method front-end.
+           For complex problems, it calls the highest priority induced method
+           that is available (ie: implemented and enabled), and if none are
+           enabled, it calls native execution. (For real problems, it calls
+           the operation's native execution interface.) */ \
+        PASTEMAC(opname,ind)( alpha, a, beta, c, cntx, rntm ); \
+    } \
+    else \
+    { \
+        PASTEMAC(opname,nat)( alpha, a, beta, c, cntx, rntm ); \
+    } \
+}
+GENFRONT(herk)
 
 #undef  GENFRONT
 #define GENFRONT( opname ) \
--- frame/base/bli_cntx.h
+++ frame/base/bli_cntx.h
@@ -6,7 +6,7 @@
 
    Copyright (C) 2014, The University of Texas at Austin
    Copyright (C) 2016, Hewlett Packard Enterprise Development LP
-   Copyright (C) 2020, Advanced Micro Devices, Inc. All rights reserved.
+   Copyright (C) 2020 - 21, Advanced Micro Devices, Inc. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -335,6 +335,12 @@ BLIS_INLINE bool bli_cntx_gemmt_sup_thresh_is_met( num_t dt, dim_t n, dim_t k, c
         return bli_cntx_l3_sup_thresh_is_met( dt, n, n, k, cntx );
 }
 
+// -- syrk specific function
+BLIS_INLINE bool bli_cntx_syrk_sup_thresh_is_met( num_t dt, dim_t n, dim_t k, cntx_t* cntx )
+{
+    //copied gemm thresholds temporarily. These needs to be derived for syrk.
+        return bli_cntx_l3_sup_thresh_is_met( dt, n, n, k, cntx );
+}
 
 // -----------------------------------------------------------------------------
 
