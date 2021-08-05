flame/blis

commit 868b90138e64c873c780d9df14150d2a370a7a42

Fixed one-time use property of bli_init() (#525).
Details:
- Fixes a rather obvious bug that resulted in segmentation fault
  whenever the calling application tried to re-initialize BLIS after
  its first init/finalize cycle. The bug resulted from the fact that
  the bli_init.c APIs made no effort to allow bli_init() to be called
  subsequent times at all due to it, and bli_finalize(), being
  implemented in terms of pthread_once(). This has been fixed by
  resetting the pthread_once_t control variable for initialization
  at the end of bli_finalize_apis(), and by resetting the control
  variable for finalization at the end of bli_init_apis(). Thanks to
  @lschork2 for reporting this issue (#525), and to Minh Quan Ho and
  Devin Matthews for suggesting the chosen solution.
- CREDITS file update.

frame/base/bli_init.c
@@ -64,32 +64,9 @@ void bli_finalize_auto( void )
 
 // -----------------------------------------------------------------------------
 
-void bli_init_apis( void )
-{
-	// Initialize various sub-APIs.
-	bli_gks_init();
-	bli_ind_init();
-	bli_thread_init();
-	bli_pack_init();
-	bli_memsys_init();
-}
-
-void bli_finalize_apis( void )
-{
-	// Finalize various sub-APIs.
-	bli_memsys_finalize();
-	bli_pack_finalize();
-	bli_thread_finalize();
-	bli_ind_finalize();
-	bli_gks_finalize();
-}
-
-// -----------------------------------------------------------------------------
-
 // A pthread_once_t variable is a pthread structure used in pthread_once().
 // pthread_once() is guaranteed to execute exactly once among all threads that
-// pass in this control object. Thus, we need one for initialization and a
-// separate one for finalization.
+// pass in this control object (until/unless the variable is reset).
 static bli_pthread_once_t once_init     = BLIS_PTHREAD_ONCE_INIT;
 static bli_pthread_once_t once_finalize = BLIS_PTHREAD_ONCE_INIT;
 
@@ -101,5 +78,33 @@ void bli_init_once( void )
 void bli_finalize_once( void )
 {
 	bli_pthread_once( &once_finalize, bli_finalize_apis );
+}
+
+// -----------------------------------------------------------------------------
+
+void bli_init_apis( void )
+{
+	// Initialize various sub-APIs.
+	bli_gks_init();
+	bli_ind_init();
+	bli_thread_init();
+	bli_pack_init();
+	bli_memsys_init();
+
+	// Reset the control variable that will allow finalization.
+	once_finalize = BLIS_PTHREAD_ONCE_INIT;
+}
+
+void bli_finalize_apis( void )
+{
+	// Finalize various sub-APIs.
+	bli_memsys_finalize();
+	bli_pack_finalize();
+	bli_thread_finalize();
+	bli_ind_finalize();
+	bli_gks_finalize();
+
+	// Reset the control variable that will allow (re-)initialization.
+	once_init = BLIS_PTHREAD_ONCE_INIT;
 }
 

CREDITS
@@ -52,6 +52,7 @@ but many others have contributed code and feedback, including
   Kyungmin Lee             @kyungminlee        (Ohio State University)
   Michael Lehn             @michael-lehn
   Shmuel Levine            @ShmuelLevine
+                           @lschork2
   Dave Love                @loveshack
   Tze Meng Low                                 (The University of Texas at Austin)
   Ye Luo                   @ye-luo             (Argonne National Laboratory)

commit a32257eeab2e9946e71546a05a1847a39341ec6b

Fixed bli_init.c compile-time error on OSX clang.
Details:
- Fixed a compile-time error in bli_init.c when compiling with OSX's
  clang. This error was introduced in 868b901, which introduced a
  post-declaration struct assignment where the RHS was a struct
  initialization expression (i.e. { ... }). This use of struct
  initializer expressions apparently works with gcc despite it not
  being strict C99. The fix included in this commit declares a temporary
  variable for the purposes of being initialized to the desired value,
  via the struct initializer, and then copies the temporary struct (via
  '=' struct assignment) to the persistent struct. Thanks to Devin
  Matthews for his help with this.

frame/base/bli_init.c
@@ -92,7 +92,13 @@ void bli_init_apis( void )
 	bli_memsys_init();
 
 	// Reset the control variable that will allow finalization.
-	once_finalize = BLIS_PTHREAD_ONCE_INIT;
+	// NOTE: We must initialize a fresh pthread_once_t object and THEN copy the
+	// contents to the static control variable because some implementations of
+	// pthreads define pthread_once_t as a struct and BLIS_PTHREAD_ONCE_INIT as
+	// a struct initializer expression (i.e. { ... }), which cannot be used in
+	// post-declaration struct assignment in strict C99.
+	const bli_pthread_once_t once_new = BLIS_PTHREAD_ONCE_INIT;
+	once_finalize = once_new;
 }
 
 void bli_finalize_apis( void )
@@ -105,6 +111,12 @@ void bli_finalize_apis( void )
 	bli_gks_finalize();
 
 	// Reset the control variable that will allow (re-)initialization.
-	once_init = BLIS_PTHREAD_ONCE_INIT;
+	// NOTE: We must initialize a fresh pthread_once_t object and THEN copy the
+	// contents to the static control variable because some implementations of
+	// pthreads define pthread_once_t as a struct and BLIS_PTHREAD_ONCE_INIT as
+	// a struct initializer expression (i.e. { ... }), which cannot be used in
+	// post-declaration struct assignment in strict C99.
+	const bli_pthread_once_t once_new = BLIS_PTHREAD_ONCE_INIT;
+	once_init = once_new;
 }
 