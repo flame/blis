b677e0d61b23f26d9536e5c363fd6bbab6ee1540
RuQing Xu <r-xu@g.ecc.u-tokyo.ac.jp>
2021-09-16 01:18:54 +0900

    Arm SVE Add SGEMM 2Vx10 Unindexed

--- kernels/armsve/3/armsve_asm_macros_scomplex.h
+++ kernels/armsve/3/armsve_asm_macros_scomplex.h
@@ -5,6 +5,7 @@
    libraries.
 
    Copyright (C) 2014, The University of Texas at Austin
+   Copyright (C) 2020, The University of Tokyo
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -30,20 +31,18 @@
    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+
 */
+// Specify to use single precision.
+#define DT    "s"
+#define LD1   "ld1w"
+#define ST1   "st1w"
+#define LD2   "ld2w"
+#define ST2   "st2w"
+#define LD1R  "ld1rw"
+#define PRFG  "prfw"
+#define SZ    "4"
+#define OFFS  "uxtw #2"
+// Include macros.
+#include "armsve_asm_macros_cmplx.h"
 
-GEMM_UKR_PROT( double,   d, gemm_armsve256_asm_8x8 )
-GEMM_UKR_PROT( double,   d, gemm_armsve_asm_2vx10_unindexed )
-GEMM_UKR_PROT( float,    s, gemm_armsve_asm_2vx10_unindexed )
-GEMM_UKR_PROT( scomplex, c, gemm_armsve_asm_2vx10_unindexed )
-GEMM_UKR_PROT( dcomplex, z, gemm_armsve_asm_2vx10_unindexed )
-GEMM_UKR_PROT( dcomplex, z, gemm_armsve_asm_2vx8_unindexed )
-GEMM_UKR_PROT( dcomplex, z, gemm_armsve_asm_2vx7_unindexed )
-//GEMMSUP_KER_PROT( double,   d, gemmsup_rv_armsve_2vx10_unindexed )
-//GEMMSUP_KER_PROT( double,   d, gemmsup_cv_armsve_2vx10_unindexed )
-//GEMMSUP_KER_PROT( double,   d, gemmsup_rv_armsve_10x2v_unindexed )
-
-PACKM_KER_PROT( double,   d, packm_armsve256_asm_8xk )
-PACKM_KER_PROT( double,   d, packm_armsve512_asm_16xk )
-PACKM_KER_PROT( double,   d, packm_armsve512_asm_12xk )
-PACKM_KER_PROT( double,   d, packm_armsve512_asm_10xk )
