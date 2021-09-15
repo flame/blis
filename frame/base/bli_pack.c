628f4a41b8c307b6cbf0c9d467b705c3548c7400
Kiran Varaganti <Kiran.Varaganti@amd.com>
2021-09-15 12:02:28 +0530

    Fixed bug in packing API
    bli_pack_set_pack_b() API wrongly inits pack_a element of rntm object
    instead of pack_b. Fixed this bug.
    
    Change-Id: I267493ab3ff0bade478d1157799a6fa5b51c7970

--- frame/base/bli_pack.c
+++ frame/base/bli_pack.c
@@ -5,7 +5,7 @@
    libraries.
 
    Copyright (C) 2014, The University of Texas at Austin
-   Copyright (C) 2018, Advanced Micro Devices, Inc.
+   Copyright (C) 2018-2021, Advanced Micro Devices, Inc. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -101,7 +101,7 @@ void bli_pack_set_pack_b( bool pack_b )
 	// Acquire the mutex protecting global_rntm.
 	bli_pthread_mutex_lock( &global_rntm_mutex );
 
-	bli_rntm_set_pack_a( pack_b, &global_rntm );
+	bli_rntm_set_pack_b( pack_b, &global_rntm );
 
 	// Release the mutex protecting global_rntm.
 	bli_pthread_mutex_unlock( &global_rntm_mutex );
