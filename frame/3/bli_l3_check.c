28b0982ea70c21841fb23802d38f6b424f8200e1
Devin Matthews <damatthews@smu.edu>
2021-11-10 12:34:50 -0600

    Refactored her[2]k/syr[2]k in terms of gemmt. (#531)
    Details:
    - Renamed herk macrokernels and supporting files and functions to gemmt, 
      which is possible since at the macrokernel level they are identical. 
      Then recast herk/her2k/syrk/syr2k in terms of gemmt within the expert
      level-3 oapi (bli_l3_oapi_ex.c) while also redefining them as literal
      functions rather than cpp macros that instantiate multiple functions.
      Thanks to Devin Matthews for his efforts on this issue (#531).
    - Check that the maximum stack buffer size is sufficiently large
      relative to the register blocksizes for each datatype, and do so when
      the context is initialized rather than when an operation is called.
      Note that with this change, users who pass in their own contexts into
      the expert interfaces currently will *not* have any checks performed.
      Thanks to Devin Matthews for suggesting this change.

--- frame/3/bli_l3_check.c
+++ frame/3/bli_l3_check.c
@@ -597,10 +597,5 @@ void bli_l3_basic_check
 
 	e_val = bli_check_object_buffer( c );
 	bli_check_error_code( e_val );
-
-	// Check for sufficiently sized stack buffers
-
-	e_val = bli_check_sufficient_stack_buf_size( bli_obj_dt( a ), cntx );
-	bli_check_error_code( e_val );
 }
 
