8ce619a9666a37835ea9206e3b9d7a01dccf21ed
Meghana-vankadari <74656386+Meghana-vankadari@users.noreply.github.com>
2021-11-12 04:16:14 +0530

    Added BLAS/CBLAS APIs for axpby, gemm_batch. (#566)
    Details:
    - Expanded the BLAS compatibility layer to include support for 
      ?axpby_() and ?gemm_batch_(). The former is a straightforward
      BLAS-like interface into the axpbyv operation while the latter
      implements a batched gemm via loops over bli_?gemm(). Also
      expanded the CBLAS compatibility layer to include support for
      cblas_?axpby() and cblas_?gemm_batch(), which serve as wrappers to 
      the corresponding (new) BLAS-like APIs. Thanks to Meghana Vankadari
      for submitting these new APIs via #566.
    - Fixed a long-standing bug in common.mk that for some reason never
      manifested until now. Previously, CBLAS source files were compiled
      *without* the location of cblas.h being specified via a -I flag.
      I'm not sure why this worked, but it may be due to the fact that
      the cblas.h file resided in the same directory as all of the CBLAS
      source, and perhaps compilers implicitly add a -I flag for the
      directory that corresponds to the location of the source file being
      compiled. This bug only showed up because some CBLAS-like source code
      was moved into an 'extra' subdirectory of that frame/compat/cblas/src
      directory. After moving the code, compilation for those files failed
      (because the cblas.h header file, presumably, could not be found in
      the same location). This bug was fixed within common.mk by explicitly
      adding the cblas.h directory to the list of -I flags passed to the
      compiler.
    - Added test_axpbyv.c and test_gemm_batch.c files to 'test' directory,
      and updated test/Makefile to build those drivers.
    - Fixed typo in error message string in cblas_sgemm.c.

---
+++ frame/compat/cblas/src/extra/cblas_caxpby.c
@@ -0,0 +1,27 @@
+#include "blis.h"
+#ifdef BLIS_ENABLE_CBLAS
+/*
+ * cblas_caxpby.c
+ *
+ * The program is a C interface to caxpby.
+ *
+ * Copyright (C) 2020, Advanced Micro Devices, Inc
+ *
+ */
+#include "cblas.h"
+#include "cblas_f77.h"
+void cblas_caxpby( f77_int N, const void *alpha,
+               const void *X, f77_int incX,
+               const void *beta,
+               void *Y, f77_int incY)
+{
+#ifdef F77_INT
+   F77_INT F77_N=N, F77_incX=incX, F77_incY=incY;
+#else 
+   #define F77_N N
+   #define F77_incX incX
+   #define F77_incY incY
+#endif
+   F77_caxpby( &F77_N, (scomplex*)alpha, (scomplex*)X, &F77_incX, (scomplex*)beta, (scomplex*)Y, &F77_incY);
+} 
+#endif
