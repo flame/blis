466b68a3ad118342dc49a8130b7b02f5e7748521
Devin Matthews <damatthews@smu.edu>
2022-01-02 14:59:41 -0600

    Add unique tag to branch labels for Apple ARM64.
    Add `%=` tag to branch labels, which expands to a unique identifier for each inline assembly block. This prevents duplicate symbol errors on Apple Silicon (#594). Fixes #594. [ci skip] since we can't test Apple Silicon anyways...

--- kernels/armv8a/3/armv8a_asm_utils.h
+++ kernels/armv8a/3/armv8a_asm_utils.h
@@ -36,10 +36,10 @@
 
 // Apple's local label requirements.
 #if defined(__APPLE__)
-#define LABEL(str) "   L" #str": \n\t"
-#define BEQ(str) "b.eq L" #str"  \n\t"
-#define BNE(str) "b.ne L" #str"  \n\t"
-#define BRANCH(str) "b L" #str"  \n\t"
+#define LABEL(str) "   L" #str"%=: \n\t"
+#define BEQ(str) "b.eq L" #str"%=  \n\t"
+#define BNE(str) "b.ne L" #str"%=  \n\t"
+#define BRANCH(str) "b L" #str"%=  \n\t"
 #else
 #define LABEL(str) "   ." #str": \n\t"
 #define BEQ(str) "b.eq ." #str"  \n\t"
