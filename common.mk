0ca03130935940dbc86dc82171e977b5d211b688
Devin Matthews <damatthews@smu.edu>
2022-01-31 10:47:31 -0600

    Add compile-time constants (macros) for the MR and NR block sizes.
    These are defined in sub-configuration-specific header files, which are only included by reference kernels.

--- common.mk
+++ common.mk
@@ -129,6 +129,7 @@ get-refkern-cflags-for   = $(strip $(call load-var-for,CROPTFLAGS,$(1)) \
                                    -DBLIS_CNAME=$(1) \
                                    $(BUILD_CPPFLAGS) \
                                    $(BUILD_SYMFLAGS) \
+                                   -include $(CONFIG_PATH)/$(1)/bli_kernel_defs_$(1).h \
                             )
 
 get-config-cflags-for    = $(strip $(call load-var-for,COPTFLAGS,$(1)) \
