3e259b69ccb7ea8ffc5b4beb5c4c11a5722354bd
RuQing Xu <r-xu@g.ecc.u-tokyo.ac.jp>
2022-08-01 22:24:10 +0900

    Armv8A Add Row-Major Kernels
    Basically it's a templated re-implementation of current 6x8's transpose.

--- kernels/armv8a/bli_kernels_armv8a.h
+++ kernels/armv8a/bli_kernels_armv8a.h
@@ -39,6 +39,8 @@ PACKM_KER_PROT( double,   d, packm_armv8a_int_8xk )
 
 GEMM_UKR_PROT( float,    s, gemm_armv8a_asm_8x12 )
 GEMM_UKR_PROT( double,   d, gemm_armv8a_asm_6x8 )
+GEMM_UKR_PROT( float,    s, gemm_armv8a_asm_12x8r )
+GEMM_UKR_PROT( double,   d, gemm_armv8a_asm_8x6r )
 // GEMM_UKR_PROT( double,   d, gemm_armv8a_asm_6x8r )
 // GEMM_UKR_PROT( double,   d, gemm_armv8a_asm_8x4 )
 // GEMM_UKR_PROT( double,   d, gemm_armv8a_asm_4x4 )
