42d631bced3554f8828917eaf82b7f6a9f94796b
Harihara Sudhan S <hariharasudhan.s@amd.com>
2022-10-13 06:35:47 +0000

    Copyright modification
    	- Added copyright information to modified/newly created
              files missing them
    
    Change-Id: If4e73b680246d0363de09587d6dc54bee00ecd71

--- frame/base/bli_cntx.h
+++ frame/base/bli_cntx.h
@@ -6,7 +6,7 @@
 
    Copyright (C) 2014, The University of Texas at Austin
    Copyright (C) 2016, Hewlett Packard Enterprise Development LP
-   Copyright (C) 2020 - 21, Advanced Micro Devices, Inc. All rights reserved.
+   Copyright (C) 2020 - 22, Advanced Micro Devices, Inc. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
--- kernels/haswell/3/bli_gemm_haswell_asm_d6x8.c
+++ kernels/haswell/3/bli_gemm_haswell_asm_d6x8.c
@@ -5,7 +5,7 @@
    libraries.
 
    Copyright (C) 2014, The University of Texas at Austin
-   Copyright (C) 2018 - 2021, Advanced Micro Devices, Inc.All rights reserved.
+   Copyright (C) 2018 - 2022, Advanced Micro Devices, Inc.All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
