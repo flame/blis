flame/blis

commit a1a5a9b4cbef9208da494c45a2f933a8e82559ac

Implemented support for fat multithreading. (#665)
Details:
- Allow the user to configure BLIS in such a way that multiple threading
  implementations get compiled into the library, with one of those
  implementations chosen at runtime. For now, there are only three
  implementations available: OpenMP, pthreads, and single. (Here,
  'single' merely refers to single-threaded mode.) The configure script
  now allows the user to give the -t option with a comma-separated list
  of values, such as '-t openmp,pthreads'. The first value in the list
  will always be the default at library initialization time, and
  'single' is always silently appended to the end of the list. The user
  can specify which implementation should execute in one of three ways:
  by setting the BLIS_THREAD_IMPL environment variable prior to launch;
  by calling the bli_thread_set_thread_impl() global runtime API; or by
  encoding their choice into a rntm_t that is passed into one of the
  expert interfaces. Any of these three choices overrides the
  initialization-time default (i.e., the first value listed to the -t
  configure option). Requesting an implementation that was not compiled
  into the library will result in an error message followed by
  bli_abort().
- Relocated the 'auto' logic for the -t option from the top-level
  Makefile to the configure script. (Currently, this logic is pretty
  dumb, choosing 'openmp' for gcc and icc, and 'pthreads' for clang.)
- Defined a new 'timpl_t' enum in bli_type_defs.h, with three valid
  values: BLIS_SINGLE, BLIS_OPENMP, BLIS_POSIX.
- Reorganized the thrcomm_t struct into a single defintion with two
  preprocessor blocks, one each for additional fields needed by OpenMP
  and pthreads.
- Added timpl_t argument to bli_thrcomm_bcast(), bli_thrcomm_barrier(),
  bli_thrcomm_init(), and bli_thrcomm_cleanup(), which these functions
  need since they are now wrappers that choose the implementation-

frame/thread/bli_thrcomm.c
@@ -35,8 +35,183 @@
 
 #include "blis.h"
 
+// -- Method-agnostic functions ------------------------------------------------
+
+thrcomm_t* bli_thrcomm_create( rntm_t* rntm, dim_t n_threads )
+{
+	#ifdef BLIS_ENABLE_MEM_TRACING
+	printf( "bli_thrcomm_create(): " );
+	#endif
+
+	thrcomm_t* comm = bli_sba_acquire( rntm, sizeof(thrcomm_t) );
+
+	const timpl_t ti = bli_rntm_thread_impl( rntm );
+
+	bli_thrcomm_init( ti, n_threads, comm );
+
+	return comm;
+}
+
+void bli_thrcomm_free( rntm_t* rntm, thrcomm_t* comm )
+{
+	if ( comm == NULL ) return;
+
+	const timpl_t ti = bli_rntm_thread_impl( rntm );
+
+	bli_thrcomm_cleanup( ti, comm );
+
+	#ifdef BLIS_ENABLE_MEM_TRACING
+	printf( "bli_thrcomm_free(): " );
+	#endif
+
+	bli_sba_release( rntm, comm );
+}
+
+// -- Method-specific functions ------------------------------------------------
+
+// Initialize a function pointer array for each family of threading-specific
+// functions (init, cleanup, and barrier).
+
+static thrcomm_init_ft init_fpa[ BLIS_NUM_THREAD_IMPLS ] =
+{
+	[BLIS_SINGLE] = bli_thrcomm_init_single,
+	[BLIS_OPENMP] =
+#if   defined(BLIS_ENABLE_OPENMP)
+	                bli_thrcomm_init_openmp,
+#elif defined(BLIS_ENABLE_PTHREADS)
+	                NULL,
+#else
+	                NULL,
+#endif
+	[BLIS_POSIX]  =
+#if   defined(BLIS_ENABLE_PTHREADS)
+	                bli_thrcomm_init_pthreads,
+#elif defined(BLIS_ENABLE_OPENMP)
+	                NULL,
+#else
+	                NULL,
+#endif
+};
+static thrcomm_cleanup_ft cleanup_fpa[ BLIS_NUM_THREAD_IMPLS ] =
+{
+	[BLIS_SINGLE] = bli_thrcomm_cleanup_single,
+	[BLIS_OPENMP] =
+#if   defined(BLIS_ENABLE_OPENMP)
+	                bli_thrcomm_cleanup_openmp,
+#elif defined(BLIS_ENABLE_PTHREADS)
+	                NULL,
+#else
+	                NULL,
+#endif
+	[BLIS_POSIX]  =
+#if   defined(BLIS_ENABLE_PTHREADS)
+	                bli_thrcomm_cleanup_pthreads,
+#elif defined(BLIS_ENABLE_OPENMP)
+	                NULL,
+#else
+	                NULL,
+#endif
+};
+static thrcomm_barrier_ft barrier_fpa[ BLIS_NUM_THREAD_IMPLS ] =
+{
+	[BLIS_SINGLE] = bli_thrcomm_barrier_single,
+	[BLIS_OPENMP] =
+#if   defined(BLIS_ENABLE_OPENMP)
+	                bli_thrcomm_barrier_openmp,
+#elif defined(BLIS_ENABLE_PTHREADS)
+	                bli_thrcomm_barrier_pthreads,
+#else
+	                bli_thrcomm_barrier_single,
+#endif
+	[BLIS_POSIX]  =
+#if   defined(BLIS_ENABLE_PTHREADS)
+	                bli_thrcomm_barrier_pthreads,
+#elif defined(BLIS_ENABLE_OPENMP)
+	                bli_thrcomm_barrier_openmp,
+#else
+	                bli_thrcomm_barrier_single,
+#endif
+};
+
+// Define dispatchers that choose a threading-specific function from each
+// of the above function pointer arrays.
+
+void bli_thrcomm_init( timpl_t ti, dim_t nt, thrcomm_t* comm )
+{
+	const thrcomm_init_ft fp = init_fpa[ ti ];
+
+	if ( fp == NULL ) bli_abort();
+
+	// Call the threading-specific init function.
+	fp( nt, comm );
+
+	// Embed the type of threading implementation within the thrcomm_t struct.
+	// This can be used later to make sure the application doesn't use a
+	// thrcomm_t initialized with threading type A with the API for threading
+	// type B. Note that we wait until after the init function has returned
+	// in case that function zeros out the entire struct before setting the
+	// fields.
+	comm->ti = ti;
+}
+
+void bli_thrcomm_cleanup( timpl_t ti, thrcomm_t* comm )
+{
+	const thrcomm_cleanup_ft fp = cleanup_fpa[ ti ];
+
+	if ( fp == NULL ) bli_abort();
+
+	// If comm is BLIS_SINGLE_COMM, we return early since there is no cleanup,
+	// especially if it is being used with a threading implementation that
+	// would normally want to free its thrcomm_t resources.
+	if ( comm == &BLIS_SINGLE_COMM ) return;
+
+	// Sanity check. Make sure the threading implementation we were asked to use
+	// is the same as the implementation that initialized the thrcomm_t object.
+	if ( ti != comm->ti )
+	{
+		printf( "bli_thrcomm_cleanup(): thrcomm_t.ti = %s, but request via rntm_t.ti = %s\n",
+		        ( comm->ti == BLIS_SINGLE ? "single" :
+		        ( comm->ti == BLIS_OPENMP ? "openmp" : "pthreads" ) ),
+		        ( ti       == BLIS_SINGLE ? "single" :
+		        ( ti       == BLIS_OPENMP ? "openmp" : "pthreads" ) ) );
+		bli_abort();
+	}
+
+	// Call the threading-specific cleanup function.
+	fp( comm );
+}
+
+void bli_thrcomm_barrier( timpl_t ti, dim_t tid, thrcomm_t* comm )
+{
+	const thrcomm_barrier_ft fp = barrier_fpa[ ti ];
+
+	if ( fp == NULL ) bli_abort();
+
+	// Sanity check. Make sure the threading implementation we were asked to use
+	// is the same as the implementation that initialized the thrcomm_t object.
+	// We skip this check if comm is BLIS_SINGLE_COMM since the timpl_t value
+	// embedded in comm will often be different than that of BLIS_SINGLE_COMM
+	// (but we don't return early since we still need to barrier... wait, or do
+	// we?).
+	if ( ti != comm->ti && comm != &BLIS_SINGLE_COMM )
+	{
+		printf( "bli_thrcomm_barrier(): thrcomm_t.ti = %s, but request via rntm_t.ti = %s\n",
+		        ( comm->ti == BLIS_SINGLE ? "single" :
+		        ( comm->ti == BLIS_OPENMP ? "openmp" : "pthreads" ) ),
+		        ( ti       == BLIS_SINGLE ? "single" :
+		        ( ti       == BLIS_OPENMP ? "openmp" : "pthreads" ) ) );
+		bli_abort();
+	}
+
+	// Call the threading-specific barrier function.
+	fp( tid, comm );
+}
+
+// -- Other functions ----------------------------------------------------------
+
 void* bli_thrcomm_bcast
      (
+       timpl_t    ti,
        dim_t      id,
        void*      to_send,
        thrcomm_t* comm
@@ -46,9 +221,9 @@ void* bli_thrcomm_bcast
 
 	if ( id == 0 ) comm->sent_object = to_send;
 
-	bli_thrcomm_barrier( id, comm );
+	bli_thrcomm_barrier( ti, id, comm );
 	void* object = comm->sent_object;
-	bli_thrcomm_barrier( id, comm );
+	bli_thrcomm_barrier( ti, id, comm );
 
 	return object;
 }

commit 5af11fbf1284c3532ca4210225a86e76bd634926

Fixed subtle barrier_fpa bug in bli_thrcomm.c.
Details:
- In bli_thrcommo.c, correctly initialize the BLIS_OPENMP element of the
  barrier function pointer array (barrier_fpa) to NULL when
  BLIS_ENABLE_OPENMP is *not* defined. Similarly, initialize the
  BLIS_POSIX element of barrier_fpa to NULL when BLIS_ENABLE_PTHREADS is
  not enabled. This bug was introduced in a1a5a9b and was likely the
  result of an incomplete edit. The effects of the bug would have
  likely manifested when querying a thrcomm_t that was initialized with
  a timpl_t value corresponding to a threading implementation that was
  omitted from the -t option at configure-time.

frame/thread/bli_thrcomm.c
@@ -115,17 +115,17 @@ static thrcomm_barrier_ft barrier_fpa[ BLIS_NUM_THREAD_IMPLS ] =
 #if   defined(BLIS_ENABLE_OPENMP)
 	                bli_thrcomm_barrier_openmp,
 #elif defined(BLIS_ENABLE_PTHREADS)
-	                bli_thrcomm_barrier_pthreads,
+	                NULL,
 #else
-	                bli_thrcomm_barrier_single,
+	                NULL,
 #endif
 	[BLIS_POSIX]  =
 #if   defined(BLIS_ENABLE_PTHREADS)
 	                bli_thrcomm_barrier_pthreads,
 #elif defined(BLIS_ENABLE_OPENMP)
-	                bli_thrcomm_barrier_openmp,
+	                NULL,
 #else
-	                bli_thrcomm_barrier_single,
+	                NULL,
 #endif
 };
 