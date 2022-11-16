aaf007fdfe025db6794417b94bcd4149a7f5d08c
Angelika Schwarz <17718454+angsch@users.noreply.github.com>
2022-11-16 20:29:28 +0100

    Prepare RISC-V infrastructure, add sgemm
    Prepare four RISC-V configurations:
     * RV32i
     * RV64i
     * RV32iv
     * RV64iv (v implies f, d)
    
    where
     * i: base integer instruction set
     * f: scalar single-precision floating point instructions
     * d: scalar double-precision floating-point instructions
     * v: vector extensions as defined in RVV 1.0
    
    Note that atomic instructions are required either
    as hardware instructions (-march=rv64iav etc.) or
    by software emulation (link against libatomic).

--- frame/include/bli_arch_config.h
+++ frame/include/bli_arch_config.h
@@ -131,6 +131,22 @@ CNTX_INIT_PROTS( power7 )
 CNTX_INIT_PROTS( bgq )
 #endif
 
+// -- RISC-V --
+
+#ifdef BLIS_CONFIG_RV32I
+CNTX_INIT_PROTS( rv32i )
+#endif
+#ifdef BLIS_CONFIG_RV64I
+CNTX_INIT_PROTS( rv64i )
+#endif
+#ifdef BLIS_CONFIG_RV32IV
+CNTX_INIT_PROTS( rv32iv )
+#endif
+#ifdef BLIS_CONFIG_RV64IV
+CNTX_INIT_PROTS( rv64iv )
+#endif
+
+
 // -- Generic --
 
 #ifdef BLIS_CONFIG_GENERIC
@@ -343,6 +359,12 @@ CNTX_INIT_PROTS( generic )
 #endif
 
 
+#ifdef BLIS_KERNELS_RVI
+#include "bli_kernels_rvi.h"
+#endif
+#ifdef BLIS_KERNELS_RVIV
+#include "bli_kernels_rviv.h"
+#endif
 
 #endif
 
