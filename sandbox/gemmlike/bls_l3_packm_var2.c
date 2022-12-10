4e7ed3082e0a1eb5198987fc6e2a69537452fe82
Field G. Van Zee <field@cs.utexas.edu>
2022-12-10 16:38:21 -0600

    Fixed func calls in gemmlike/bls_l3_packm_var?.c.
    Details:
    - Updated bls_l3_packm_var[123].c to use bli_thrinfo_n_way() and
      bli_thrinfo_work_id() instead of bli_thrinfo_num_threads() and
      bli_thrinfo_thread_id(), respectively. This change probably should
      have been included in aeb5f0c.
    - This change is the first of multiple that are necessary to get the
      gemmlike sandbox operating at full performance again. (It appears
      each thread is packing every micropanel, and so the thrinfo_t tree
      needs to be reconfigured. Same goes for sup when optional packing
      is requested.)

--- sandbox/gemmlike/bls_l3_packm_var2.c
+++ sandbox/gemmlike/bls_l3_packm_var2.c
@@ -121,8 +121,8 @@ void PASTECH2(bls_,ch,varname) \
 \
 	/* Query the number of threads and thread ids from the current thread's
 	   packm thrinfo_t node. */ \
-	const dim_t nt  = bli_thrinfo_num_threads( thread ); \
-	const dim_t tid = bli_thrinfo_thread_id( thread ); \
+	const dim_t nt  = bli_thrinfo_n_way( thread ); \
+	const dim_t tid = bli_thrinfo_work_id( thread ); \
 \
 	/* Suppress warnings in case tid isn't used (ie: as in slab partitioning). */ \
 	( void )nt; \
