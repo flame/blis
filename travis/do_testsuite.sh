97de0445f8a39129adf62355cd03dda9766dee1e
Field G. Van Zee <field@cs.utexas.edu>
2023-02-15 13:32:25 -0600

    Whitespace changes.

--- travis/do_testsuite.sh
+++ travis/do_testsuite.sh
@@ -9,25 +9,25 @@ export BLIS_JR_NT=1
 export BLIS_IR_NT=1
 
 if [ "$TEST" = "FAST" -o "$TEST" = "ALL" ]; then
-    make testblis-fast || cat ./output.testsuite
-    $DIST_PATH/testsuite/check-blistest.sh ./output.testsuite
+	make testblis-fast || cat ./output.testsuite
+	$DIST_PATH/testsuite/check-blistest.sh ./output.testsuite
 fi
 
 if [ "$TEST" = "MD" -o "$TEST" = "ALL" ]; then
 	make testblis-md || cat ./output.testsuite
-    $DIST_PATH/testsuite/check-blistest.sh ./output.testsuite
+	$DIST_PATH/testsuite/check-blistest.sh ./output.testsuite
 fi
 
 if [ "$TEST" = "SALT" -o "$TEST" = "ALL" ]; then
 	# Disable multithreading within BLIS.
 	export BLIS_JC_NT=1 BLIS_IC_NT=1 BLIS_JR_NT=1 BLIS_IR_NT=1
 	make testblis-salt || cat ./output.testsuite
-    $DIST_PATH/testsuite/check-blistest.sh ./output.testsuite
+	$DIST_PATH/testsuite/check-blistest.sh ./output.testsuite
 fi
 
 if [ "$TEST" = "1" -o "$TEST" = "ALL" ]; then
-    make testblis || cat ./output.testsuite
-    $DIST_PATH/testsuite/check-blistest.sh ./output.testsuite
+	make testblis || cat ./output.testsuite
+	$DIST_PATH/testsuite/check-blistest.sh ./output.testsuite
 fi
 
 make testblas || cat ./output.testsuite
