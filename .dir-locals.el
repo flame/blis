0ba6e9eafb1e667373d9dbc2aa045557921f33e2
Lee Killough <15950023+leekillough@users.noreply.github.com>
2023-02-18 13:15:42 -0600

    Refined emacs handling of indentation. (#717)
    Details:
    - This refines the emacs autoformatting to be better in line with 
      contribution guidelines.
    - Removed a stray shebang in a .mk file which confuses emacs about the
      file mode, which should be makefile-mode. (emacs also removes stray 
      whitespace at the ends of lines.)

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,11 +1,32 @@
-;; Emacs C mode formatting for the BLIS layout requirements.
-((c-mode . ((c-file-style . "stroustrup")
-	    (c-basic-offset . 4)
-	    (comment-start . "// ")
-	    (comment-end . "")
-	    (indent-tabs-mode . t)
-	    (tab-width . 4)
-	    (parens-require-spaces . nil)
-	    (require-final-newline . t)
-	    (eval add-hook `before-save-hook `delete-trailing-whitespace)
-	    )))
+;; Emacs formatting for the BLIS layout requirements.
+
+(
+ ;; Recognize *.mk files as Makefile fragments
+ (auto-mode-alist . (("\\.mk\\'" . makefile-mode)) )
+
+ ;; Makefiles require tabs and are almost always width 8
+ (makefile-mode . (
+                   (indent-tabs-mode . t)
+                   (tab-width . 8)
+                   )
+                )
+
+ ;; C code formatting roughly according to docs/CodingConventions.md
+ (c-mode . (
+            (c-file-style . "bsd")
+            (c-basic-offset . 4)
+            (comment-start . "// ")
+            (comment-end . "")
+            (parens-require-spaces . nil)
+            )
+         )
+
+ ;; Default formatting for all source files not overriden above
+ (prog-mode . (
+               (indent-tabs-mode . nil)
+               (tab-width . 4)
+               (require-final-newline . t)
+               (eval add-hook `before-save-hook `delete-trailing-whitespace)
+               )
+            )
+)
