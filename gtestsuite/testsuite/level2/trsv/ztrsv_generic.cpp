bd765a4bc7367963cb3c17a6a0d7e2459426917a
Shubham Sharma <Shubham.Sharma3@amd.com>
2024-03-13 14:53:51 +0530

    GTestSuite: Added Tests for ZTRSV
      - Added API tests for ZTRSV.
      - Added Extreme Value Test cases (EVT) for ZTRSV.
        - Tests for various combinations of INFs
           and NANs for X vector and B matrix are added.
      - Added memory testing for ZTRSV
        API.
    
    AMD-Internal: [CPUPL-4716]
    Change-Id: I0291acaafa78073979c307a4cc9595d429229c0c

--- gtestsuite/testsuite/level2/trsv/ztrsv_generic.cpp
+++ gtestsuite/testsuite/level2/trsv/ztrsv_generic.cpp
@@ -35,17 +35,18 @@
 #include <gtest/gtest.h>
 #include "test_trsv.h"
 
-class ztrsvTest :
-        public ::testing::TestWithParam<std::tuple<char,
-                                                   char,
-                                                   char,
-                                                   char,
-                                                   gtint_t,
-                                                   dcomplex,
-                                                   gtint_t,
-                                                   gtint_t>> {};
+class ztrsvAPI :
+        public ::testing::TestWithParam<std::tuple<char,       // storage format
+                                                   char,       // uplo
+                                                   char,       // trans
+                                                   char,       // diag
+                                                   gtint_t,    // n
+                                                   dcomplex,   // alpha
+                                                   gtint_t,    // incx
+                                                   gtint_t,    // ld_inc
+                                                   bool>> {};  // is memory test
 
-TEST_P(ztrsvTest, RandomData)
+TEST_P(ztrsvAPI, FunctionalTest)
 {
     using T = dcomplex;
     //----------------------------------------------------------
@@ -54,9 +55,9 @@ TEST_P(ztrsvTest, RandomData)
     //----------------------------------------------------------
     // matrix storage format(row major, column major)
     char storage = std::get<0>(GetParam());
-    // denotes whether matrix a is u,l
+    // denotes whether matrix A is u,l
     char uploa = std::get<1>(GetParam());
-    // denotes whether matrix a is n,c,t,h
+    // denotes whether matrix A is n,c,t,h
     char transa = std::get<2>(GetParam());
     // denotes whether matrix diag is u,n
     char diaga = std::get<3>(GetParam());
@@ -64,12 +65,13 @@ TEST_P(ztrsvTest, RandomData)
     gtint_t n  = std::get<4>(GetParam());
     // specifies alpha value
     T alpha = std::get<5>(GetParam());
-    // stride size for x:
+    // increment for x (incx):
     gtint_t incx = std::get<6>(GetParam());
     // lda increment.
     // If increment is zero, then the array size matches the matrix size.
     // If increment are nonnegative, the array size is bigger than the matrix size.
     gtint_t lda_inc = std::get<7>(GetParam());
+    bool is_mem_test = std::get<8>(GetParam());
 
     // Set the threshold for the errors:
     // Check gtestsuite trsv.h or netlib source code for reminder of the
@@ -85,63 +87,81 @@ TEST_P(ztrsvTest, RandomData)
     //----------------------------------------------------------
     //     Call test body using these parameters
     //----------------------------------------------------------
-    test_trsv<T>( storage, uploa, transa, diaga, n, alpha, lda_inc, incx, thresh );
+    test_trsv<T>( storage, uploa, transa, diaga, n, alpha, lda_inc, incx, thresh, is_mem_test );
 }
 
-class ztrsvTestPrint {
+class ztrsvPrint {
 public:
     std::string operator()(
-        testing::TestParamInfo<std::tuple<char,char,char,char,gtint_t,dcomplex,gtint_t,gtint_t>> str) const {
-        char sfm       = std::get<0>(str.param);
-        char uploa     = std::get<1>(str.param);
-        char transa    = std::get<2>(str.param);
-        char diaga     = std::get<3>(str.param);
-        gtint_t n      = std::get<4>(str.param);
-        dcomplex alpha = std::get<5>(str.param);
-        gtint_t incx   = std::get<6>(str.param);
-        gtint_t ld_inc = std::get<7>(str.param);
+        testing::TestParamInfo<std::tuple<char,char,char,char,gtint_t,dcomplex,gtint_t,gtint_t,bool>> str) const {
+        char sfm         = std::get<0>(str.param);
+        char uploa       = std::get<1>(str.param);
+        char transa      = std::get<2>(str.param);
+        char diaga       = std::get<3>(str.param);
+        gtint_t n        = std::get<4>(str.param);
+        dcomplex alpha   = std::get<5>(str.param);
+        gtint_t incx     = std::get<6>(str.param);
+        gtint_t ld_inc   = std::get<7>(str.param);
+        bool is_mem_test = std::get<8>(str.param);
 #ifdef TEST_BLAS
-        std::string str_name = "ztrsv_";
+        std::string str_name = "blas_";
 #elif TEST_CBLAS
-        std::string str_name = "cblas_ztrsv";
+        std::string str_name = "cblas_";
 #else  //#elif TEST_BLIS_TYPED
-        std::string str_name = "bli_ztrsv";
+        std::string str_name = "bli_";
 #endif
-        str_name    = str_name + "_" + sfm;
-        str_name    = str_name + "_" + uploa+transa;
-        str_name    = str_name + "_d" + diaga;
-        str_name    = str_name + "_" + std::to_string(n);
-        std::string alpha_str = ( alpha.real > 0) ? std::to_string(int(alpha.real)) : ("m" + std::to_string(int(std::abs(alpha.real))));
-                    alpha_str = alpha_str + "pi" + (( alpha.imag > 0) ? std::to_string(int(alpha.imag)) : ("m" + std::to_string(int(std::abs(alpha.imag)))));
-        str_name    = str_name + "_a" + alpha_str;
-        std::string incx_str = ( incx > 0) ? std::to_string(incx) : "m" + std::to_string(std::abs(incx));
-        str_name    = str_name + "_" + incx_str;
-        str_name    = str_name + "_" + std::to_string(ld_inc);
+        str_name    = str_name + "stor_" + sfm;
+        str_name    = str_name + "_uplo_" + uploa;
+        str_name    = str_name + "_transa_" + transa;
+        str_name    = str_name + "_diaga_" + diaga;
+        str_name    = str_name + "_n_" + std::to_string(n);
+        std::string alpha_str = testinghelpers::get_value_string(alpha);
+        str_name    = str_name + "_alpha_" + alpha_str;
+        std::string incx_str = ( incx >= 0) ? std::to_string(incx) : "m" + std::to_string(std::abs(incx));
+        str_name    = str_name + "_incx_" + incx_str;
+        str_name    = str_name + "_lda_" + std::to_string(
+                    testinghelpers::get_leading_dimension( sfm, transa, n, n, ld_inc )
+                );
+        str_name    = str_name + (is_mem_test ? "_mem_test_enabled" : "_mem_test_disabled");
         return str_name;
     }
 };
 
-// Black box testing.
 INSTANTIATE_TEST_SUITE_P(
-        Blackbox,
-        ztrsvTest,
+        Native,
+        ztrsvAPI,
         ::testing::Combine(
             ::testing::Values('c'
 #ifndef TEST_BLAS
-            ,'r'
+                             ,'r'
 #endif
             ),                                                               // storage format
             ::testing::Values('u','l'),                                      // uploa
             ::testing::Values('n','c','t'),                                  // transa
             ::testing::Values('n','u'),                                      // diaga , n=NONUNIT_DIAG u=UNIT_DIAG
-            ::testing::Range(gtint_t(10), gtint_t(31), 10),                  // n
-            ::testing::Values(dcomplex{1.0, 0.0}
+            ::testing::Values( gtint_t(32),
+                               gtint_t(24),
+                               gtint_t(8),
+                               gtint_t(4),
+                               gtint_t(2),
+                               gtint_t(1),
+                               gtint_t(15),
+                               gtint_t(98),
+                               gtint_t(173),
+                               gtint_t(211) ),                               // n (random values)
+            ::testing::Values(dcomplex{1.0, 0.0}                             // APIs other than BLIS TYPED support Alpha = 1 only
 #ifdef TEST_BLIS_TYPED
-            ,dcomplex{1.0, -2.0}
+            ,dcomplex{6.1, -2.9}, dcomplex{-3.3, -1.4}
+            ,dcomplex{-1.0, 0.0}, dcomplex{0.0, 0.0}
+
 #endif
             ),                                                               // alpha
-            ::testing::Values(gtint_t(1)),                                   // stride size for x
-            ::testing::Values(gtint_t(0), gtint_t(1))                        // increment to the leading dim of a
+            ::testing::Values(gtint_t(-153), gtint_t(-10),
+                              gtint_t(-2), gtint_t(-1),
+                              gtint_t( 1), gtint_t( 2),
+                              gtint_t(14), gtint_t(433)),                    // incx
+            ::testing::Values(gtint_t(0), gtint_t(10), gtint_t(358)),        // increment to the leading dim of a
+            ::testing::Values(false, true)                                   // is memory test
         ),
-        ::ztrsvTestPrint()
+        ::ztrsvPrint()
     );
