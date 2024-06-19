415893066e966159799d96166cadcf9bb5535b1c
Devin Matthews <damatthews@smu.edu>
2024-06-18 22:03:32 -0500

    Add ScaLAPACK compatibility mode. (#813)
    Details:
    - Add configure options `--enable-scalapack-compat` and `--disabled-scalapack-compat`
      (default disabled).
    - Add a macro `BLIS_{ENABLE,DISABLE}_SCALAPACK_COMPAT` to bli_config.h.
    - This option and macro control any changes to the API necessary to maintain
      compatibility with ScaLAPACK. Currently, this only means disabling the complex
      versions of `syr`, `syr2`, and `symv`. In the future, other changes could be
      controlled by the same flag.
    - Complex `syr2` wasn't enabled at the same time that complex `syr` and `symv` were.
      This is now corrected.

--- frame/compat/bla_syr.h
+++ frame/compat/bla_syr.h
@@ -38,6 +38,8 @@
 // Prototype BLAS-to-BLIS interfaces.
 //
 #undef  GENTPROT
+#undef  GENTPROTRO
+#define GENTPROTRO GENTPROT
 #define GENTPROT( ftype, ch, blasname ) \
 \
 BLIS_EXPORT_BLAS void PASTEF77(ch,blasname) \
@@ -50,7 +52,11 @@ BLIS_EXPORT_BLAS void PASTEF77(ch,blasname) \
      );
 
 #ifdef BLIS_ENABLE_BLAS
+#ifdef BLIS_ENABLE_SCALAPACK_COMPAT
+INSERT_GENTPROTRO_BLAS( syr )
+#else
 INSERT_GENTPROT_BLAS( syr )
+#endif
 #endif
 
 #endif
