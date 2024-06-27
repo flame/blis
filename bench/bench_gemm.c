2ac24d1f9cbc548b50bb887291a59c740f0304c1
Varaganti, Kiran <kiran.varaganti@amd.com>
2024-06-27 14:26:04 +0530

    Avoided Extra copy of "c" matrix
    Initailized c_save instead of 'c" and then removed copying c to c_save.
    Because at the start every n_repeats iteration we are copying back c_save to c.
    Therefore if we initialize c_save, we can avoid extra copy of "c" to c_save before calling
    GEMM. For very large sizes matrix initialization takes considerable amount of time. This can
    be reduced now.
    
    Change-Id: I2c6ffe169e991607314897cb0c1fbfc0d74ef179

--- bench/bench_gemm.c
+++ bench/bench_gemm.c
@@ -5,7 +5,7 @@
    libraries.
 
    Copyright (C) 2014, The University of Texas at Austin
-   Copyright (C) 2020 - 2023, Advanced Micro Devices, Inc. All rights reserved.
+   Copyright (C) 2020 - 2024, Advanced Micro Devices, Inc. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -240,7 +240,7 @@ int main( int argc, char** argv )
 #ifdef AOCL_MATRIX_INITIALISATION
         bli_randm( &a );
         bli_randm( &b );
-        bli_randm( &c );
+        bli_randm( &c_save );
 #endif
 
         bli_obj_set_conjtrans( transa, &a);
@@ -249,7 +249,7 @@ int main( int argc, char** argv )
         bli_setsc( alpha_r, alpha_i, &alpha );
         bli_setsc( beta_r, beta_i, &beta );
 
-        bli_copym( &c, &c_save );
+        // bli_copym( &c, &c_save );
 
         dtime_save = DBL_MAX;
 
