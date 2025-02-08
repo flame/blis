e5400a74c3e681357031d27eb375b1eef7cae2f6
Devin Matthews <damatthews@smu.edu>
2025-02-08 13:47:08 -0600

    Add CircleCI (#855)
    Details:
    - This PR adds CircleCI testing in addition to TravisCI and Appveyor.
    - All of the same tests as on Travis are run, except that different hardware typically ends up being used (usually Zen on Travis, Xeon Platinum on Circle). This has actually exposed a couple of bugs (see #850 and #852).
    - The `travis` directory has been renamed to `ci` as it is now shared.
    - Running SDE on CircleCI is a bit problematic because glibc changed how CPUID detection is done. This requires running some architectures with different hardware definition files and forcing a config via `BLIS_ARCH_TYPE`.
    
    (cherry picked from commit 40a52dc0289f27f74a43e886ae14bf11738db169)

---
+++ ci/do_sde.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+set -e
+set -x
+
+SDE_VERSION=sde-external-8.69.1-2021-07-18-lin
+SDE_TARBALL=$SDE_VERSION.tar.bz2
+SDE=$SDE_VERSION/sde64
+
+#
+# This doesn't seem to be necessary anymore
+#
+#curl --verbose --form accept_license=1 --form form_id=intel_licensed_dls_step_1 \
+#     --output /dev/null --cookie-jar jar.txt \
+#     --location https://software.intel.com/protected-download/267266/144917
+#curl --verbose --cookie jar.txt --output $SDE_TARBALL \
+#     https://software.intel.com/system/files/managed/2a/1a/$SDE_TARBALL
+
+#curl --verbose --output $SDE_TARBALL \
+#     https://software.intel.com/content/dam/develop/external/us/en/documents/downloads/$SDE_TARBALL
+
+CI_UTILS=ci-utils
+CI_UTILS_URL=https://github.com/flame/${CI_UTILS}.git
+CI_UTILS_SDE_DIR=sde
+SDE_DIRPATH=$CI_UTILS/$CI_UTILS_SDE_DIR
+
+git clone $CI_UTILS_URL
+mv $SDE_DIRPATH/$SDE_TARBALL .
+
+tar xvf $SDE_TARBALL
+
+make -j2 testsuite-bin blastest-bin
+
+for ARCH in penryn sandybridge haswell skx knl piledriver steamroller excavator zen; do
+    export BLIS_ARCH_TYPE=-1
+
+    if [ "$ARCH" = "knl" ]; then
+        TESTSUITE_WRAPPER="$SDE -knl --"
+    elif [ "$ARCH" = "sandybridge" ]; then
+        # The sandybridge.def file causes a segfault in SDE on some systems.
+        # Instead, use the CPUID values for haswell, but force BLIS to use the
+        # sandybridge configuration.
+        TESTSUITE_WRAPPER="$SDE -cpuid_in $DIST_PATH/ci/cpuid/haswell.def --"
+        export BLIS_ARCH_TYPE=4
+    elif [ "$ARCH" = "piledriver" ]; then
+        # We used to "patch" ld.so and libm to remove CPUID checks so that glibc
+        # wouldn't try to use instructions not supported by SDE (FMA4). That no
+        # longer works, so test Piledriver/Steamroller/Excavator as haswell
+        # but with the configuration forced via environment variable.
+        TESTSUITE_WRAPPER="$SDE -cpuid_in $DIST_PATH/ci/cpuid/haswell.def --"
+        export BLIS_ARCH_TYPE=11
+    elif [ "$ARCH" = "steamroller" ]; then
+        TESTSUITE_WRAPPER="$SDE -cpuid_in $DIST_PATH/ci/cpuid/haswell.def --"
+        export BLIS_ARCH_TYPE=10
+    elif [ "$ARCH" = "excavator" ]; then
+        TESTSUITE_WRAPPER="$SDE -cpuid_in $DIST_PATH/ci/cpuid/haswell.def --"
+        export BLIS_ARCH_TYPE=9
+    else
+        TESTSUITE_WRAPPER="$SDE -cpuid_in $DIST_PATH/ci/cpuid/$ARCH.def --"
+    fi
+
+    make TESTSUITE_WRAPPER="$TESTSUITE_WRAPPER" check
+
+    TMP=`grep "active sub-configuration" output.testsuite`
+    CONFIG=${TMP##* }
+    if [ "$CONFIG" != "$ARCH" ]; then
+        echo "Wrong configuration chosen:"
+        echo "    Expected: $ARCH"
+        echo "    Got: $CONFIG"
+        exit 1
+    fi
+done
+
