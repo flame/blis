3b58cf4d51b990de260dccaa2c5987051ab19d2a
Devin Matthews <damatthews@smu.edu>
2025-02-28 11:11:20 -0600

    Check all threading backends in CI.

--- ci/do_testsuite.sh
+++ ci/do_testsuite.sh
@@ -7,11 +7,19 @@ export BLIS_JC_NT=1
 export BLIS_IC_NT=2
 export BLIS_JR_NT=1
 export BLIS_IR_NT=1
+export BLIS_THREAD_IMPL="single"
 
 if [ "$TEST" = "FAST" -o "$TEST" = "ALL" ]; then
 	make testblis-fast
 	cat ./output.testsuite
 	$DIST_PATH/testsuite/check-blistest.sh ./output.testsuite
+
+	for impl in $(echo $THR | sed 's/none//' | tr , ' '); do
+		export BLIS_THREAD_IMPL="$impl"
+		make testblis-fast
+		cat ./output.testsuite
+		$DIST_PATH/testsuite/check-blistest.sh ./output.testsuite
+	done
 fi
 
 if [ "$TEST" = "MD" -o "$TEST" = "ALL" ]; then
