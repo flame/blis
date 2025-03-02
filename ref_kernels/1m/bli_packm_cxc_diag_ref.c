0169f235a0f70e06c11c9cf555a007768576b851
Devin Matthews <damatthews@smu.edu>
2025-03-01 23:48:56 -0600

    Fix the reference packing kernels for broadcast packing. Mostly the problems were in the diagonal kernel.

--- ref_kernels/1m/bli_packm_cxc_diag_ref.c
+++ ref_kernels/1m/bli_packm_cxc_diag_ref.c
@@ -35,7 +35,98 @@
 #include "blis.h"
 
 
-#define PACKM_DIAG_BODY( ctypea, ctypep, cha, chp, mn_min, mn_max, dfac, inca, lda, op ) \
+#define PACKM_DIAG_DIAG_( ctypea, ctypep, ctypep_r, cha, chp, chp_r, dfac, inca, lda ) \
+\
+do \
+{ \
+	if ( bli_is_unit_diag( diaga ) ) \
+	{ \
+		for ( dim_t mnk = 0; mnk < cdim; ++mnk ) \
+		{ \
+			ctypep_r kar, kai; \
+			bli_tgets( chp,chp, kappa_cast, kar, kai ); \
+			ctypep_r* pi1r = (ctypep_r*)( pi1 + mnk*(dfac + ldp) ); \
+			ctypep_r* pi1i = (ctypep_r*)( pi1 + mnk*(dfac + ldp) ) + dfac; \
+			for ( dim_t d = 0; d < dfac; ++d ) \
+				bli_tcopyris( chp,chp, kar, kai, *(pi1r + d), *(pi1i + d) ); \
+		} \
+	} \
+	else if ( bli_is_hermitian( struca ) ) \
+	{ \
+		for ( dim_t mnk = 0; mnk < cdim; ++mnk ) \
+		{ \
+			ctypep alpha_cast, kappa_alpha; \
+			bli_tcopys( cha,chp, *(alpha1 + mnk*(inca + lda)), alpha_cast ); \
+			bli_tseti0s( chp, alpha_cast ); \
+			bli_tscal2s( chp,chp,chp,chp, kappa_cast, alpha_cast, kappa_alpha ); \
+			ctypep_r kar, kai; \
+			bli_tgets( chp,chp, kappa_alpha, kar, kai ); \
+			ctypep_r* pi1r = (ctypep_r*)( pi1 + mnk*(dfac + ldp) ); \
+			ctypep_r* pi1i = (ctypep_r*)( pi1 + mnk*(dfac + ldp) ) + dfac; \
+			for ( dim_t d = 0; d < dfac; ++d ) \
+				bli_tcopyris( chp,chp, kar, kai, *(pi1r + d), *(pi1i + d) ); \
+		} \
+	} \
+	else if ( bli_is_conj( conja )) \
+	{ \
+		for ( dim_t mnk = 0; mnk < cdim; ++mnk ) \
+		{ \
+			ctypep kappa_alpha; \
+			bli_tscal2js( chp,cha,chp,chp, kappa_cast, *(alpha1 + mnk*(inca + lda)), kappa_alpha ); \
+			ctypep_r kar, kai; \
+			bli_tgets( chp,chp, kappa_alpha, kar, kai ); \
+			ctypep_r* pi1r = (ctypep_r*)( pi1 + mnk*(dfac + ldp) ); \
+			ctypep_r* pi1i = (ctypep_r*)( pi1 + mnk*(dfac + ldp) ) + dfac; \
+			for ( dim_t d = 0; d < dfac; ++d ) \
+				bli_tcopyris( chp,chp, kar, kai, *(pi1r + d), *(pi1i + d) ); \
+		} \
+	} \
+	else \
+	{ \
+		for ( dim_t mnk = 0; mnk < cdim; ++mnk ) \
+		{ \
+			ctypep kappa_alpha; \
+			bli_tscal2s( chp,cha,chp,chp, kappa_cast, *(alpha1 + mnk*(inca + lda)), kappa_alpha ); \
+			ctypep_r kar, kai; \
+			bli_tgets( chp,chp, kappa_alpha, kar, kai ); \
+			ctypep_r* pi1r = (ctypep_r*)( pi1 + mnk*(dfac + ldp) ); \
+			ctypep_r* pi1i = (ctypep_r*)( pi1 + mnk*(dfac + ldp) ) + dfac; \
+			for ( dim_t d = 0; d < dfac; ++d ) \
+				bli_tcopyris( chp,chp, kar, kai, *(pi1r + d), *(pi1i + d) ); \
+		} \
+	} \
+	\
+	/* invert the diagonal if requested */ \
+	if ( invdiag ) \
+	{ \
+		for ( dim_t mnk = 0; mnk < cdim; ++mnk ) \
+		{ \
+			ctypep_r* pi1r = (ctypep_r*)( pi1 + mnk*(dfac + ldp) ); \
+			ctypep_r* pi1i = (ctypep_r*)( pi1 + mnk*(dfac + ldp) ) + dfac; \
+			for ( dim_t d = 0; d < dfac; ++d ) \
+				bli_tinvertris( chp,chp, *(pi1r + d), *(pi1i + d) ); \
+		} \
+	} \
+	\
+	/* if this an edge case in both directions, extend the diagonal with ones */ \
+	for ( dim_t mnk = cdim; mnk < bli_min( cdim_max, n_max ); ++mnk ) \
+	{ \
+		ctypep_r* pi1r = (ctypep_r*)( pi1 + mnk*(dfac + ldp) ); \
+		ctypep_r* pi1i = (ctypep_r*)( pi1 + mnk*(dfac + ldp) ) + dfac; \
+		ctypep_r oner, onei; \
+		bli_tset1s( chp_r, oner ); \
+		bli_tset0s( chp_r, onei ); \
+		for ( dim_t d = 0; d < dfac; ++d ) \
+			bli_tcopyris( chp,chp, oner, onei, *(pi1r + d), *(pi1i + d) ); \
+	} \
+} while (0)
+
+
+#define PACKM_DIAG_DIAG( ctypea, ctypep, cha, chp, dfac, inca, lda ) \
+PACKM_DIAG_DIAG_( ctypea, ctypep, PASTEMAC(chp,ctyper), cha, chp, PASTEMAC(chp,prec), dfac, inca, lda )
+
+
+#define PACKM_DIAG_BODY_( ctypea, ctypep, ctypep_r, cha, chp, chp_r, mn_min, mn_max, dfac, inca, lda, op ) \
 \
 do \
 { \
@@ -44,11 +135,18 @@ do \
 	{ \
 		ctypep kappa_alpha; \
 		PASTEMAC(t,op)( chp,cha,chp,chp, kappa_cast, *(alpha1 + mn*inca + k*lda), kappa_alpha ); \
+		ctypep_r kar, kai; \
+		bli_tgets( chp,chp, kappa_alpha, kar, kai ); \
+		ctypep_r* pi1r = (ctypep_r*)( pi1 + mn*dfac + k*ldp ); \
+		ctypep_r* pi1i = (ctypep_r*)( pi1 + mn*dfac + k*ldp ) + dfac; \
 		for ( dim_t d = 0; d < dfac; d++ ) \
-			bli_tcopys( chp,chp, kappa_alpha, *(pi1 + mn*dfac + d + k*ldp) ); \
+			bli_tcopyris( chp,chp, kar, kai, *(pi1r + d), *(pi1i + d) ); \
 	} \
 } while(0)
 
+
+#define PACKM_DIAG_BODY( ctypea, ctypep, cha, chp, mn_min, mn_max, dfac, inca, lda, op ) \
+PACKM_DIAG_BODY_( ctypea, ctypep, PASTEMAC(chp,ctyper), cha, chp, PASTEMAC(chp,prec), mn_min, mn_max, dfac, inca, lda, op )
 
 #define PACKM_DIAG_BODY_L( ctypea, ctypep, cha, chp, op ) \
 	PACKM_DIAG_BODY( ctypea, ctypep, cha, chp, k+1, cdim, cdim_bcast, inca_l, lda_l, op )
@@ -83,7 +181,7 @@ void PASTEMAC(cha,chp,opname,arch,suf) \
 	bli_tset0s_mxn \
 	( \
 	  chp, \
-	  cdim_max, \
+	  cdim_max*cdim_bcast, \
 	  n_max, \
 	  ( ctypep* )p, 1, ldp  \
 	); \
@@ -130,57 +228,7 @@ void PASTEMAC(cha,chp,opname,arch,suf) \
 	} \
 \
 	/* write the diagonal */ \
-	if ( bli_is_unit_diag( diaga ) ) \
-	{ \
-		for ( dim_t mnk = 0; mnk < cdim; ++mnk ) \
-		for ( dim_t d = 0; d < cdim_bcast; ++d ) \
-			bli_tcopys( chp,chp, kappa_cast, *(pi1 + mnk*(cdim_bcast + ldp) + d) ); \
-	} \
-	else if ( bli_is_hermitian( struca ) ) \
-	{ \
-		for ( dim_t mnk = 0; mnk < cdim; ++mnk ) \
-		{ \
-			ctypep alpha_cast, kappa_alpha; \
-			bli_tcopys( cha,chp, *(alpha1 + mnk*(inca + lda)), alpha_cast ); \
-			bli_tseti0s( chp, alpha_cast ); \
-			bli_tscal2s( chp,chp,chp,chp, kappa_cast, alpha_cast, kappa_alpha ); \
-			for ( dim_t d = 0; d < cdim_bcast; ++d ) \
-				bli_tcopys( chp,chp, kappa_alpha, *(pi1 + mnk*(cdim_bcast + ldp) + d) ); \
-		} \
-	} \
-	else if ( bli_is_conj( conja )) \
-	{ \
-		for ( dim_t mnk = 0; mnk < cdim; ++mnk ) \
-		{ \
-			ctypep kappa_alpha; \
-			bli_tscal2js( chp,cha,chp,chp, kappa_cast, *(alpha1 + mnk*(inca + lda)), kappa_alpha ); \
-			for ( dim_t d = 0; d < cdim_bcast; ++d ) \
-				bli_tcopys( chp,chp, kappa_alpha, *(pi1 + mnk*(cdim_bcast + ldp) + d) ); \
-		} \
-	} \
-	else \
-	{ \
-		for ( dim_t mnk = 0; mnk < cdim; ++mnk ) \
-		{ \
-			ctypep kappa_alpha; \
-			bli_tscal2s( chp,cha,chp,chp, kappa_cast, *(alpha1 + mnk*(inca + lda)), kappa_alpha ); \
-			for ( dim_t d = 0; d < cdim_bcast; ++d ) \
-				bli_tcopys( chp,chp, kappa_alpha, *(pi1 + mnk*(cdim_bcast + ldp) + d) ); \
-		} \
-	} \
-\
-	/* invert the diagonal if requested */ \
-	if ( invdiag ) \
-	{ \
-		for ( dim_t mnk = 0; mnk < cdim; ++mnk ) \
-		for ( dim_t d = 0; d < cdim_bcast; ++d ) \
-			bli_tinverts( chp,chp, *(pi1 + mnk*(cdim_bcast + ldp) + d) ); \
-	} \
-\
-	/* if this an edge case in both directions, extend the diagonal with ones */ \
-	for ( dim_t mnk = cdim; mnk < bli_min( cdim_max, n_max ); ++mnk ) \
-	for ( dim_t d = 0; d < cdim_bcast; ++d ) \
-		bli_tset1s( chp, *(pi1 + mnk*(cdim_bcast + ldp) + d) ); \
+	PACKM_DIAG_DIAG( ctypea, ctypep, cha, chp, cdim_bcast, inca, lda ); \
 }
 
 INSERT_GENTFUNC2_BASIC( packm_diag, BLIS_CNAME_INFIX, BLIS_REF_SUFFIX )
