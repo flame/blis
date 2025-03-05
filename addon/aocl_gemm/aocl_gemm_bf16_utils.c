4d22451fbb360ae493a749eef95b3e32c7319919
varshav <varsha.v@amd.com>
2025-03-05 06:31:19 +0000

    Bug Fix in BF16 Re-order/unreorder with AOCL_ENABLE_INSTRUCTIONS
     - Currently, the bf16 reorder function does not add padding for
       n=1 cases. But, the bf16 AVX2 Unreorder path considers the input
       re-ordered B matrix to be padded along the n and k dimension.
     - Hence, modified the conditions to make sure the path doesn't break
       while the AVX2 kernels are executed in AVX512 machines when
       B matrix reordered.
    
    Change-Id: I7dd3d37a24758a8e93e80945b533abfcf15f65a1

--- addon/aocl_gemm/aocl_gemm_bf16_utils.c
+++ addon/aocl_gemm/aocl_gemm_bf16_utils.c
@@ -100,7 +100,7 @@ AOCL_GEMM_REORDER(bfloat16, bf16bf16f32of32_reference)
 	}
 
 #if (defined(BLIS_KERNELS_ZEN4) && (!defined(LPGEMM_BF16_JIT)))
-	if( n == 1 )
+	if( ( lpgemm_get_enabled_arch() != BLIS_ARCH_ZEN3 ) && ( n == 1 ) )
 	{
 		if( rs_b == 1 )
 		{
@@ -257,8 +257,10 @@ AOCL_GEMM_GET_REORDER_BUF_SIZE(bf16bf16f32of32)
 	// not rounded to NR (=64), since that would result in memory wastage.
 #if (defined(BLIS_KERNELS_ZEN4) && (!defined(LPGEMM_BF16_JIT)))
 	dim_t n_reorder;
-
-	if( n == 1 )
+	/*It is expected that while bf16 input is passed to AVX2 kernels,
+	  the unreorder/conversion of bf16->f32 is done, which expects the
+	  reordered matrix to be padded with n multiple of 16, k multiple of 2. */
+	if( ( lpgemm_get_enabled_arch() != BLIS_ARCH_ZEN3 ) &&  ( n == 1 ) )
 	{
 		n_reorder = 1;
 	}
@@ -269,7 +271,7 @@ AOCL_GEMM_GET_REORDER_BUF_SIZE(bf16bf16f32of32)
 
 	// Extra space since packing does length in multiples of 2.
 	dim_t k_reorder;
-	if( n == 1 )
+	if( ( lpgemm_get_enabled_arch() != BLIS_ARCH_ZEN3 ) && ( n == 1 ) )
 	{
 		k_reorder = k;
 	}
@@ -362,6 +364,15 @@ AOCL_GEMM_REORDER(bfloat16, bf16bf16f32of32)
 	}
 
 #if (defined(BLIS_KERNELS_ZEN4) && (!defined(LPGEMM_BF16_JIT)))
+	/*When AOCL_ENABLE_INSTRUCTIONS=AVX2, f32 kernels would be executed for
+	  bf16 input, for which re-ordered bf16 input is converted and unreordered
+	  to hold f32 values. The un-reorder/convert API considers the padded bf16 input.*/
+	if( lpgemm_get_enabled_arch() == BLIS_ARCH_ZEN3 )
+	{
+		aocl_reorder_bf16bf16f32of32_reference( order,trans ,mat_type, input_buf_addr,
+			reorder_buf_addr, k, n, ldb );
+		return;
+	}
 	if( n == 1 )
 	{
 		if( rs_b == 1 )
