17ffb7212bebae92991e090f40ddb7e3c8cc1354
Devin Matthews <damatthews@smu.edu>
2025-06-21 20:00:21 -0500

    Update license and related info.
    (cherry picked from commit cb0da3e6e851c0c6b1896812a6e986f4a6a11f4a)

--- frame/3/bli_l3_sup_var1n2m.c
+++ frame/3/bli_l3_sup_var1n2m.c
@@ -5,6 +5,7 @@
    libraries.
 
    Copyright (C) 2019, Advanced Micro Devices, Inc.
+   Copyright (C) 2022, Southern Methodist University
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
